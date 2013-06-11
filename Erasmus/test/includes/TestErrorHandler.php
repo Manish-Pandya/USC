2aae77b4ab75b807741b6163eb5520b87ea3aadf
Mitch Martin <mmartin@graysail.com>
2013-06-11 15:39:25 -0400

    Stubbed test class for ErrorHandler

---
+++ Erasmus/test/includes/TestErrorHandler.php
@@ -0,0 +1,25 @@
+<?php
+require_once(dirname(__FILE__) . '/../../simpletest/autorun.php');
+require_once(dirname(__FILE__) . '/../../../src/includes/ErrorHandler.php');
+
+class TestErrorHandler extends UnitTestCase {
+	
+	//TODO: TEST CRUD OPERATIONS
+	public function test_todo(){
+		$this->fail("TODO: Add test cases for TestErrorHandler");
+	}
+	
+	public function test_handleException(){
+		
+	}
+	
+	public function test_handleError(){
+		
+	}
+	
+	public function test_handleFatalError(){
+		
+	}
+}
+
+?>
