baf05115953c51611a9827b28ee76f28eb2cd2ca
Mitch Martin <mmartin@graysail.com>
2013-06-12 12:56:48 -0400

    Added empty test case file for action_functions.php

---
+++ Erasmus/test/includes/action_functions/TestActionFunctions.php
@@ -0,0 +1,15 @@
+<?php
+require_once(dirname(__FILE__) . '/../../simpletest/autorun.php');
+
+//Include action functions to test
+require_once(dirname(__FILE__) . '/../../../src/includes/action_functions.php');
+
+class TestActionFunctions extends UnitTestCase {
+	
+	//TODO: TEST ACTION FUNCTIONS
+	public function test_todo(){
+		$this->fail("TODO: Add test cases for Action Functions");
+	}
+}
+
+?>
