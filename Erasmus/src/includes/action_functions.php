e273270fb8d333fa41a1a9afd59c6c5bc1db75b0
Mitch Martin <mmartin@graysail.com>
2013-11-14 14:56:58 -0500

    Added mock implementation for getUserById, saveUser, getAllRoles, getChecklist, getQuestion, getPI, getHazardsInRoom.
    Added ActionError class to encapsulate error details in action functions

--- Erasmus/src/includes/action_functions.php
+++ Erasmus/src/includes/action_functions.php
@@ -4,6 +4,10 @@
  * and should not execute any code upon inclusion.
  * 
  * Because this file merely provides the functions, they are easily testable
+ * 
+ * If an error should occur, Action functions should return either NULL or
+ * an instance of ActionError. Returning an ActionError allows the function
+ * to provide additional information about the error.
  */
 ?><?php
 //TODO: Split these functions up into further includes?
@@ -15,6 +19,7 @@ function getAllUsers(){
 	$LOG = Logger::getLogger( 'Action:getAllUsers' );
 	$allUsers = array();
 	
+	//TODO: Query for Users
 	for( $i = 0; $i < 10; $i++ ){
 		$user = new User();
 		$user->setIsActive(TRUE);
@@ -31,13 +36,76 @@ function getAllUsers(){
 	return $allUsers;
 };
 
-function saveUser(){ };
+function getUserById(){
+	if( array_key_exists( 'id', $_REQUEST)){
+		$keyid = $_REQUEST['id'];
+	
+		//TODO: query for User with this ID
+		$user = new User();
+		$user->setIsActive(TRUE);
+		$user->setEmail("user$keyid@host.com");
+		$user->setName("User #$keyid");
+		$user->setUsername("user$keyid");
+		$user->setKeyId($keyid);
+	
+		return $user;
+	}
+	else{
+		//error
+		return new ActionError("No request parameter 'id' was provided");
+	}
+}
+
+function saveUser(){
+	$LOG = Logger::getLogger('Action:saveUser');
+	
+	//read user JSON from input stream
+	$stuff = file_get_contents('php://input');
+	
+	$LOG->info( 'Read from input stream: ' . $stuff );
+	
+	//TODO: verify that $stuff is actual data
+	if( !empty( $stuff) ){	
+		//convert to User object
+		$userObject = JsonManager::decode($stuff, new User());
+		
+		$LOG->info( 'Converted to User: ' . $userObject);
+		
+		$userObject->setKeyId( 54321 );
+		
+		return $userObject;
+	}
+	else{
+		return new ActionError('No data read from input stream');
+	}
+};
+
 function activateUser(){ };
 function deactivateUser(){ };
-function getAllRoles(){ };
+
+function getAllRoles(){
+	return array(
+		'Administrator',
+	);
+};
 
 // Checklist Hub
-function getChecklist(){ };
+function getChecklist(){
+	if( array_key_exists( 'id', $_REQUEST)){
+		$keyid = $_REQUEST['id'];
+	
+		//TODO: query for Checklist with this ID
+		$checklist = new Checklist();
+		$checklist->setKeyId($keyid);
+	
+		return $checklist;
+	}
+	else{
+		//error
+		return new ActionError("No request parameter 'id' was provided");
+	}
+};
+
 function getQuestions(){ };
 function saveChecklist(){ };
 function saveQuestion(){ };
@@ -48,18 +116,76 @@ function saveHazards(){ };
 //function saveChecklist(){ };	//DUPLICATE FUNCTION
 
 // Question Hub
-function getQuestion(){ };
+function getQuestion(){
+	if( array_key_exists( 'id', $_REQUEST)){
+		$keyid = $_REQUEST['id'];
+	
+		//TODO: query for Question with this ID
+		$question = new Question();
+		$question->setKeyId($keyid);
+		$question->setText('What?');
+		$question->setStandardsAndGuidelines('Because.');
+	
+		return $question;
+	}
+	else{
+		//error
+		return new ActionError("No request parameter 'id' was provided");
+	}
+};
+
 function saveQuestionRelation(){ };
 function saveDeficiencyRelation(){ };
 function saveRecommendationRelation(){ };
 
 // Inspection, step 1 (PI / Room assessment)
-function getPI(){ };
+function getPI(){
+	if( array_key_exists( 'id', $_REQUEST)){
+		$keyid = $_REQUEST['id'];
+		
+		//TODO: query for PI with this ID
+		$pi = new PrincipalInvestigator();
+		$pi->setKeyId($keyid);
+		
+		return $pi;
+	}
+	else{
+		//error
+		return new ActionError("No request parameter 'id' was provided");
+	}
+};
+
 function getRooms(){ };
 function saveInspection(){ };
 
 // Inspection, step 2 (Hazard Assessment)
-function getHazardsInRoom(){ };
+function getHazardsInRoom(){
+	if( array_key_exists( 'id', $_REQUEST)){
+		$roomId = $_REQUEST['id'];
+		
+		//TODO: get Room
+		$room = new Room();
+		$room->setKeyId($roomId);
+		$room->setName("Room $roomId");
+		$hazards = array();
+		
+		for( $i = 0; $i < 5; $i++ ){
+			$hazard = new Hazard();
+			$hazard->setRooms( array($room) );
+			$hazard->setKeyId($i);
+			$hazard->setName("Dangerous thing #$i");
+			
+			$hazards[] = $hazard;
+		}
+	
+		return $hazards;
+	}
+	else{
+		//error
+		return new ActionError("No request parameter 'id' was provided");
+	}
+};
+
 function saveHazardRelation(){ };
 function saveRoomRelation(){ };
 
---
+++ Erasmus/src/includes/classes/ActionError.php
@@ -0,0 +1,14 @@
+<?php
+
+class ActionError {
+	public $message;
+	
+	public function __construct( $message ){
+		$this->message = $message;
+	}
+	
+	public function getMessage(){ return $this->message; }
+	public function setMessage($m){ $this->message = $m; }
+}
+
+?>
