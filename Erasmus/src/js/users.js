0728085bd9b3709b5e613f7dbd060ee844f76552
Matthew Breeden <mbreeden@graysail.com>
2013-11-15 12:53:51 -0500

    User Hub cleanups and nitpicks.  Hopefully ready for prod.

--- Erasmus/src/js/users.js
+++ Erasmus/src/js/users.js
@@ -12,9 +12,6 @@ userList.factory('testFactory', function($http){
 	//user jsonp method of the angularjs $http object to request data from service layer
 	$http.jsonp(url)
 		.success( function(data) {	
-		   console.log(url);
-		   console.log('success');
-		   console.log(data);
 		   //onSuccess is the method we have passed from the controller.  Binds data from service layer to angularjs $scope object
 	       onSuccess(data);
 	    })
@@ -26,8 +23,17 @@ userList.factory('testFactory', function($http){
         });
         
 	};
-	tempFactory.saveUser = function(onSuccess, url){
-		alert('saving user');
+	tempFactory.saveUser = function(url, user, onSuccess){
+		$http.post(url, user)
+		.success(function(data, status, headers, config) {
+		    onSuccess(data);
+		})
+		.error(function(data, status, headers, config) {
+			console.log(headers());
+            console.log(status);
+            console.log(config);
+		});
+
 	}
 	
 	return tempFactory;
@@ -55,20 +61,40 @@ function UserListController($scope, testFactory) {
 
   $scope.editUser = function(user){
 
+  	//set some display properties for the user objects, so that we can hilight the user currently being edited
   	angular.forEach($scope.users, function(thisUser, key){
-  		console.log(user);
 	  	thisUser.edit = false;
 	  	thisUser.notEdit = true;
+	  	thisUser.updated = false;
 	});
 	
+	//set display properties to hilight user currently being edited.
 	user.notEdit = false;
   	user.edit = true;
 
-  	var editedUser = angular.copy(user);
+  	//make a copy of the edited user, save it in the scope.  this will allow us to cancel any edits 
+  	$scope.userCopy = angular.copy(user);
 
-  	console.log(editedUser);
+  }
 
-  	$scope.userCopy = editedUser;
+  //click handler checks the user's IsActive property, udpates accordingly
+  $scope.handleUserActive = function(user){
+
+  	//set the IsActive state for the user in the view model
+	if(user.IsActive == false || !user.IsActive){
+  		user.IsActive = true;
+  	}else{
+  		user.IsActive = false;
+  	}
+
+
+  	//callback function to make sure that the user state matches the the state of the corresponding object on the server
+  	var switchActiveState = function(data,user){
+	 	user = data;
+  	}
+
+  	//send the edit to the server, pass it to the callback
+  	testFactory.saveUser('/Erasmus/src/ajaxaction.php?action=saveUser', user, switchActiveState);
 
   }
   
@@ -77,51 +103,77 @@ function UserListController($scope, testFactory) {
    * used for creating and updating users
    * 
    */
-  $scope.saveUser = function(user){
+  $scope.saveUser = function(copy, user){
+
+	//callback function.  recieves new user data and updates the edited user view model object with it
+	var updateUserView = function(data){
+		console.log(user);
+	  	console.log($scope.userCopy);
+	    $scope.userCopy = data;
+  
+	  	angular.forEach($scope.users, function(thisUser, key){
+		  	thisUser.notEdit = false;
+		});
 
 
-  	$scope.userCopy.edit = false;
+	  	//set each property of the edited user to the corresponding property of the user object on the server
+  	   for (var property in $scope.userCopy) {
+			if (data.hasOwnProperty(property)) {
+			    user[property] = $scope.userCopy[property];
+			}
+	   }
 
-  	for (var property in $scope.userCopy) {
-	    if ($scope.userCopy.hasOwnProperty(property)) {
-	    	console.log(property);
-	        user[property] = $scope.userCopy[property];
-	    }
+	   	user.edit = false;
+		user.updated = true;
 	}
 
-	for(i=0;i<$scope.users.length;i++){
+	testFactory.saveUser('/Erasmus/src/ajaxaction.php?action=saveUser', $scope.userCopy, updateUserView);
 
-		thisUser = $scope.users[i];
-
-		thisUser.notEdit = false;
-		thisUser.edit = false;
-		thisUser.updated = false;
-	}
-
-	user.updated = true;
-
-	console.log(user);
-
-	//testFactory.saveUser(onGetUsers,'http://hazmars.graysail.com/api.php?callback=JSON_CALLBACK');
   }
 
+  $scope.addUser = function(){
+
+  	//new empty user object
+  	newUser = {};
+
+  	//grab a list of the User objects properties, set them to empty strings.
+  	for (var property in $scope.users[0]) {
+		if ($scope.users[0].hasOwnProperty(property)) {
+			console.log(property);
+		    newUser[property] = '';
+		}
+    }
+
+    //make sure the new user is active and not in an edited state.
+    newUser.IsActive = true;
+    newUser.edit = false;
+
+    //add the new user to the beginning of the $scope.users array of user objects
+  	$scope.users.unshift(newUser);
+
+  	//send the user to the edit function, so that its properties can be edited in the UI
+  	$scope.editUser(newUser);
+
+  }
+
+  //undo edits to a user
   $scope.cancelEdits = function(user){
+
+  	//set the user back to a state that indicates it's not currently being edited.
+  	//since we haven't saved the user, we still have the original user preserved in $scope.users, and can access it
 	user.notEdit = false;
 	user.edit = false;
 
+	//reset the display properties for all the users
 	for(i=0;i<$scope.users.length;i++){
 
 		thisUser = $scope.users[i];
-
 		thisUser.notEdit = false;
 		thisUser.edit = false;
 		thisUser.updated = false;
 	}
   }
   
-  function onSaveUser(){
-	  
-  }
 };
 
 //set controller
--- Erasmus/src/views/hubs/UserHub.php
+++ Erasmus/src/views/hubs/UserHub.php
@@ -4,38 +4,47 @@ require_once '../top_view.php';
 <script src="../../js/users.js"></script>
 
 <div class="navbar">
-<ul class="nav pageMenu" style="min-height: 50px; background: #86b32d; color:white !important; padding: 4px 0 0 0; width:100%">
+<ul class="nav pageMenu" style="min-height: 50px; background: #51a351; color:white !important; padding: 4px 0 0 0; width:100%">
 	<li class="span3">
 		<img src="../../img/user-icon.png" class="pull-left" style="height:50px" />
 		<h2  style="padding: 11px 0 5px 85px;">User Hub</h2>	
 	</li>
 	<li>
-		<a class=" btn" style="" data-toggle="modal"  href="#addUser" ><img src='../../img/add-user-icon.png'>Add User</a>
+		
 	</li>
 </ul>
 
 </div>
 <span ng-app="userList" ng-controller="UserListController">
-<table class="userList table table-striped table-hover">
+	
+	<div class="btn-group" style="margin:15px 0">
+
+     <a href="javascript:void(0);" class="btn btn-large" ng-init="{showInactive = false}" ng-click="showInactive = !showInactive" ng-class="{ 'btn-sinfo' : showInactive, 'btn-susccess': !showInactive }">
+	     <span ng-show="showInactive"><i class="icon-minus-3" style="font-size:30px; width: 35px; font-size: 30px; margin-left:-9px"></i>Hide Inactive Users</span>
+	     <span ng-hide="showInactive"><i class="icon-plus-3"  style="font-size:30px; width: 35px; font-size: 30px; margin-left:-9px"></i>Show Inactive Users</span>
+     </a>
+     <a class="btn-large btn btn" ng-click="addUser()" style="padding:10px; margin-left:5px;" data-toggle="modal"  href="#addUser"><img src='../../img/add-user-icon.png'>Add User</a>
+   </div>
+
+
+<table class="userList table table-striped table-hover" ng-class="{'hide-inactive-rows': !showInactive}">
 <thead>
 	<tr>
 		<th>Edit User</th><th>Activate/Deactivate User</th><th>Name</th><th>LDAP ID</th><th>Email</th><th>Role</th>
 	</tr>
 </thead>
-<!--
-	"Roles": null,
-    "Username": "user0",
-    "Name": "User #0",
-    "Email": "user0@host.com",
-    "KeyId": 0,
-    "DateCreated": null,
-    "DateLastModified": null,
-    "IsActive": true
--->
-<tbody >
-	<tr id="{{user.id}}" ng-repeat="user in users" ng-class="{edit: user.edit, notedit: user.notEdit, updated: user.updated}">
-		<td ng-hide="user.edit"><a class="edit btn btn-large btn-primary" ng-click="editUser(user)">Edit</a></td><td ng-show="user.edit"><a class="edit btn btn-large btn-info" ng-click="saveUser(user)">Save</a></td>
-		<td ng-hide="user.edit"><a class="btn btn-danger btn-large DeactivateeRow" href="#">Deactivate</a></td><td ng-show="user.edit"><a class="edit btn btn-large btn-danger" ng-click="cancelEdits(user)">Cancel</a></td>
+
+<tbody>
+	<tr id="{{user.id}}" ng-repeat="user in users" ng-class="{edit: user.edit, notedit: user.notEdit, updated: user.updated, inactive: !user.IsActive}">
+		<td ng-hide="user.edit"><a class="edit btn btn-large btn-primary" ng-click="editUser(user)">Edit</a></td><td ng-show="user.edit"><a class="edit btn btn-large btn-info" ng-click="saveUser(userCopy, user)">Save</a></td>
+		<td ng-hide="user.edit">
+			<a class="btn btn-danger btn-large DeactivateeRow" ng-click="handleUserActive(user)" ng-show="user.IsActive">Deactivate</a>
+			<a class="btn btn-success btn-large DeactivateeRow" ng-click="handleUserActive(user)" ng-hide="user.IsActive">Activate</a>
+		</td>
+
+		<td ng-show="user.edit"><a class="edit btn btn-large btn-danger" ng-click="cancelEdits(user)">Cancel</a></td>
+		
+
 		<td ng-hide="user.edit">{{user.Name}}</td><td ng-show="user.edit"><input ng-model="userCopy.Name"/></td>
 		<td ng-hide="user.edit">{{user.Username}}</td><td ng-show="user.edit"><input ng-model="userCopy.Username"/></td>
 		<td ng-hide="user.edit">{{user.Email}}</td><td ng-show="user.edit"><input ng-model="userCopy.Email"/></td>
@@ -43,154 +52,9 @@ require_once '../top_view.php';
 	</tr>
 </tbody>
 </table>
-<pre>{{users | json}}</pre>
+
 </span>
-<!--
-<div ng-app="userList" ng-controller="UserListController">
-	<ul >
-		<li ng-repeat="user in users">
-			<ul class="span12">
-				<li class="span1"><a class="edit btn btn-large btn-primary" ng-click="editUser(user)">Edit</a></li>
-				<li class="span2"><a class="btn btn-danger btn-large DeactivateeRow" href="#">Deactivate</a></li>
-				<li class="span2" ng-hide="user.edit">{{user.name}}</li><li ng-show="user.edit"><input ng-model="userCopy.name"/></li>
-				<li class="span2">bUserington</li>
-				<li class="span2">{{user.email}}</li>
-				<li class="span2">Administrator</li>
-			</ul>
-		</li>
-	</ul>
-</div>
--->
-<!-- begin edit user modal dialogue
-<div class="modal hide fade" id="editUser1">
-  <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-    <h3>Editing <span id="nameOfEditedUser">Bob Userington</span></h3>
-  </div>
-  <form style="padding:0; margin:0;" class="form-horizontal">
-  <div class="modal-body">
- 
-  	<div class="control-group">
-	    <label class="control-label" for="fName">First Name</label>
-	    <div class="controls">
-	      <input type="text" name="fName" id="fName" placeholder="Password" value="Bob">
-	    </div>
-    </div>
-    
-    <div class="control-group">
-	    <label class="control-label" for="lName">Last Name</label>
-	    <div class="controls">
-	      <input type="text" name="lName" id="lName" placeholder="Password" value="Userington">
-	    </div>
-    </div>
-    
-    <div class="control-group">
-	    <label class="control-label" for="email">Email</label>
-	    <div class="controls">
-	      <input type="text" name="email" id="email" placeholder="Password" value="bob@bob.bob">
-	    </div>
-    </div>
-    
-    <div class="control-group">
-	    <label class="control-label" for="role">Role</label>
-	    <div class="controls">
-	      <select name="role" id="role">
-	      	<option>Administrator</option>
-	      	<option>Inpsector</option>
-	      	<option>Lab User</option>
-	      	<option>Principle Investigator</option>
-	      </select>
-	    </div>
-    </div>
-  </div>
-  <div class="modal-footer">
-    <a href="#" class="btn btn-danger btn-large" data-dismiss="modal">Close</a>
-    <a href="#" class="btn btn-primary btn-large" id="saveChanges">Save changes</a>
-  </div>
-  </form>
-</div> -->
-<!-- end edit user modal dialogue -->
 
-
-<!-- begin add new user modal dialogue 
-<div class="modal hide fade" id="addUser">
-	<div class="modal-header">
-		<h3>Add a New User</h3>
-	</div>
-	<form style="padding:0; margin:0;" class="form-horizontal">
-	<div class="modal-body">
-
-	<div class="control-group">
-	    <label class="control-label" for="fName">LDAP ID</label>
-	    <div class="controls">
-	      <input type="text" name="fName" id="ldapID" placeholder="" value="">
-	    </div>
-    </div>
-    
-	<div class="control-group">
-	    <label class="control-label" for="fName">First Name</label>
-	    <div class="controls">
-	      <input type="text" name="fName" id="fNameNew"  >
-	    </div>
-    </div>
-    
-    <div class="control-group">
-	    <label class="control-label" for="lName">Last Name</label>
-	    <div class="controls">
-	      <input type="text" name="lNameNew" id="lNameNew" >
-	    </div>
-    </div>
-    
-    <div class="control-group">
-	    <label class="control-label" for="email">Email</label>
-	    <div class="controls">
-	      <input type="text" name="email" id="emailNew">
-	    </div>
-    </div>
-    
-    <div class="control-group">
-	    <label class="control-label" for="role">Role</label>
-	    <div class="controls">
-	      <select name="role" id="role">
-	      	<option>Administrator</option>
-	      	<option>Inpsector</option>
-	      	<option>Lab User</option>
-	      	<option>Principle Investigator</option>
-	      </select>
-	    </div>
-    </div>
-		
-	</div>
-	 <div class="modal-footer">
-    <a href="#" class="btn btn-danger btn-large" data-dismiss="modal">Close</a>
-    <a href="#" class="btn btn-primary btn-large">Create</a>
-  </div>
-</div>-->
-<!-- end add new user modal dialogue -->
-<script>
-/*
-$(document).ready(function(){
-	var query = location.href.split('#');
-	var anchorPart = query[1];
-	if 	(anchorPart != null){
-		$('#'+anchorPart).addClass('blue-border');
-		$('#'+anchorPart+' td').addClass('shadow');
-		$('#editUser1').modal('show');
-		$('#fName').val('Davit');
-		$('#lName').val('Mrelashvili');
-		$('#email').val('dmrelashvili@sc.edu');
-		$("#role").val("Principle Investigator");
-		$('#nameOfEditedUser').text('Davit Mrelashvili');
-		$("#saveChanges").attr('href', "PIHub.php")
-	}
-})
-
-$('#ldapID').change(function(){
-	$('#fNameNew').val('Robert');
-	$('#lNameNew').val('Userington');
-	$('#emailNew').val('ruserington@sc.edu');
-})*/
-</script>
 <?php
 require_once '../bottom_view.php';
 ?>
