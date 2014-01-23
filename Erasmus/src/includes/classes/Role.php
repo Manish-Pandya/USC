9ed5047c1bc0d72743abf900af512560f7f95595
hoke <hoke@graysail.com>
2014-01-23 14:19:46 -0500

    Created Role class

---
+++ Erasmus/src/includes/classes/Role.php
@@ -0,0 +1,72 @@
+<?php
+
+include_once 'GenericCrud.php';
+
+/**
+ *
+ *
+ *
+ * @author Hoke Currie, GraySail LLC
+ */
+class Role extends GenericCrud{
+	
+	// CRUD Meta-Data
+	/** Name of the DB Table */
+	protected static $TABLE_NAME = "erasmus_role";
+	
+	/** Key/Value Array listing column names mapped to their types */
+	protected static $COLUMN_NAMES_AND_TYPES = array(
+		//users are a relationship
+		"name"		=> "text",
+	);
+	
+	/** Relationships */
+	protected static $USERS_RELATIONSHIP = array(
+		"className"	=>	"User",
+		"tableName"	=>	"erasmus_user_role",
+		"keyName"	=>	"role_id",
+		"foreignKeyName"	=>	"user_id"
+	); 
+	
+	// Access information
+	
+	/** Array of users */
+	private $users;
+	
+	// General Role Info
+	
+	/** name of this Role */
+	private $name;
+	
+	/** Email address of this User */
+	private $email;
+	
+	// Constructor(s)
+	public function __construct(){
+		
+	}
+	
+	// Required for GenericCrud
+	public function getTableName(){
+		return self::$TABLE_NAME;
+	}
+	
+	public function getColumnData(){
+		return self::$COLUMN_NAMES_AND_TYPES;
+	}
+	
+	// Accessors / Mutators
+	public function getUsers(){ 
+		if($this->roles == null) {
+			$roleDAO = new GenericDAO($this);
+			$this->roles = $roleDAO->getRelatedItemsById($this->key_id, DataRelationShip::fromArray(self::$USERS_RELATIONSHIP));
+		}
+		return $this->roles;
+	}
+	public function setUsers($users){ $this->users = $users; }
+	
+	public function getName(){ return $this->name; }
+	public function setName($name){ $this->name = $name; }
+	
+}
+?>
