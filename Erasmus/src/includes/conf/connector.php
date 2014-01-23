7ec5276d32090e23aa5aba4d5750530d21317c01
hoke <hoke@graysail.com>
2014-01-23 15:34:37 -0500

    other part of move

---
+++ Erasmus/src/includes/conf/connector.php
@@ -0,0 +1,14 @@
+<?php
+	
+// Wired to use either PEAR DB (deprecated) or PEAR MDB2 (proper choice for php5, but not available in production env) 
+//require_once 'DB.php';
+require_once 'MDB2.php';
+	$dbString = getDBConnection(); 
+	$mdb2 =& DB::connect($dbString);
+	if (PEAR::isError($mdb2)) {
+   		die("DATABASE CONNECTION ERROR");
+	}
+//	$mdb2->setFetchMode(DB_FETCHMODE_OBJECT);
+	$mdb2->setFetchMode(MDB2_FETCHMODE_OBJECT);
+	
+?>
