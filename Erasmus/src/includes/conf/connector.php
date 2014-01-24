8557be14d5fc7a755443e6c536f793360265fd0b
Mitch Martin <mmartin@graysail.com>
2014-01-24 10:18:25 -0500

    Updated reference from DB::connect to MDB2::connect

--- Erasmus/src/includes/conf/connector.php
+++ Erasmus/src/includes/conf/connector.php
@@ -3,8 +3,8 @@
 // Wired to use either PEAR DB (deprecated) or PEAR MDB2 (proper choice for php5, but not available in production env) 
 //require_once 'DB.php';
 require_once 'MDB2.php';
-	$dbString = getDBConnection(); 
-	$mdb2 =& DB::connect($dbString);
+	$dbString = getDBConnection();
+	$mdb2 =& MDB2::connect($dbString);
 	if (PEAR::isError($mdb2)) {
    		die("DATABASE CONNECTION ERROR");
 	}
