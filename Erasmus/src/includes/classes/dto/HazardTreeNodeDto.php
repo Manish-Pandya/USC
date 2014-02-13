84c3a7289b464c145ed9c9f32304e48abde4bbc0
Matthew Breeden <mbreeden@graysail.com>
2014-02-13 12:45:00 -0500

    Changed name of property $room_ids to $room_dtos to reflect the fact that a collection of roomDTO objects will now be passed to this class instead of a collection of Room key_ids

--- Erasmus/src/includes/classes/dto/HazardTreeNodeDto.php
+++ Erasmus/src/includes/classes/dto/HazardTreeNodeDto.php
@@ -2,19 +2,19 @@
 class HazardTreeNodeDto {
 	private $key_id;
 	private $hazard_name;
-	private $room_ids;
+	private $room_dtos;
 	private $children;
 	
-	public function __construct( $key_id, $hazard_name, $room_ids, $children ){
+	public function __construct( $key_id, $hazard_name, $room_dtos, $children ){
 		$this->key_id = $key_id;
 		$this->hazard_name = $hazard_name;
-		$this->room_ids = $room_ids;
+		$this->room_dtos = $room_dtos;
 		$this->children = $children;
 	}
 	
 	public function getKey_Id(){ return $this->key_id; }
 	public function getHazardName(){ return $this->hazard_name; }
-	public function getRoomIds(){ return $this->room_ids; }
+	public function getRoomDtos(){ return $this->room_dtos; }
 	public function getChildren(){ return $this->children; }
 }
 ?>
