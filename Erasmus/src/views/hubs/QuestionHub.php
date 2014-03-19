4de85f49a8502abcfc05fe33e7b19ba707e59797
Matthew Breeden <mbreeden@graysail.com>
2014-03-19 14:35:16 -0400

    Question's can now be edited

--- Erasmus/src/views/hubs/QuestionHub.php
+++ Erasmus/src/views/hubs/QuestionHub.php
@@ -11,11 +11,12 @@ require_once '../top_view.php';
 	</ul>
 </div>
 <div class="container-fluid whitebg" ng-app="questionHub" ng-controller="QuestionHubController">
-{{question}}
-	<h1 id="currentQuestion">Current Question:<br><span id="questionText">{{question.Text}}</span></h1>
+<pre>{{question|json}}</pre>
+	<h1 ng-show="!question.beingEdited" id="currentQuestion">Current Question:<br><span id="questionText">{{question.Text}}</span><a class="btn btn-primary bt-large" ng-click="editQuestion()"><i class="icon-pencil"></i>Edit Question</a></h1>
+	<input ng-show="question.beingEdited" type="text" class="span9" ng-model="questionCopy.Text"/><a ng-click="saveEditedQuestion(question)" class="btn btn-success btn-small"><i class="icon-checkmark"></i>Save</a><a class="btn btn-danger btn-small" ng-click="cancelEdit()"><i class="icon-cancel"></i>Cacnel</a>
 	<h3 style="margin-top:30px;">Deficiencies for this question:</h3>
 	<ul class="deficiencyList listWithChecks sortable" id="sortable">
-		<li ng-repeat="def in question.Deficiencies">Exposure Control Plan has not been reviewed and updated at least annually<div class="checklistRow"><a class="btn  btn-danger deactivateRow">Deactivate</a></div></li>
+		<li ng-repeat="def in question.Deficiencies">{{def.Text}}<div class="checklistRow"><a class="btn  btn-danger deactivateRow" ng-click="handleObjActive(def,question)">Deactivate</a></div></li>
 	</ul>
 	
 	<form class="form" style="margin-top:10px;">
@@ -23,35 +24,40 @@ require_once '../top_view.php';
 	    <div class="control-group">
 		    <label class="control-label" for="email">Add a Deficiency for this Question:</label>
 		    <div class="controls">
-		      <textarea rows="5" id="newDeficiency" ng-model="question.newDeficiency" cols="500" style="width:50%"></textarea>
+		      <label>DEFICIENCY DESCRIPTION</label>
+		      <textarea rows="5" id="newDeficiency" ng-model="question.newDeficiency.text" cols="500" style="width:50%"></textarea>
+		      <label>COMPLIANCE REFERENCE</label>
+		 	  <input type="text" ng=model="question.newDeficiency.reference"/>
+		 	  <label>COMPLIANCE DESCRIPTION</label>
+		 	  <textarea rows="3" ng-model="question.newDeficiency.description" cols="500" style="width:50%"></textarea>
 		 </div>
 		 <a class="btn btn-large btn-success addDeficiency" ng-click="addDeficiency(question)">Add</a>
 	</form>
 	
 	<h3 style="margin-top:30px;">Recommendations for this question:</h3>
 	<ul class="recommendationList listWithChecks sortable" id="sortable">
-		<li ng-repeat="rec in question.Recommendations">{{rec.Text}}<div class="checklistRow"><a class="btn  btn-danger deactivateRow">Deactivate</a></div></li>
+		<li ng-repeat="rec in question.Recommendations">{{rec.Text}}<div class="checklistRow"><a class="btn  btn-danger deactivateRow" ng-click="handleObjActive(rec,question)">Deactivate</a></div></li>
 	</ul>
 		
 	<form class="form" style="margin-top:10px;">
 	    <div class="control-group">
 		    <label class="control-label" for="email">Add a Recommendation for this Question:</label>
 		    <div class="controls">
-		      <textarea rows="5" id="newRecommendation" cols="500" style="width:50%"></textarea>
+		      <textarea rows="5" id="newRecommendation" ng-model="question.newRecommendation" cols="500" style="width:50%"></textarea>
 		 </div>
 		 <a class="btn btn-large btn-success" ng-click="addRecommendation(question)">Add</a>
 	</form>
 
 	<h3 style="margin-top:30px;">Notes for this question:</h3>
 	<ul class="recommendationList listWithChecks sortable" id="sortable">
-		<li ng-repeat="obs in question.Observations">{{obs.Text}}<div class="checklistRow"><a class="btn  btn-danger deactivateRow">Deactivate</a></div></li>
+		<li ng-repeat="obs in question.Observations">{{obs.Text}}<div class="checklistRow"><a class="btn  btn-danger deactivateRow" ng-click="handleObjActive(obs,question)">Deactivate</a></div></li>
 	</ul>
 		
 	<form class="form" style="margin-top:10px;">
 	    <div class="control-group">
-		    <label class="control-label" for="email">Add a Recommendation for this Question:</label>
+		    <label class="control-label" for="email">Add a Note for this Question:</label>
 		    <div class="controls">
-		      <textarea rows="5" id="newRecommendation" cols="500" style="width:50%"></textarea>
+		      <textarea rows="5" id="newRecommendation" ng-model="question.newObservation" cols="500" style="width:50%"></textarea>
 		 </div>
 		 <a class="btn btn-large btn-success" ng-click="addObservation(question)">Add</a>
 	</form>
