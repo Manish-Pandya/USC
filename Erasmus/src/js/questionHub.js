bd1ace64aa21bea03121afdf7b337003404ec13e
Matthew Breeden <mbreeden@graysail.com>
2014-04-03 00:46:14 -0400

    add new question if checklist_id in get instead of question id.  link back to checklist hub

--- Erasmus/src/js/questionHub.js
+++ Erasmus/src/js/questionHub.js
@@ -5,6 +5,10 @@ function QuestionHubController($scope, $rootElement, $location, convenienceMetho
 	function init(){
 		if($location.search().id){
 			getQuestionById($location.search().id);
+
+		}else if($location.search().checklist_id){
+			$scope.noQuestion = true;
+			getChecklist($location.search().checklist_id);
 		}
 		$scope.newDeficiency = {};
 		$scope.newDeficiency.reference;
@@ -18,19 +22,36 @@ function QuestionHubController($scope, $rootElement, $location, convenienceMetho
 		$scope.doneLoading = false;
 		var url = '../../ajaxaction.php?action=getQuestionById&id='+id+'&callback=JSON_CALLBACK';
 		convenienceMethods.getData( url, onGetQuestion, onFailGetQuestion );
+
 	}
 
 	function onGetQuestion(data){
 		$scope.question = data;
-		$scope.doneLoading = true;
+		getChecklist(data.Checklist_id);
+		$scope.noQuestion = false;
+
 	}
 
 	function onFailGetQuestion(){
+		alert('There was a problem getting the question.');
+	}
 
+	function getChecklist(id){
+		var url = '../../ajaxaction.php?action=getChecklistById&id='+id+'&callback=JSON_CALLBACK';
+		convenienceMethods.getData( url, onGetChecklist, onFailGetChecklist );
+	}
+
+	function onGetChecklist(data){
+		$scope.checklist = data;
+		$scope.doneLoading = true;
+	}
+
+	function onFailGetChecklist(){
+		alert("There was a problem gettting this question's checklist.");
 	}
 
 	$scope.addDeficiency = function(question){
-
+		$scope.savingDeficiency = true;
 		console.log(question);
 
 		question.IsDirty = true;
@@ -52,17 +73,19 @@ function QuestionHubController($scope, $rootElement, $location, convenienceMetho
 	}
 
 	function onAddDef(def, question){
+		$scope.savingDeficiency = false;
 		question.Deficiencies.push(def);
 		question.IsDirty = false;
 	}
 
 	function onFailAddDef(){
+		$scope.savingDeficiency = false;
 		alert("There was a problem when attempting to add the deficiency.");
 	}
 
 	$scope.addObservation = function(question){
 		question.IsDirty = true;
-
+		$scope.savingObservation = true;
 		$scope.newObs = {
 			Class:  'Observation',
 			Is_active: true,
@@ -77,16 +100,18 @@ function QuestionHubController($scope, $rootElement, $location, convenienceMetho
 	}
 
 	function onAddObs(def, question){
+		$scope.savingObservation = false;
 		question.Observations.push(def);
 		question.IsDirty = false;
 	}
 
 	function onFailAddPbs(){
+		$scope.savingObservation = false;
 		alert("There was a problem when attempting to add the note.");
 	}
 	$scope.addRecommendation = function(question){
 		question.IsDirty = true;
-
+		$scope.savingRecommendation = true;
 		$scope.newRec = {
 			Class:  'Recommendation',
 			Is_active: true,
@@ -99,11 +124,13 @@ function QuestionHubController($scope, $rootElement, $location, convenienceMetho
 	}
 
 	function onAddRec(rec, question){
+		$scope.savingRecommendation = false;
 		question.Recommendations.push(rec);
 		question.IsDirty = false;
 	}
 
 	function onFailAddRec(){
+		$scope.savingRecommendation = false;
 		alert("There was a problem when attempting to add the recommendation.");
 	}
 
@@ -125,15 +152,18 @@ function QuestionHubController($scope, $rootElement, $location, convenienceMetho
         obj.IsDirty = false;
         obj.Invalid = false;
 	}
-	function onFailSetActive(){}
+	function onFailSetActive(){
+
+	}
 
 	function onSaveQuestion(dto, question){
 	     //temporarily use our question copy client side to bandaid server side bug that causes subquestions to be returned as indexed instead of associative
-        dto = angular.copy($scope.questionCopy);
-        convenienceMethods.setPropertiesFromDTO( dto, question );
-        question.isBeingEdited = false;
-        question.IsDirty = false;
+        convenienceMethods.setPropertiesFromDTO( $scope.questionCopy, $scope.question );
+        $scope.question.isBeingEdited = false;
+        $scope.question.IsDirty = false;
+        $scope.questionCopy.IsDirty = false;
         question.Invalid = false;
+        $scope.noQuestion = false;
 	}
 
 	function onFailSaveQuestion(){
@@ -145,9 +175,20 @@ function QuestionHubController($scope, $rootElement, $location, convenienceMetho
 		$scope.questionCopy = angular.copy($scope.question);
 	}
 	$scope.saveEditedQuestion = function(question){
+		$scope.questionCopy.IsDirty = true;
 		var url = '../../ajaxaction.php?action=saveQuestion';
         convenienceMethods.updateObject( $scope.questionCopy, question, onSaveQuestion, onFailSaveQuestion, url );
 	}
+
+	$scope.$watch('checklist', function(oldValue, newValue){
+     	if($scope.checklist){
+     		$scope.questionCopy = {
+     			Class: "Question",
+     			Checklist_id: $scope.checklist.Key_id,
+     			Order_index: 0
+     		}
+     	}
+  	}, true);
 }
 
 questionHub.controller('QuestionHubController',QuestionHubController);
