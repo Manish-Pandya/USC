5f44a95018ecbd21314f4f83a953663ecefa52ab
Matthew Breeden <mbreeden@graysail.com>
2014-06-04 17:38:01 -0400

    added position property to user class.  Added to database as varchar(255)

--- Erasmus/src/includes/classes/User.php
+++ Erasmus/src/includes/classes/User.php
@@ -9,23 +9,24 @@ include_once 'GenericCrud.php';
  * @author Mitch Martin, GraySail LLC
  */
 class User extends GenericCrud{
-	
+
 	// CRUD Meta-Data
 	/** Name of the DB Table */
 	protected static $TABLE_NAME = "erasmus_user";
-	
+
 	/** Key/Value Array listing column names mapped to their types */
 	protected static $COLUMN_NAMES_AND_TYPES = array(
 		//roles are a relationship
 		"username"	=> "text",
 		"name"		=> "text",
-		"email"		=> "text", 
+		"email"		=> "text",
 		"lab_phone"      => "text",
 		"office_phone"	=>  "text",
 		"supervisor_id"		=> "integer",
 		"emergency_phone" => "text",
 		"primary_department_id" => "integer",
-							
+		"position" => "text",
+
 		//GenericCrud
 		"key_id"			=> "integer",
 		"date_created"		=> "timestamp",
@@ -33,14 +34,14 @@ class User extends GenericCrud{
 		"is_active"			=> "boolean",
 		"last_modified_user_id"			=> "integer"
 	);
-	
+
 	/** Relationships */
 	public static $ROLES_RELATIONSHIP = array(
 		"className"	=>	"Role",
 		"tableName"	=>	"user_role",
 		"keyName"	=>	"role_id",
 		"foreignKeyName"	=>	"user_id"
-	); 
+	);
 
 	protected static $PI_RELATIONSHIP = array(
 			"className"	=>	"PrincipalInvestigator",
@@ -55,45 +56,45 @@ class User extends GenericCrud{
 			"keyName"	=>	"key_id",
 			"foreignKeyName"	=>	"user_id"
 	);
-		
+
 	// Access information
-	
+
 	/** Array of roles */
 	private $roles;
-	
+
 	/** Optional Related PI record (if this user is a PI) */
 	private $principalInvestigator;
-	
+
 	/** Optional Related Inspector record (if this user is a Inspector) */
 	private $inspector;
-	
+
 	/** Supervisor Principal Investigator (if this user works for a PI */
 	private $supervisor;
 	private $supervisor_id;
-	
+
 	// General User Info
-	
+
 	/** System Name for this User */
 	private $username;
-	
+
 	/** 'Real' name of this User */
 	private $name;
-	
+
 	/** Email address of this User */
 	private $email;
-	
+
 	/** lab, emergency and office phone numbers of this user, if this user has any or all of them **/
 	private $lab_phone;
 	private $emergency_phone;
 	private $office_phone;
-	
+
 	/** this user's primary department.  Lab Contacts have a single department **/
 	private $primary_department_id;
 	private $primary_department;
-	
+
 	// Constructor(s)
 	public function __construct(){
-		
+
 		// Define which subentities to load
 		$entityMaps = array();
 		$entityMaps[] = new EntityMap("lazy","getPrincipalInvestigator");
@@ -102,20 +103,20 @@ class User extends GenericCrud{
 		$entityMaps[] = new EntityMap("eager","getRoles");
 		$entityMaps[] = new EntityMap("eager","getEmergency_phone");
 		$this->setEntityMaps($entityMaps);
-		
+
 	}
-	
+
 	// Required for GenericCrud
 	public function getTableName(){
 		return self::$TABLE_NAME;
 	}
-	
+
 	public function getColumnData(){
 		return self::$COLUMN_NAMES_AND_TYPES;
 	}
-	
+
 	// Accessors / Mutators
-	public function getRoles(){ 
+	public function getRoles(){
 		if($this->roles === NULL && $this->hasPrimaryKeyValue()) {
 			$thisDAO = new GenericDAO($this);
 			$this->roles = $thisDAO->getRelatedItemsById($this->getKey_id(), DataRelationship::fromArray(self::$ROLES_RELATIONSHIP));
@@ -123,8 +124,8 @@ class User extends GenericCrud{
 		return $this->roles;
 	}
 	public function setRoles($roles){ $this->roles = $roles; }
-	
-	public function getPrincipalInvestigator(){ 
+
+	public function getPrincipalInvestigator(){
 		if($this->principalInvestigator === NULL && $this->hasPrimaryKeyValue()) {
 			$thisDAO = new GenericDAO($this);
 			$piArray = $thisDAO->getRelatedItemsById($this->getKey_id(), DataRelationship::fromArray(self::$PI_RELATIONSHIP));
@@ -133,9 +134,9 @@ class User extends GenericCrud{
 		return $this->principalInvestigator;
 	}
 	public function setPrincipalInvestigator($principalInvestigator){ $this->principalInvestigator = $principalInvestigator; }
-	
 
-	public function getInspector(){ 
+
+	public function getInspector(){
 		if($this->inspector === NULL && $this->hasPrimaryKeyValue()) {
 			$thisDAO = new GenericDAO($this);
 			$inspectorArray = $thisDAO->getRelatedItemsById($this->getKey_id(), DataRelationship::fromArray(self::$INSPECTOR_RELATIONSHIP));
@@ -144,10 +145,10 @@ class User extends GenericCrud{
 		return $this->inspector;
 	}
 	public function setInspector($inspector){ $this->inspector = $inspector; }
-	
+
 	public function getSupervisor_id(){ return $this->supervisor_id; }
 	public function setSupervisor_id($id){ $this->supervisor_id = $id; }
-	
+
 	public function getSupervisor() {
 		if($this->supervisor === NULL && $this->hasPrimaryKeyValue()) {
 			$superDAO = new GenericDAO(new PrincipalInvestigator());
@@ -158,16 +159,16 @@ class User extends GenericCrud{
 	public function setSupervisor($supervisor) {
 		$this->supervisor = $supervisor;
 	}
-	
+
 	public function getUsername(){ return $this->username; }
 	public function setUsername($username){ $this->username = $username; }
-	
+
 	public function getName(){ return $this->name; }
 	public function setName($name){ $this->name = $name; }
-	
+
 	public function getEmail(){ return $this->email; }
 	public function setEmail($email){ $this->email = $email; }
-		
+
 	public function getEmergency_phone(){ return $this->emergency_phone; }
 	public function setEmergency_phone($Emergency_phone){ $this->emergency_phone = $Emergency_phone; }
 
@@ -176,10 +177,10 @@ class User extends GenericCrud{
 
 	public function getOffice_phone(){ return $this->office_phone; }
 	public function setOffice_phone($office_phone){ $this->office_phone = $office_phone;}
-	
+
 	public function getPrimary_department_id(){ return $this->primary_department_id;}
 	public function setPrimary_department_id($primary_department_id){ $this->primary_department_id = $primary_department_id; }
-	
+
 	public function getPrimary_department() {
 		if($this->primary_department === NULL && $this->hasPrimaryKeyValue()) {
 			$deptartmentDAO = new GenericDAO(new Department());
