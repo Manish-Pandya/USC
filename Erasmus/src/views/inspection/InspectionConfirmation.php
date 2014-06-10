ef7fab29ccf2873be210335adc3cca6adc3eaed2
Matthew Breeden <mbreeden@graysail.com>
2014-06-10 11:19:53 -0400

    Now loads postInspectionRefactor.js

--- Erasmus/src/views/inspection/InspectionConfirmation.php
+++ Erasmus/src/views/inspection/InspectionConfirmation.php
@@ -1,8 +1,9 @@
 <?php
 require_once '../top_view.php';
 ?>
-<script src="../../js/postInspection.js"></script>
+<script src="../../js/postInspectionRefactor.js"></script>
 <div ng-app="postInspections" ng-controller="mainController">
+{{Inspection}}
 <div class="navbar">    		
 
 	<ul class="nav pageMenu" style="min-height: 50px; background: #d00; color:white !important; padding: 2px 0 2px 0; width:100%">
