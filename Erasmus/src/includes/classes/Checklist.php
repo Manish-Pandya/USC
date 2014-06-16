5a6a6bce51f84de1956c55ce5f476876643bac15
Matthew Breeden <mbreeden@graysail.com>
2014-06-16 15:46:02 -0400

    Fixed typo.  ($ ruins everything around me)

--- Erasmus/src/includes/classes/Checklist.php
+++ Erasmus/src/includes/classes/Checklist.php
@@ -6,10 +6,10 @@
  * @author Hoke Currie, GraySail LLC
  */
 class Checklist extends GenericCrud {
-	
+
 	/** Name of the DB Table */
 	protected static $TABLE_NAME = "checklist";
-	
+
 	/** Key/Value Array listing column names mapped to their types */
 	protected static $COLUMN_NAMES_AND_TYPES = array(
 		"name"	=>	"text",
@@ -25,7 +25,7 @@ class Checklist extends GenericCrud {
 		"is_active"			=> "boolean",
 		"last_modified_user_id"			=> "integer"
 	);
-	
+
 	/** Relationships */
 	protected static $QUESTIONS_RELATIONSHIP = array(
 			"className"	=>	"Question",
@@ -33,52 +33,52 @@ class Checklist extends GenericCrud {
 			"keyName"	=>	"key_id",
 			"foreignKeyName"	=>	"checklist_id"
 	);
-	
+
 	/** Name of this Checklist */
 	private $name;
-	
+
 	/** The Hazard entity to which this Checklist applies */
 	private $hazard;
 	private $hazard_id;
-	
+
 	/** The name of the master hazard category assigned to this checklist's hazard, e.g. Chemical, Biological Radiological */
 	private $master_hazard;
-	
-	
+
+
 	/** Array of Question entities that comprise this Checklist */
 	private $questions;
-	
+
 	/** Non-persisted value used to filter Responses for a particular inspection */
 	private $inspectionId;
-	
+
 	public function __construct(){
-	
+
 		// Define which subentities to load
 		$entityMaps = array();
 		$entityMaps[] = new EntityMap("lazy","getHazard");
 		$entityMaps[] = new EntityMap("eager","getQuestions");
 		$this->setEntityMaps($entityMaps);
-		
+
 	}
-	
+
 	// Required for GenericCrud
 	public function getTableName(){
 		return self::$TABLE_NAME;
 	}
-	
+
 	public function getColumnData(){
 		return self::$COLUMN_NAMES_AND_TYPES;
 	}
-	
+
 	public function getHazard(){
 		if($this->hazard == null) {
 			$hazardDAO = new GenericDAO("Hazard");
 			$this->hazard = $hazardDAO->getById($this->hazard_id);
 		}
-		return $this->hazard; 
+		return $this->hazard;
 	}
 	public function setHazard($hazard){
-		$this->hazard = $hazard; 
+		$this->hazard = $hazard;
 	}
 
 	public function getHazard_id(){ return $this->hazard_id; }
@@ -87,7 +87,7 @@ class Checklist extends GenericCrud {
 	public function getName(){ return $this->name; }
 	public function setName($name){ $this->name = $name; }
 
-	public function getMaster_hazard(){ return $this->$master_hazard; }
+	public function getMaster_hazard(){ return $this->master_hazard; }
 	public function setMaster_hazard($master_hazard){ $this->master_hazard = $master_hazard; }
 
 	public function getQuestions(){
@@ -99,12 +99,12 @@ class Checklist extends GenericCrud {
 
 	public function getInspectionId(){ return $this->inspectionId; }
 	public function setInspectionId($inspectionId){ $this->inspectionId = $inspectionId; }
-	
+
 	private function filterQuestionsForInspection($questions){
 
 		$LOG = Logger::getLogger( 'Action:' . __FUNCTION__ );
 		$LOG->debug("about to init ".  count($questions) . " Question objects with inspection filter info.");
-		
+
 		if(!empty($this->inspectionId)) {
 			$LOG->debug("Inspection Id " . $this->inspectionId . " found.");
 			// Define which subentities to load
@@ -114,7 +114,7 @@ class Checklist extends GenericCrud {
 			$entityMaps[] = new EntityMap("eager","getRecommendations");
 			$entityMaps[] = new EntityMap("eager","getObservations");
 			$entityMaps[] = new EntityMap("eager","getResponses");
-			
+
 			foreach ($questions as $question){
 				$question->setInspectionId($this->inspectionId);
 				$question->setEntityMaps($entityMaps);
@@ -122,6 +122,6 @@ class Checklist extends GenericCrud {
 		}
 		return $questions;
 	}
-	
+
 }
 ?>
