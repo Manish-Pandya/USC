067f1e9b1c430cbfd5c7795483b8c5a7a9acec63
Matthew Breeden <mbreeden@graysail.com>
2014-07-16 13:37:38 -0400

    Added max height css for modal dialogues

--- Erasmus/src/js/modalPosition.js
+++ Erasmus/src/js/modalPosition.js
@@ -1,13 +1,24 @@
 //This module provides a directive to calculate the height of a bootstrap modal and position it accordingly, vertically
 angular.module('modalPosition', [])
 
-.directive('modal', function($window) {
+.directive('modal', ['$window', function($window) {
     return {
-        restrict : 'C',
-        link : function(scope, element, attributes) {
-            var topMargin = $window.innerHeight - element[0].clientHeight;
-            console.log(topMargin/2);
-            $(element[0]).css({top: topMargin/2, marginTop:0});
-      }
+	    restrict : 'C',
+	    link : function(scope, element, attributes) {
+	    	scope.onResize = function() {
+	            var topMargin = $window.innerHeight - element[0].clientHeight;
+                $(element[0]).css({maxHeight: $window.innerHeight*.9});	            
+                $(element[0]).css({top: topMargin/2, marginTop:-10, overflowY:'auto'});
+        	}
+        	scope.onResize();
+
+            angular.element($(element[0])).bind('DOMNodeInserted', function() {
+                scope.onResize();
+            })
+
+            angular.element($window).bind('resize', function() {
+                scope.onResize();
+            });
+      	}
     }
- })
+ }]);
