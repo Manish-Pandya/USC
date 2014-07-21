11bff8160f6d1fe3e831746dbceb3c8ed63c6a5a
Perry <thescratcherman@gmail.com>
2014-07-21 17:00:08 -0400

    fixed undefined method errors when ActionError passed instead of another class

--- Erasmus/src/includes/classes/ActionError.php
+++ Erasmus/src/includes/classes/ActionError.php
@@ -3,8 +3,12 @@
 class ActionError {
 	public $message;
 	
+	private $LOG;
+
 	public function __construct( $message ){
 		$this->message = $message;
+		$this->LOG = Logger::getLogger( __CLASS__ );
+
 	}
 	
 	public function __toString(){
@@ -13,6 +17,15 @@ class ActionError {
 	
 	public function getMessage(){ return $this->message; }
 	public function setMessage($m){ $this->message = $m; }
+
+	// prevent any undefined method exceptions if ActionError is passed to a
+	// function that expects another type of object.
+	public function __call($method, $args) {
+		if(!isset($this->$method)) {
+			$this->LOG->error('Method ' . $method . ' does not exist on object of type ' . get_class());
+			return NULL;
+		}
+	}
 }
 
 ?>
