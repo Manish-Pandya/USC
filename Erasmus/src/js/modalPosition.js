e0503ec618af072e634fdf4a65eaabbd63b6ff83
Matthew Breeden <mbreeden@graysail.com>
2014-07-22 16:24:36 -0400

    Fix to caculation of the height of modal-body elements

--- Erasmus/src/js/modalPosition.js
+++ Erasmus/src/js/modalPosition.js
@@ -8,7 +8,8 @@ angular.module('modalPosition', [])
 	    	scope.onResize = function() {
 	            var topMargin = $window.innerHeight - element[0].clientHeight;
                 $(element[0]).css({maxHeight: $window.innerHeight*.9});	            
-                $(element[0]).css({top: topMargin/2, marginTop:-10, overflowY:'auto'});
+                $(element[0]).css({top: topMargin/2, marginTop:-10, overflowY:'hidden'});
+                $(element[0]).find('.modal-body').css({overflowY:'auto', maxHeight:$window.innerHeight*.85-30});
         	}
         	scope.onResize();
 
