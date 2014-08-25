606aba428840fb4f1e007566009d8ef025eef85e
perry <thescratcherman@gmail.com>
2014-08-25 18:20:01 -0400

    added Carboy entity for tracking the container's commission and retirement dates

---
+++ Erasmus/src/includes/classes/Carboy.php
@@ -0,0 +1,55 @@
+<?php
+
+include_once 'GenericCrud.php';
+
+/**
+ * 
+ * 
+ * 
+ * @author Perry Cate, GraySail LLC
+ */
+class Carboy extends GenericCrud {
+
+	/** Name of the DB Table */
+	protected static $TABLE_NAME = "carboy";
+	
+	/** Key/Value array listing column names and their types */
+	protected static $COLUMN_NAMES_AND_TYPES = array(
+		"commission_date"				=> "DateTime",
+		"retirement_date"				=> "DateTime",
+		
+		//GenericCrud
+		"key_id"						=> "integer",
+		"is_active"						=> "boolean",
+		"date_last_modified"			=> "timestamp",
+		"last_modified_user_id"			=> "integer",
+		"date_created"					=> "timestamp",
+		"created_user_id"				=> "integer"
+	);
+	
+	//access information
+	
+	/** DateTime (not to be confused with timestamp) with the date this carboy was made */
+	private $commission_date;
+	
+	/** DateTime with the date this carboy should be thrown away */
+	private $retirement_date;
+
+	// Required for GenericCrud
+	public function getTableName() {
+		return self::$TABLE_NAME;
+	}
+	
+	public function getColumnData() {
+		return self::$COLUMN_NAMES_AND_TYPES;
+	}
+	
+	// Accessors / Mutators
+	public function getCommission_date() { return $this->commission_date; }
+	public function setCommission_date($newDate) { $this->commission_date = $newDate; }
+	
+	public function getRetirement_date() { return $this->retirement_date; }
+	public function setRetirement_date($newDate) { $this->retirement_date = $newDate; }
+
+}
+?>
