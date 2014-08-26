c35078e550d3e52f0d8c2f72309e6b65265b8af9
perry <thescratcherman@gmail.com>
2014-08-26 17:08:38 -0400

    added PurchaseOrder entity

---
+++ Erasmus/src/includes/classes/PurchaseOrder.php
@@ -0,0 +1,78 @@
+<?php
+
+include_once 'GenericCrud.php';
+
+/**
+ * 
+ * 
+ * 
+ * @author Perry Cate, GraySail LLC
+ */
+class PurchaseOrder extends GenericCrud {
+
+	/** Name of the DB Table */
+	protected static $TABLE_NAME = "purchase_order";
+	
+	/** Key/Value array listing column names and their types */
+	protected static $COLUMN_NAMES_AND_TYPES = array(
+		"principal_investigator_id"		=> "integer",
+		"purchase_order_number"			=> "text", // The "number" may also contain letters.
+		
+		//GenericCrud
+		"key_id"						=> "integer",
+		"is_active"						=> "boolean",
+		"date_last_modified"			=> "timestamp",
+		"last_modified_user_id"			=> "integer",
+		"date_created"					=> "timestamp",
+		"created_user_id"				=> "integer"
+	);
+	
+	//access information
+
+	/** integer containing the id of the principal investigator making this order. */
+	private $principal_investigator_id;
+	
+	/** reference to the principal investigator who made this order */
+	private $principal_investigator;
+	
+	/** alphanumeric string to represent identifiable purchase "number" */
+	private $purchase_order_number;
+	
+	
+	public function __construct() {
+		
+		// Define which subentities to load
+		$entityMaps = array();
+		$entityMaps[] = new EntityMap("lazy", "getPrincipalInvestigator");
+
+	}
+	
+	// Required for GenericCrud
+	public function getTableName() {
+		return self::$TABLE_NAME;
+	}
+	
+	public function getColumnData() {
+		return self::$COLUMN_NAMES_AND_TYPES;
+	}
+	
+	// Accessors / Mutators
+	public function getPrincipal_investigator_id() { return $this->principal_investigator_id; }
+	public function setPrincipal_investigator_id($newId) { $this->principal_investigator_id = $newId; }
+	
+	public function getPrincipal_investigator() {
+		if($this->principal_investigator == null) {
+			$principalInvestigatorDAO = new GenericDAO(new PrincipalInvestigator());
+			$this->principal_investigator = $principalInvestigatorDAO->getById($this->getPrincipal_investigator_id());
+		}
+		return $this->principal_investigator;
+	}
+	public function setPrincipal_investigator($newPI) {
+		$this->principal_investigator = $newPI;
+	}
+	
+	public function getPurchase_order_number() { return $this->purchase_order_number; }
+	public function setPurchase_order_number($newNumber) { $this->purchase_order_number = $newNumber; }
+	
+}
+?>
