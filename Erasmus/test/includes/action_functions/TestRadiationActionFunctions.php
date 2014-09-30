0b08862482aaf82dcf9a6377fefc88f55f5c1f91
perry <thescratcherman@gmail.com>
2014-09-30 17:38:37 -0400

    fixed some whitespace and comment issues, and added extra assertion I forgot

--- Erasmus/test/includes/action_functions/TestRadiationActionFunctions.php
+++ Erasmus/test/includes/action_functions/TestRadiationActionFunctions.php
@@ -13,13 +13,14 @@ require_once(dirname(__FILE__) . '/../../../src/includes/action_functions.php');
 
 /*
 	IMPORTANT: Until I can find a different way, most of these tests are dependent on
-	entities having a record in the database with key_id = 1
+	entities having a record in the database with the following key id:
 */
+define("KEY_ID", 1);
 
 
 class TestRadiationActionFunctions extends UnitTestCase {
 	
-	// Reset $_REQUEST between tests so that tests aren't affected by previous ones.
+	// Reset $_REQUEST between tests so that tests using $_REQUEST don't affect each other
 	function tearDown() {
 		foreach( $_REQUEST as $key=>$value ) {
 			unset( $_REQUEST[$key] );
@@ -29,31 +30,35 @@ class TestRadiationActionFunctions extends UnitTestCase {
 	// getIsotopeById
 	public function test_getIsotopeById_noId() {
 		$isotope = getIsotopeById();
-		$this->assertTrue( $isotope instanceof ActionError);
+		$this->assertTrue( $isotope instanceof ActionError );
 	}
 	public function test_getIsotopeById_passId() {
-		$isotope = getIsotopeById(1);
-		$this->assertTrue( $isotope instanceof Isotope);
+		$isotope = getIsotopeById( KEY_ID );
+		$this->assertTrue( $isotope instanceof Isotope );
+		$this->assertEqual( $isotope->getKey_id(), KEY_ID );
 	}
 	public function test_getIsotopeById_requestId() {
-		$_REQUEST['id'] = 1;
+		$_REQUEST['id'] = KEY_ID;
 		$isotope = getIsotopeById();
-		$this->assertTrue( $isotope instanceof Isotope);
+		$this->assertTrue( $isotope instanceof Isotope );
+		$this->assertEqual( $isotope->getKey_id(), KEY_ID );
 	}
 	
 	// getCarboyById
 	public function test_getCarboyById_noId() {
 		$carboy = getCarboyById();
-		$this->assertTrue( $carboy instanceof ActionError);
+		$this->assertTrue( $carboy instanceof ActionError );
 	}
 	public function test_getCarboyById_passId() {
-		$carboy = getCarboyById(1);
-		$this->assertTrue( $carboy instanceof Carboy);
+		$carboy = getCarboyById( KEY_ID );
+		$this->assertTrue( $carboy instanceof Carboy );
+		$this->assertEqual( $carboy->getKey_id(), KEY_ID );
 	}
 	public function test_getCarboyById_requestId() {
 		$_REQUEST['id'] = 1;
 		$carboy = getCarboyById();
-		$this->assertTrue( $carboy instanceof Carboy);
+		$this->assertTrue( $carboy instanceof Carboy );
+		$this->assertEqual( $carboy->getKey_id(), KEY_ID );
 	}
 	
 }
