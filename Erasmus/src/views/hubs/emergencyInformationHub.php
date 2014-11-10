f96eeab84ff503492dab94a0a2e4d51478fe19fc
Matthew Breeden <Matthew Breeden@MatthewBreeden>
2014-11-10 18:10:59 -0500

    Updated view per Mark Robbins' layout.

--- Erasmus/src/views/hubs/emergencyInformationHub.php
+++ Erasmus/src/views/hubs/emergencyInformationHub.php
@@ -3,11 +3,11 @@ require_once '../top_view.php';
 ?>
 <script src="../../js/emergencyInfoHub.js"></script>
 
-<span id="buildingHub"  ng-app="emergencyInfo" >
+<span id="buildingHub"  ng-app="emergencyInfo" ng-controller="emergencyInfoController">
 	<div class="navbar">
-		<ul class="nav pageMenu row-fluid orangeBg">
+		<ul class="nav pageMenu row-fluid" style="background:#002060;">
 			<li class="span12">			
-				<h2 style="padding: 11px 0 5px 0; font-weight:bold; text-align:center">
+				<h2 style="padding: 11px 0 5px 0; font-weight:bold;">
 					<img src="../../img/hazard-icon.png"  style="height:50px" />
 					Emergency Information
 					<a style="float:right;margin: 11px 28px 0 0;" href="../RSMSCenter.php"><i class="icon-home" style="font-size:40px;"></i></a>	
@@ -16,65 +16,82 @@ require_once '../top_view.php';
 		</ul>
 		<div style=>&nbsp;</div>
 	</div>
-	<div class="whiteBg" style="margin-top:-40px;">
-		<span ng-controller="emergencyInfoController" id="emergency-info">
+	<div class="whiteBg" style="margin-top:-40px; padding-bottom:15px !important;">
+		<span id="emergency-info">
+
+			<div class="center">
+				<a class="btn btn-info" ng-click="searchType = 'location'"><h2>Search by Location</h2></a>
+				<a class="btn btn-info" ng-click="searchType = 'pi'"><h2>Search by Principal Investigator</h2></a>
+			</div>
 			<div id="buildings">
 				<form class="row form-inline" style="margin-left:0">
-					<label>Building</label>
-					<input ng-if="Buildings" style="" type="text" typeahead-on-select='onSelectBuilding($item, $model, $label)' ng-model="selectedBuilding" placeholder="Select a Building" typeahead="building as building.Name for building in Buildings | filter:$viewValue">
-						<input ng-if="!Buildings" style="" type="text" disabled="disabled" placeholder="Getting buildings...">
-			       	<img ng-if="!Buildings" class="" style="height: 23px; margin: 3px 0 0 -60px; position: absolute;" src="<?php echo WEB_ROOT?>img/loading.gif"/>
-					<label>Room</label>
-					<input ng-if="building" style="" type="text" typeahead-on-select='onSelectRoom($item, $model, $label)' ng-model="selectedRoom" placeholder="Select a Room" typeahead="rooms as room.Name for room in building.Rooms | filter:$viewValue2">
-					<input ng-if="!building" placeholder="Select a Building" disabled="disabled">
+					<span ng-if="searchType == 'location'">
+						<label>Building Name or Physical Address:</label>
+						<input ng-if="buildings" style="width:350px" type="text" typeahead-on-select='eif.onSelectPIOrBuilding($item)' ng-model="selectedBuilding" placeholder="Select a Building" typeahead="building as building.Name for building in buildings | filter:$viewValue">
+						<input ng-if="!buildings" style="width:350px" type="text" disabled="disabled" placeholder="Getting buildings...">
+				       	<img ng-if="!buildings" class="" style="height: 23px; margin: 15px 0 0 -44px; position: absolute;" src="<?php echo WEB_ROOT?>img/loading.gif"/>
+
+						<label>Room:</label>
+						<input ng-if="rooms" style="" type="text" typeahead-on-select='onSelectRoom($item)' ng-model="selectedRoom" placeholder="Select a Room" typeahead="room as room.roomText for room in rooms | filter:$viewValue">
+						<input ng-if="!rooms" placeholder="Select a Building" disabled="disabled">
+				    </span>
+
+				    <span ng-if="searchType == 'pi'">
+						<label>Principal Investigator:</label>
+						<input ng-if="pis" style="width:280px" type="text" typeahead-on-select='eif.onSelectPIOrBuilding($item)' ng-model="selectedPi" placeholder="Select a Principal Investigator" typeahead="pi as (pi.User.Name) for pi in pis | filter:$viewValue">
+						<input ng-if="!pis" style="width:280px" type="text" disabled="disabled" placeholder="Getting Principal Investigators...">
+				       	<img ng-if="!pis" class="" style="height: 23px; margin: 15px 0 0 -44px; position: absolute;" src="<?php echo WEB_ROOT?>img/loading.gif"/>
+					
+						<label>Location:</label>
+						<input ng-if="rooms" style="width:350px" type="text" typeahead-on-select='onSelectRoom($item)' ng-model="selectedRoom" placeholder="Select a Room" typeahead="room as room.roomText for room in rooms | filter:$viewValue">
+						<input ng-if="!rooms" style="width:350px" placeholder="Select a Principal Investigator" disabled="disabled">
+					</span>
 
 				</form>
-				<h2 class="alert alert-danger" ng-if="error">{{error}}</h2>
-				<span ng-if="building">
-				<h2 ng-if="building && !room">Select a room to display EMERGENCY INFORMATION for {{building.Name}}</h2>
-				<div class="roomDisplay" ng-if="room">
-					<h2>Room {{room.Name}}</h2>
-					
-					<h3 style="text-decoration:underline; margin-top:20px;">EMERGENCY CONTACTS</h3>
-					<h4 ng-if="!room.PrincipalInvestigators.length">No Principal Investigators have been assigned to room {{room.Name}}</h4>
-					<ul>
-						<li style="margin:10px auto; width:500px; list-style:none" class="lefterizer row" ng-repeat="pi in room.PrincipalInvestigators">
-							<span style="width:200px; float:left; text-decoration:underline;">Principal Investigator:</span> <span>{{pi.User.Name}}</span><br>
-							<span ng-if="pi.User.Emergency_phone" style="width:150px; float:left; text-decoration:underline;">Emergency Phone: </span><span style="width:265px; margin-left:-16px;">{{pi.User.Emergency_phone}}</span><br>
-							<span style="float:left; width:160px; text-decoration:underline;">Department(s)</span><span><ul><li class="offset2" ng-repeat="department in pi.Departments">{{department.Name}}</li></ul></span>
-						</li>
+					<span ng-if="loading && !error" class="loading">
+					   <img style="width:100px"src="<?php echo WEB_ROOT?>img/loading.gif"/>
+					  Loading...
+					</span>
 
-					</ul>
+				<table ng-if="hazards && pisByRoom" class="table table-striped pisTable">
+					<tr class="blue-tr">
+						<th>Principal Investigator</th>
+						<th>Emergency Phone</th>
+						<th>PI Department</th>
+					</tr>
+					<tr ng-repeat="pi in pisByRoom">
+						<td>{{pi.User.Name}}</td>
+						<td><span ng-if="pi.User.Emergency_phone">{{pi.User.Emergency_phone}}</span></td>
+						<td>
+							<ul>
+								<li ng-repeat="dept in pi.Departments">{{dept.Name}}</li>
+							</ul>
+						</td>
+					</tr>
+				</table>
+				<table ng-if="hazards && pisByRoom" class="table table-striped pisTable" style="max-width:700px;">
+					<tr class="blue-tr">
+						<th>Lab Personnel Contacts</th>
+						<th>Emergency Phone</th>
+					</tr>
+					<tr ng-repeat="contact in personnel">
+						<td>{{contact.Name}}</td>
+						<td>{{contact.Emergency_phone}}</td>
+					</tr>
+				</table>
 
-					<h3 ng-if="room.PrincipalInvestigators.length && room.PrincipalInvestigators[0] && room.PrincipalInvestigators[0].LabPersonnel.length" style="text-decoration:underline; margin-top:20px;">Laboratory Safety Contact(s):</h3>
-					<li style="margin:10px auto; width:700px; list-style:none;"  class="lefterizer row" ng-repeat="pi in room.PrincipalInvestigators">
-						<ul>
-							<li ng-repeat="contact in pi.LabPersonnel" style="text-align:left; list-style:none"><span><span  style="text-decoration:underline;">Name:</span><span style="width: 200px; display: inline-block;
-margin-left: 10px;">{{contact.Name}}</span><span style="text-decoration:underline;">Emergency Phone</span><span style="width: 200px; display: inline-block; margin-left: 10px;">{{contact.Emergency_phone}}</span></li>
-						</ul>
-					</li>
-
-
-					<div ng-if="gettingHazards && room.PrincipalInvestigators.length" class="container loading" style="margin-top:20px;">
-				      <img class="" src="../../img/loading.gif"/>
-				      Building Hazard List...
-				    </div>
-
-			    <h2 ng-show="numberOfHazardsPresent > 0" style=" margin-top:20px; text-decoration:underline">LABORATORY HAZARDS</h2>
-			    <h4 ng-if="numberOfHazardsPresent < 1 && !gettingHazards">There are no hazards in the system for {{building.Name}}, room {{room.Name}}</h4>
-				<ul class="allHazardList" style="width:960px; margin:30px auto;">
-					<li class="hazardList" ng-if="numberOfHazardsPresent > 0" ng-class="{narrow: hazard.hidden}" data-ng-repeat="hazard in hazards" style="width:350px;">
-					<h1 class="hazardListHeader" id="{{hazard.cssId}}" ng-show="hazard.hidden" ng-click="hazard.hidden = !hazard.hidden">&nbsp;</h1>
-					<span ng-hide="hazard.hidden">
-				    <h1 ng-click="hazard.hidden = !hazard.hidden" class="hazardListHeader" id="{{hazard.cssId}}">{{hazard.Name}}</h1>
-
-					<ul ng-show="hazard.ActiveSubHazards">
-						<div ng-include="'EmergencyInfoList.php'" ng-init="SubHazards = hazard.ActiveSubHazards"></div>
-		    		</ul>
-					</li>
-					<div style='clear:both'>&nbsp;</div>
-				</ul>
-				<div style='clear:both'>&nbsp;</div>					
-	</div>		
+			</div>
+		</div>
+		<h1 class="hazardHeader" ng-if="hazards && pisByRoom">LABORATORY HAZARDS</h1>
+		<ul class="modalHazardList">
+			<li ng-if="hazards && pisByRoom" data-ng-repeat="hazard in hazards" class="modalHazard{{hazard.Key_id}}">
+				<h1>{{hazard.Name}}</h1>
+				<ul ng-if="hazard.ActiveSubHazards">
+					<div ng-include="'EmergencyInfoList.php'" ng-init="SubHazards = hazard.ActiveSubHazards"></div>
+	    		</ul>
+			</li>
+			<div style='clear:both'>&nbsp;</div>
+		</ul>				
+		
 	</span>
 </span>
