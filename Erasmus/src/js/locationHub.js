cf9190545ed8cc82e7adec0da5dfdf88507b7316
Matthew Breeden <mbreeden@graysail.com>
2014-12-03 00:15:00 -0500

    Fixed lazy load bug in factory's getCampuses method

--- Erasmus/src/js/locationHub.js
+++ Erasmus/src/js/locationHub.js
@@ -1,75 +1,3 @@
-<<<<<<< HEAD
-var piHub = angular.module('locationHub', ['ui.bootstrap','convenienceMethodModule'])
-
-.config(function($routeProvider){
-  $routeProvider
-    .when('/rooms', 
-      {
-        templateUrl: 'locationHubPartial/rooms.html', 
-        controller: roomsCtrl
-      }
-    )
-    .when('/personnel', 
-      {
-        templateUrl: 'locationHubPartial/buildings.html', 
-        controller: buildingsCtrl
-      }
-    )   
-    .when('/departments', 
-      {
-        templateUrl: 'locationHubPartial/campuses.html', 
-        controller: campusesCtrl
-      }
-    )
-    .otherwise(
-      {
-        redirectTo: '/rooms'
-      }
-    );
-})
-
-.factory('piHubFactory', function(convenienceMethods,$q){
-  var factory = {};
-  
-  factory.createRoom = function(roomDto){
-    var url = "../../ajaxaction.php?action=saveRoom";
-    var deferred = $q.defer();
-    convenienceMethods.saveDataAndDefer(url, roomDto).then(
-      function(promise){
-        deferred.resolve(promise);
-      },
-      function(promise){
-        deferred.reject();
-      }
-    );  
-    return deferred.promise
-  }
-
-  return factory;
-});
-
-routeController = function($scope, $rootScope, $location, convenienceMethods, $modal, piHubFactory){
-
-  $scope.setRoute = function(route)
-  {
-    $scope.route = route;
-    $location.path(route);
-  }
-
-}
-
-roomsCtrl = function($scope, convenienceMethods, $modal, piHubFactory){
-
-
-}
-
-buildingsCtrl = function($scope, $rootScope, $location, convenienceMethods, $modal, piHubFactory){
-
-}
-
-campusesCtrl = function($scope, $rootScope, $location, convenienceMethods, $modal, piHubFactory){
-
-=======
 var locationHub = angular.module('locationHub', ['ui.bootstrap','convenienceMethodModule','once'])
 
 .config(function($routeProvider){
@@ -606,6 +534,5 @@ modalCtrl = function($scope, locationHubFactory, $modalInstance, convenienceMeth
     	);
 
     }
->>>>>>> branch 'master' of git@bitbucket.org:hoke/erasmus.git
 
 }
