e3daecee3433049d4585d1cf24cb5811ced9d0a7
Matthew Breeden <mbreeden@graysail.com>
2014-12-10 09:50:32 -0500

    For elements hidden after search is performed.  'Search Again' button added to hide results and reveal form

--- Erasmus/src/views/hubs/emergencyInformationHub.php
+++ Erasmus/src/views/hubs/emergencyInformationHub.php
@@ -19,28 +19,36 @@ require_once '../top_view.php';
 	<div class="whiteBg" style="margin-top:-40px; padding-bottom:15px !important;">
 		<span id="emergency-info">
 
-			<div class="center">
-				<a class="btn btn-info" ng-click="searchType = 'location'"><h2>Search by Location</h2></a>
-				<a class="btn btn-info" ng-click="searchType = 'pi'"><h2>Search by Principal Investigator</h2></a>
+			<div class="center" ng-show="!showingHazards">
+				<a class="btn btn-info btn-large" ng-click="searchType = 'location'"><h2>Search by Location</h2></a>
+				<a class="btn btn-info btn-large" ng-click="searchType = 'pi'"><h2>Search by Principal Investigator</h2></a>
 			</div>
+			<div class="center" ng-show="showingHazards">
+				<a class="btn btn-info left btn-large"  ng-click="showingHazards = !showingHazards"><i class="icon-redo"></i>Search Again</a>
+			</div>
+			<div class="spacer large"></div>
+			<div class="spacer small"></div>
+			<h2 class="alert" ng-if="error">{{error}}</h2>
 			<div id="buildings">
-				<form class="row form-inline" style="margin-left:0">
+				<form class="row form-inline" style="margin-left:0" ng-if="!showingHazards">
 					<span ng-if="searchType == 'location'">
 						<label>Building Name or Physical Address:</label>
-						<input ng-if="buildings" style="width:350px" type="text" typeahead-on-select='eif.onSelectPIOrBuilding($item)' ng-model="selectedBuilding" placeholder="Select a Building" typeahead="building as building.Name for building in buildings | filter:$viewValue">
+						<input ng-if="buildings" style="width:350px" type="text" typeahead-on-select='eif.onSelectBuilding($item)' ng-model="selectedBuilding" placeholder="Select a Building" typeahead="building as building.Name for building in buildings | filter:$viewValue">
 						<input ng-if="!buildings" style="width:350px" type="text" disabled="disabled" placeholder="Getting buildings...">
-				       	<img ng-if="!buildings" class="" style="height: 23px; margin: 15px 0 0 -44px; position: absolute;" src="<?php echo WEB_ROOT?>img/loading.gif"/>
+				       	<i ng-if="!buildings" class="icon-spinnery-dealie spinner small" style="height: 23px; margin: 15px 0 0 -44px; position: absolute;"></i>
 
 						<label>Room:</label>
-						<input ng-if="rooms" style="" type="text" typeahead-on-select='onSelectRoom($item)' ng-model="selectedRoom" placeholder="Select a Room" typeahead="room as room.roomText for room in rooms | filter:$viewValue">
-						<input ng-if="!rooms" placeholder="Select a Building" disabled="disabled">
+						<input ng-if="rooms" style="" type="text" typeahead-on-select='onSelectRoom($item)' ng-model="selectedRoom" placeholder="Select a Room" typeahead="room as room.roomText for room in rooms | filter:{roomText: $viewValue}">
+						<input ng-if="!rooms && !gettingRooms" placeholder="Select a Building" disabled="disabled">
+						<input ng-if="!rooms && gettingRooms" placeholder="Getting rooms..." disabled="disabled">
+				       	<i ng-if="!rooms && gettingRooms" class="icon-spinnery-dealie spinner small" style="height: 23px; margin: 15px 0 0 -44px; position: absolute;"></i>
 				    </span>
 
 				    <span ng-if="searchType == 'pi'">
 						<label>Principal Investigator:</label>
 						<input ng-if="pis" style="width:280px" type="text" typeahead-on-select='eif.onSelectPIOrBuilding($item)' ng-model="selectedPi" placeholder="Select a Principal Investigator" typeahead="pi as (pi.User.Name) for pi in pis | filter:$viewValue">
 						<input ng-if="!pis" style="width:280px" type="text" disabled="disabled" placeholder="Getting Principal Investigators...">
-				       	<img ng-if="!pis" class="" style="height: 23px; margin: 15px 0 0 -44px; position: absolute;" src="<?php echo WEB_ROOT?>img/loading.gif"/>
+				       	<i ng-if="!pis" class="icon-spinnery-dealie spinner small" style="height: 23px; margin: 15px 0 0 -44px; position: absolute;"></i>
 
 						<label>Location:</label>
 						<input ng-if="rooms" style="width:350px" type="text" typeahead-on-select='onSelectRoom($item)' ng-model="selectedRoom" placeholder="Select a Room" typeahead="room as room.roomText for room in rooms | filter:$viewValue">
@@ -48,12 +56,12 @@ require_once '../top_view.php';
 					</span>
 
 				</form>
-					<span ng-if="loading && !error" class="loading">
-					   <img style="width:100px"src="<?php echo WEB_ROOT?>img/loading.gif"/>
-					  Loading...
-					</span>
+				<span ng-if="loading && !error" class="loading" style="margin-left:-140px;">
+		       	  <i class="icon-spinnery-dealie spinner large"></i>
+				  <span>Loading...</span>
+				</span>
 
-				<table ng-if="hazards && pisByRoom" class="table table-striped pisTable table-bordered">
+				<table ng-if="hazards && pisByRoom && showingHazards" class="table table-striped pisTable table-bordered">
 					<tr class="blue-tr">
 						<th>Principal Investigator</th>
 						<th>Emergency Phone</th>
@@ -69,7 +77,7 @@ require_once '../top_view.php';
 						</td>
 					</tr>
 				</table>
-				<table ng-if="hazards && pisByRoom" class="table table-striped pisTable table-bordered" style="max-width:700px;">
+				<table ng-if="hazards && pisByRoom && showingHazards" class="table table-striped pisTable table-bordered" style="max-width:700px;">
 					<tr class="blue-tr">
 						<th>Lab Personnel Contacts</th>
 						<th>Emergency Phone</th>
@@ -83,17 +91,18 @@ require_once '../top_view.php';
 			</div>
 		</div>
 
-		<h1 class="hazardHeader" ng-if="hazards">LABORATORY HAZARDS</h1>
-		<ul class="modalHazardList">
-			<li ng-if="hazards" data-ng-repeat="hazard in hazards" class="modalHazard{{hazard.Key_id}}">
-				<h1>{{hazard.Name}}</h1>
-				<h3 style="margin-left:32px" ng-if="eif.noSubHazardsPresent(hazard)">No {{hazard.Name}} hazards.</h3>
-				<ul ng-if="hazard.ActiveSubHazards">
-					<div ng-include="'EmergencyInfoList.php'" ng-init="SubHazards = hazard.ActiveSubHazards"></div>
-	    		</ul>
-			</li>
-			<div style='clear:both'>&nbsp;</div>
-		</ul>
-
+		<span ng-if="showingHazards">
+			<h1 class="hazardHeader" ng-if="hazards">LABORATORY HAZARDS</h1>
+			<ul class="modalHazardList">
+				<li ng-if="hazards" data-ng-repeat="hazard in hazards" class="modalHazard{{hazard.Key_id}}">
+					<h1>{{hazard.Name}}</h1>
+					<h3 style="margin-left:32px" ng-if="eif.noSubHazardsPresent(hazard)">No {{hazard.Name}} hazards.</h3>
+					<ul ng-if="hazard.ActiveSubHazards">
+						<div ng-include="'EmergencyInfoList.php'" ng-init="SubHazards = hazard.ActiveSubHazards"></div>
+		    		</ul>
+				</li>
+				<div style='clear:both'>&nbsp;</div>
+			</ul>
+		</span>
 	</span>
 </span>
