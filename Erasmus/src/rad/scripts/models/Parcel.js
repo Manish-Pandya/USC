8d49fbd461229ad208eb617c4f670314a635a8bc
TheGuyWithTheFace <thescratcherman@gmail.com>
2015-01-12 00:57:10 -0500

    finished stub for Parcel entity

--- Erasmus/src/rad/scripts/models/Parcel.js
+++ Erasmus/src/rad/scripts/models/Parcel.js
@@ -3,4 +3,14 @@
 //constructor
 var Parcel = function() {};
 Parcel.prototype = {
+
+    // TODO eager accessors, relationships, method names
+
 }
+
+// inherit from GenericModel
+extend(Parcel, GenericModel);
+
+angular
+    .module("parcel", [])
+    .value("Parcel", Parcel);
