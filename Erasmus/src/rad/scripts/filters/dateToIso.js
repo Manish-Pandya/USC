78bac0a52b99a240f1a625294483615e35faf8e9
Matthew Breeden <Matthew Breeden@MatthewBreeden.att.net>
2015-02-12 13:49:51 -0500

    added generic modal controller

---
+++ Erasmus/src/rad/scripts/filters/dateToIso.js
@@ -0,0 +1,13 @@
+angular.module('00RsmsAngularOrmApp')
+	.filter('dateToISO', function() {
+	  return function(input) {
+	  		if(!input)return "N/A";
+			// Split timestamp into [ Y, M, D, h, m, s ]
+			var t = input.split(/[- :]/);
+			// Apply each element to the Date function
+			console.log(t);
+			if(t[0]=="0000")return "N/A";
+			var d = new Date(t[0], t[1], t[2]);
+			return d.getMonth() + '/' + d.getDate() + '/' + d.getFullYear();
+	  };
+	})
