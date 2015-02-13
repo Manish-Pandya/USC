58aa3d244975384e2127997735b70030d194ce9b
Matthew Breeden <Matthew Breeden@MatthewBreeden>
2015-02-13 16:32:05 -0500

    added dataSwitch.promises to prevent extra api calls.  If promise already exists, it is returned

--- Erasmus/src/rad/scripts/dataStore/dataSwitch.js
+++ Erasmus/src/rad/scripts/dataStore/dataSwitch.js
@@ -14,6 +14,9 @@ angular
             // constructor
             var dataSwitch = {};
 
+            // cache of requests
+            dataSwitch.promises = [];
+
             /**
              * Given a parent entity to operate on, will load given property from the
              *  datastore or API and set it to the parent entity.
@@ -62,28 +65,37 @@ angular
                 // should always return a promise
                 var deferred = $q.defer();
 
-                // check cache first
-                if( dataStoreManager.checkCollection(className) ) {
-                    deferred.resolve( dataStoreManager.get(className) );
+                //this request has already been made.  we return the promise already created by it instead of making another
+                if( dataSwitch.promises[className] ){
+                    console.log('truthy');
+                    return dataSwitch.promises[className].promise;
+                }else{
+                    //this is a new request.  make reference to our promise in dataSwitch so the next time we make it, we return the one we already made
+                    dataSwitch.promises[className] = deferred;
+                    // check cache first
+                    if( dataStoreManager.checkCollection(className) ) {
+                        deferred.resolve( dataStoreManager.get(className) );
+                    }
+                    // if not in cache, get from server
+                    else {
+
+                        //prepare url fragment to send
+                        console.log('in dataswitch api branch')
+                        var action = genericAPIFactory.fetchActionString('getAll', className);
+
+                        // get data
+                        genericAPIFactory.read(action).then(function(returnedPromise) {
+                            var instatedObjects = modelInflatorFactory.instateAllObjectsFromJson(returnedPromise.data);
+                            deferred.resolve(instatedObjects);
+
+                            // add returned data to cache
+                            dataStoreManager.store(instatedObjects, true, className);
+                        });
+
+                    }
+
+                    return deferred.promise;
                 }
-                // if not in cache, get from server
-                else {
-
-                    //prepare url fragment to send
-                    var action = genericAPIFactory.fetchActionString('getAll', className);
-
-                    // get data
-                    genericAPIFactory.read(action).then(function(returnedPromise) {
-                        var instatedObjects = modelInflatorFactory.instateAllObjectsFromJson(returnedPromise.data);
-                        deferred.resolve(instatedObjects);
-
-                        // add returned data to cache
-                        dataStoreManager.store(instatedObjects, true, className);
-                    });
-
-                }
-
-                return deferred.promise;
             }
             return dataSwitch;
         });
