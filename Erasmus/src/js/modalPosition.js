dc1d02eddf9f47fc4aac44446fa60eff8b92f351
Matthew Breeden <mbreeden@graysail.com>
2015-02-16 17:07:02 -0500

    udpated to reflect changes on master branch

--- Erasmus/src/js/modalPosition.js
+++ Erasmus/src/js/modalPosition.js
@@ -1,26 +1,29 @@
-//This module provides a directive to calculate the height of a bootstrap modal and position it accordingly, vertically
+//This module provides a directive to calculate the height of a bootstrap modal and position it accordingly
 angular.module('modalPosition', [])
 
 .directive('modal', ['$window', function($window) {
     return {
-	    restrict : 'C',
-	    link : function(scope, element, attributes) {
-	    	scope.onResize = function() {
-	            var topMargin = $window.innerHeight - element[0].clientHeight;
-                $(element[0]).css({maxHeight: $window.innerHeight*.9, minHeight:'250px'});	    
-                $(element[0]).find('.modal-content').css({maxHeight: $window.innerHeight*.9, minHeight:'250px'});          
-                $(element[0]).css({top: topMargin/2, marginTop:-10});
-                $(element[0]).find('.modal-body').css({overflowY:'auto', maxHeight:$window.innerHeight*.85-50});
+        restrict : 'C',
+        link : function(scope, element, attributes) {
+            scope.onResize = function() {
+                var topMargin = $window.innerHeight - element[0].clientHeight;
+                $(element[0]).css({maxHeight: $window.innerHeight*.95, minHeight:'250px'});     
+                $(element[0]).find('.modal-content').css({maxHeight: ($window.innerHeight*.95-50), minHeight:'250px'});          
+                $(element[0]).css({top: (topMargin/2)-20, marginTop:-10});
+                $(element[0]).find('.modal-body').css({overflowY:'auto', maxHeight:$window.innerHeight*.85-150});
                 $(element[0]).find('.modal-body ul').css({ maxHeight:$window.innerHeight*.85-210});
 
-                if( $('.piHazards').length ){
-                    console.log($window.innerWidth);
-                    $(element[0]).width($window.innerWidth * .8);
-
-                    $(element[0]).css({'left':$window.innerWidth * .1+'px', 'marginLeft': 0});
+                if( $('.wide-modal').length ){
+                    if($window.innerWidth > 1370){
+                        $(element[0]).width($window.innerWidth * .8);
+                        $(element[0]).css({'left':$window.innerWidth * .1+'px', 'marginLeft': 0});
+                    }else{
+                        $(element[0]).width($window.innerWidth*.98);
+                        $(element[0]).css({'left':$window.innerWidth*.005+'px', 'marginLeft': 0});
+                    }
                 }
-        	}
-        	scope.onResize();
+            }
+            scope.onResize();
 
             angular.element($(element[0])).bind('DOMNodeInserted', function() {
                 scope.onResize();
@@ -29,6 +32,6 @@ angular.module('modalPosition', [])
             angular.element($window).bind('resize', function() {
                 scope.onResize();
             });
-      	}
+        }
     }
  }]);
