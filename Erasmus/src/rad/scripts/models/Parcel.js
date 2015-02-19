fbcb05e71ecad3921e66c6462af7a7080fd6cf6d
Matthew Breeden <Matthew Breeden@MatthewBreeden>
2015-02-19 16:54:11 -0500

    added relationships

--- Erasmus/src/rad/scripts/models/Parcel.js
+++ Erasmus/src/rad/scripts/models/Parcel.js
@@ -4,8 +4,39 @@
 var Parcel = function() {};
 Parcel.prototype = {
 
-    // TODO eager accessors, relationships, method names
+	className: "Parcel",
 
+	eagerAccessors:[
+		{method:"loadIsotope", boolean: 'Isotope_id'},
+		{method:"loadPurchaseOrder", boolean: 'Purchase_order_id'}
+	],
+
+	IsotopeRelationship:{
+		className: 	  'Isotope',
+		keyReference:  'Isotope_id',
+		queryString:  'getIsotopeById',
+		queryParam:   ''	
+	},
+
+	PurchaseOrderRelationship:{
+		className: 	  'PurchaseOrder',
+		keyReference:  'Purchase_order_id',
+		queryString:  'getPurchaseOrderById',
+		queryParam:   ''	
+	},
+
+    // TODO eager accessors, relationships, method names
+	loadIsotope: function() {
+        if(!this.Isotope){
+            dataLoader.loadChildObject(this, 'Isotope', 'Isotope', this.Isotope_id);
+        }
+    },
+
+	loadPurchaseOrder: function() {
+        if(!this.PurchaseOrder){
+            dataLoader.loadChildObject(this, 'PurchaseOrder', 'PurchaseOrder', this.Purchase_order_id);
+        }
+    }
 }
 
 // inherit from GenericModel
