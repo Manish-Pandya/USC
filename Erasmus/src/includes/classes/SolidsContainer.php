d7d4454338b62c666f23ecacbab34a18b84fa856
Matthew Breeden <Matthew Breeden@MatthewBreeden>
2015-02-19 16:57:13 -0500

    added name property

--- Erasmus/src/includes/classes/SolidsContainer.php
+++ Erasmus/src/includes/classes/SolidsContainer.php
@@ -17,41 +17,44 @@ class SolidsContainer extends GenericCrud {
 	protected static $COLUMN_NAMES_AND_TYPES = array(
 		"principal_investigator_id"		=> "integer",
 		"room_id"						=> "integer",
-		
+		"name"							=> "text",
+
 		//GenericCrud
 		"key_id"						=> "integer",
 		"is_active"						=> "boolean",
 		"date_last_modified"			=> "timestamp",
 		"last_modified_user_id"			=> "integer",
 		"date_created"					=> "timestamp",
-		"created_user_id"				=> "integer"
-	);		
-	
+		"created_user_id"				=> "integer",
+	);
+
 	protected static $WASTEBAG_RELATIONSHIP = array(
 		"className" => "WasteBag",
 		"tableName" => "waste_bag",
 		"keyName"	=> "key_id",
 		"foreignKeyName" => "container_id"
 	);
-	
+
 	//access information
-	
+
 	/** integer key id of the principal investigator this container belongs to. */
 	private $principal_investigator_id;
 	private $principal_investigator;
-	
+
 	/** integer key id of the room this container is in */
 	private $room_id;
 	private $room;
-	
+
 	/** array of all waste bags that have been in this container */
 	private $waste_bags;
-	
+
 	/** array of the waste bags currently in this container */
 	private $current_waste_bags;
-	
+
+	private $name;
+
 	public function __construct() {
-		
+
 		// Define which subentities to load
 		$entityMaps = array();
 		$entityMaps[] = new EntityMap("lazy", "getPrincipal_investigator");
@@ -60,20 +63,20 @@ class SolidsContainer extends GenericCrud {
 		$entityMaps[] = new EntityMap("lazy", "getCurrentWasteBags");
 		$this->setEntityMaps($entityMaps);
 	}
-	
+
 	// Required for GenericCrud
 	public function getTableName() {
 		return self::$TABLE_NAME;
 	}
-	
+
 	public function getColumnData() {
 		return self::$COLUMN_NAMES_AND_TYPES;
 	}
-	
+
 	// Accessors / Mutators
 	public function getPrincipal_investigator_id() { return $this->principal_investigator_id; }
 	public function setPrincipal_investigator_id($newId) { $this->principal_investigator_id = $newId; }
-	
+
 	public function getPrincipal_investigator() {
 		if($this->principal_investigator === null && $this->hasPrimaryKeyValue()) {
 			$piDao = new GenericDAO(new PrincipalInvestigator());
@@ -84,10 +87,10 @@ class SolidsContainer extends GenericCrud {
 	public function setPrincipal_investigator($newPI) {
 		$this->principal_investigator = $newPI;
 	}
-	
+
 	public function getRoom_id() { return $this->room_id; }
 	public function setRoom_id($newId) { $this->room_id = $newId; }
-	
+
 	public function getRoom() {
 		if($this->room === null && $this->hasPrimaryKeyValue()) {
 			$roomDao = new GenericDAO(new Room());
@@ -98,9 +101,9 @@ class SolidsContainer extends GenericCrud {
 	public function setRoom($newRoom) {
 		$this->room = $newRoom;
 	}
-	
+
 	/** CAUTION:
-	 * 
+	 *
 	 * This method will return all WasteBags that ever existed in this container!
 	 * To get just the bags currently existing in this container, call
 	 * getCurrentWasteBags instead.
@@ -115,11 +118,11 @@ class SolidsContainer extends GenericCrud {
 	public function setWAsteBags($newBags) {
 		$this->waste_bags = $newBags;
 	}
-	
+
 	public function getCurrentWasteBags() {
 		// get all waste bags
 		$wasteBags = $this->getWasteBags();
-		
+
 		// only select bags that have not been entered in drum
 		$currentBags = array();
 		foreach($wasteBags as $bag) {
@@ -130,5 +133,15 @@ class SolidsContainer extends GenericCrud {
 		}
 		return $currentBags;
 	}
-	
+
+
+	public function getName()
+	{
+	    return $this->name;
+	}
+
+	public function setName($name)
+	{
+	    $this->name = $name;
+	}
 }
