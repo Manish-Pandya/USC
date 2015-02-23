592501984bd30f36da0c6322f0b9621a951f1c47
Matthew Breeden <mbreeden@graysail.com>
2015-02-23 00:32:24 -0500

    now can set view date property which can be used as human-readable ng-model

--- Erasmus/src/rad/scripts/filters/dateToIso.js
+++ Erasmus/src/rad/scripts/filters/dateToIso.js
@@ -1,13 +1,18 @@
 angular.module('00RsmsAngularOrmApp')
 	.filter('dateToISO', function() {
-	  return function(input) {
+	  return function(input,object,propertyName) {
 	  		if(!input)return "N/A";
 			// Split timestamp into [ Y, M, D, h, m, s ]
 			var t = input.split(/[- :]/);
 			// Apply each element to the Date function
-			if(t[0]=="0000")return "N/A";
-			var d = new Date(t[0], t[1], t[2]);
-			input = d.getMonth() + '/' + d.getDate() + '/' + d.getFullYear();
+			if(object && propertyName){
+				if(t[0]=="0000")return "N/A";
+				var d = new Date(t[0], t[1], t[2]);
+				input = d.getMonth() + '/' + d.getDate() + '/' + d.getFullYear();
+				object["view_"+propertyName] = input;
+				console.log(object["view_"+propertyName]);
+			}
+
 			return input
 	  };
 	})
