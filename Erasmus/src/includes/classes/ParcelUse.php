3342497e3816ceae52e496968b3df2d8f0269c26
Matthew Breeden <Matthew Breeden@MatthewBreeden>
2015-02-25 17:34:21 -0500

    Made quantity a column and property rather than just a decorator, added experiment_use and date_used properties

--- Erasmus/src/includes/classes/ParcelUse.php
+++ Erasmus/src/includes/classes/ParcelUse.php
@@ -17,6 +17,9 @@ class ParcelUse extends GenericCrud {
 	protected static $COLUMN_NAMES_AND_TYPES = array(
 		"parcel_id"						=> "integer",
 		"date_of_use"					=> "timestamp",
+		"quantity"						=> "float",
+		"experiment_use"				=> "text",
+		"date_used"						=> "timestamp",
 
 		//GenericCrud
 		"key_id"						=> "integer",
@@ -34,32 +37,36 @@ class ParcelUse extends GenericCrud {
 		"keyName"	=> "key_id",
 		"foreignKeyName"	=> "parcel_use_id"
 	);
-	
+
 	//access information
 
 	/** Float containing the amount of isotope used */
 	private $quantity;
-	
+
 	/** Reference to the Isotope entity this usage concerns */
 	private $parcel;
 
 	/** Integer containing the id of the parcel this usage concerns */
 	private $parcel_id;
-	
+
 	/** timestamp of the date that this usage took place */
 	private $date_of_use;
-	
+
 	/** Array of waste types and amounts from this use */
 	private $parcelUseAmounts;
-	
+
+	/** How the parcel was used in an experiment */
+	private $experiment_use;
+
+	/** Date the parcel was used **/
+	private $date_used;
 
 	public function __construct() {
 
 		// Define which subentities to load
 		$entityMaps = array();
 		$entityMaps[] = new EntityMap("lazy", "getParcel");
-		$entityMaps[] = new EntityMap("lazy", "getParcelUseAmounts");
-		$entityMaps[] = new EntityMap("eager", "getQuantity");
+		$entityMaps[] = new EntityMap("eager", "getParcelUseAmounts");
 		$this->setEntityMaps($entityMaps);
 	}
 
@@ -73,21 +80,9 @@ class ParcelUse extends GenericCrud {
 	}
 
 	// Accessors / Mutators
-	public function getQuantity() {
-		if( $this->quantity == NULL ) {
-			$useAmounts = $this->getParcelUseAmounts();
-
-			$this->quantity = 0;
-			foreach($useAmounts as $amount) {
-				$this->quantity += $amount->getCurie_level();
-			}
-		}
-		return $this->quantity;
-	}
-	
 	public function getParcel_id() { return $this->parcel_id; }
 	public function setParcel_id($newId) { $this->parcel_id = $newId; }
-	
+
 	public function getParcel() {
 		if($this->parcel == null) {
 			$parcelDAO = new GenericDAO(new Parcel());
@@ -98,10 +93,10 @@ class ParcelUse extends GenericCrud {
 	public function setParcel($newParcel) {
 		$this->parcel = $newParcel;
 	}
-	
+
 	public function getDate_of_use() { return $this->date_of_use; }
 	public function setDate_of_use($newDate) { $this->date_of_use = $newDate; }
-	
+
 	public function getParcelUseAmounts() {
 		if($this->parcelUseAmounts === NULL && $this->hasPrimaryKeyValue()) {
 			$thisDao = new GenericDAO($this);
@@ -112,9 +107,15 @@ class ParcelUse extends GenericCrud {
 		}
 		return $this->parcelUseAmounts;
 	}
-	public function setParcelUseAmounts($newUseAmounts) {
-		$this->parcelUseAmounts = $newUseAmounts;
-	}
+	public function setParcelUseAmounts($newUseAmounts) {$this->parcelUseAmounts = $newUseAmounts;}
 
+	public function getQuantity() {return $this->quantity;}
+    public function setQuantity($quantity) {$this->quantity = $quantity;}
+
+    public function getExperiment_use() {return $this->experiment_use;}
+    public function setExperiment_use($experiment_use) {$this->experiment_use = $experiment_use;}
+
+    public function getDate_used() {return $this->date_used;}
+    public function setDate_used($date_used) {$this->date_used = $date_used;}
 }
 ?>
