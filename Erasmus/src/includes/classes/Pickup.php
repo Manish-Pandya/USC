cd9761c7310490f1da9f117295940561d0d8f910
Matthew Breeden <Matthew Breeden@MatthewBreeden>
2015-02-25 17:35:20 -0500

    added waste_bag

--- Erasmus/src/includes/classes/Pickup.php
+++ Erasmus/src/includes/classes/Pickup.php
@@ -3,21 +3,22 @@
 include_once 'GenericCrud.php';
 
 /**
- * 
- * 
- * 
+ *
+ *
+ *
  * @author Perry Cate, GraySail LLC
  */
 class Pickup extends GenericCrud {
 
 	/** Name of the DB Table */
 	protected static $TABLE_NAME = "pickup";
-	
+
 	/** Key/Value array listing column names and their types */
 	protected static $COLUMN_NAMES_AND_TYPES = array(
 		"pickup_date"					=> "timestamp",
 		"pickup_user_id"				=> "integer",
-		
+		"principal_investigator_id"		=> "integer",
+
 		//GenericCrud
 		"key_id"						=> "integer",
 		"is_active"						=> "boolean",
@@ -26,7 +27,7 @@ class Pickup extends GenericCrud {
 		"date_created"					=> "timestamp",
 		"created_user_id"				=> "integer"
 	);
-	
+
 	/** Relationships */
 	public static $CARBOYS_RELATIONSHIP = array(
 		"className" => "Carboy",
@@ -34,55 +35,61 @@ class Pickup extends GenericCrud {
 		"keyName"   => "key_id",
 		"foreignKeyName" => "pickup_id"
 	);
-	
+
 	public static $WASTEBAGS_RELATIONSHIP = array(
 		"className" => "WasteBag",
 		"tableName" => "waste_bag",
 		"keyName"   => "key_id",
-		"foreignKeyName" => "pickup_id"	
+		"foreignKeyName" => "pickup_id"
 	);
-	
+
 	//access information
-	
+
 	/** Date (timestamp) that this pickup occurred. */
 	private $pickup_date;
-	
+
 	/** Integer id of the user who picked up the materials. */
 	private $pickup_user_id;
-	
+
 	/** Array of Carboys picked up */
 	private $carboys;
-	
+
 	/** Array of Waste Bags picked up */
 	private $waste_bags;
-	
+
+	/** Key_id of the PI who scheduled this pickup */
+	private $principal_investigator_id;
+
+	/** PI who scheduled this pikcup */
+	private $principalInvestigator;
 
 	public function __construct() {
-		
+
 		// Define which subentities to load
 		$entityMaps = array();
 		$entityMaps[] = new EntityMap("eager", "getCarboys");
 		$entityMaps[] = new EntityMap("eager", "getWasteBags");
+		$entityMaps[] = new EntityMap("lazy", "getPrincipalInvestigator");
 		$this->setEntityMaps($entityMaps);
 
 	}
-	
+
 	// Required for GenericCrud
 	public function getTableName() {
 		return self::$TABLE_NAME;
 	}
-	
+
 	public function getColumnData() {
 		return self::$COLUMN_NAMES_AND_TYPES;
 	}
-	
+
 	// Accessors / Mutators
 	public function getPickup_date() { return $this->pickup_date; }
 	public function setPickup_date($newDate) { $this->pickup_date = $newDate; }
 
 	public function getPickup_user_id() { return $this->pickup_user_id; }
 	public function setPickup_user_id($newId) { $this->pickup_user_id = $newId; }
-	
+
 	public function getCarboys() {
 		if($this->carboys === null && $this->hasPrimaryKeyValue()) {
 			$thisDao = new GenericDAO($this);
@@ -91,11 +98,9 @@ class Pickup extends GenericCrud {
 		}
 		return $this->carboys;
 	}
-	public function setCarboys($newCarboys) {
-		$this->carboys = $newCarboys;
-	}
-	
-	
+	public function setCarboys($newCarboys) {$this->carboys = $newCarboys;}
+
+
 	public function getWasteBags() {
 		if($this->waste_bags === NULL && $this->hasPrimaryKeyValue()) {
 			$thisDao = new GenericDAO($this);
@@ -104,8 +109,18 @@ class Pickup extends GenericCrud {
 		}
 		return $this->waste_bags;
 	}
-	public function setWasteBags($newBags) {
-		$this->waste_bags = $newBags;
+	public function setWasteBags($newBags) {$this->waste_bags = $newBags;}
+
+	public function getPrincipalInvestigatorId(){return $this->principal_investigator_id;}
+	public function setPrincipalInvestigatorId($principal_investigator_id){$this->principal_investigator_id = $principal_investigator_id;}
+
+	public function getPrincipalInvestigator(){
+		if($this->principalInvestigator = null) {
+			$piDAO = new GenericDAO(new PrincipalInvestigator());
+			$this->principalInvestigator = $piDAO->getById($this->getPrincipalInvestigatorId());
+		}
+		return $this->principalInvestigator;
 	}
+	public function setPrincipalInvestigator($principalInvestigator){$this->principalInvestigator = $principalInvestigator;}
 }
 ?>
