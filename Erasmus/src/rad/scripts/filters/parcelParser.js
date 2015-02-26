d25a7fa61cef1ab8875fbb124d4d22222111df7a
Matthew Breeden <Matthew Breeden@MatthewBreeden>
2015-02-26 17:54:42 -0500

    moved push statement out of loop

--- Erasmus/src/rad/scripts/filters/parcelParser.js
+++ Erasmus/src/rad/scripts/filters/parcelParser.js
@@ -1,26 +1,21 @@
 angular.module('00RsmsAngularOrmApp')
 	.filter('parcelParser', function() {
-	  return function(uses, test) {
-	  		console.log(test);
+	  return function(uses) {
 	  		if(!uses)return;
-	  		console.log(uses);
 	  		var j = uses.length;
 	  		var filteredUses = [];
 	  		while(j--){
 	  			var use = uses[j];
-		  		console.log(use);
 	  			use.Solids = [];use.Liquids = [];use.Vials = [];
-
 		  		var i = use.ParcelUseAmounts.length;
 		  		while(i--){
 		  			var amt = use.ParcelUseAmounts[i];
-		  			if(amt.Waste_type_id == 4)use.Solids.unshift(amt);
-	  				if(amt.Waste_type_id == 3)use.Vials.unshift(amt);
-	  				if(amt.Waste_type_id == 1)use.Liquids.unshift(amt);
-	  				filteredUses.push(use);
+		  			if(amt.Waste_type_id == 4)use.Solids.push(amt);
+	  				if(amt.Waste_type_id == 3)use.Vials.push(amt);
+	  				if(amt.Waste_type_id == 1)use.Liquids.push(amt);
 		  		}
+  				filteredUses.unshift(use);
 		  	}
-		  	console.log(filteredUses);
 			return filteredUses
 	  };
 	})
