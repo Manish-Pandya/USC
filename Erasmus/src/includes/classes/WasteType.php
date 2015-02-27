d041394464b2b62de81398a547699650db76e932
Matthew Breeden <Matthew Breeden@MatthewBreeden>
2015-02-27 18:17:46 -0500

    added __construct and entity maps

--- Erasmus/src/includes/classes/WasteType.php
+++ Erasmus/src/includes/classes/WasteType.php
@@ -3,20 +3,20 @@
 include_once 'GenericCrud.php';
 
 /**
- * 
- * 
- * 
+ *
+ *
+ *
  * @author Perry Cate, GraySail LLC
  */
 class WasteType extends GenericCrud {
 
 	/** Name of the DB Table */
 	protected static $TABLE_NAME = "waste_type";
-	
+
 	/** Key/Value array listing column names and their types */
 	protected static $COLUMN_NAMES_AND_TYPES = array(
 		"name"							=> "text",
-		
+
 		//GenericCrud
 		"key_id"						=> "integer",
 		"is_active"						=> "boolean",
@@ -25,25 +25,31 @@ class WasteType extends GenericCrud {
 		"date_created"					=> "timestamp",
 		"created_user_id"				=> "integer"
 	);
-	
+
+	public function __construct() {
+		$entityMaps = array();
+		$this->setEntityMaps($entityMaps);
+	}
+
+
 	// Access information
 
 	/** String containing the name of this type of waste. */
 	private $name;
-	
+
 	// Required for GenericCrud
 	public function getTableName() {
 		return self::$TABLE_NAME;
 	}
-	
+
 	public function getColumnData() {
 		return self::$COLUMN_NAMES_AND_TYPES;
 	}
-	
+
 	// Getters / Setters
 	public function getName() { return $this->name; }
 	public function setName($newName) { $this->name = $newName; }
 
-	
+
 }
 ?>
