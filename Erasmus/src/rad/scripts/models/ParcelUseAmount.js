bf6395ae42e000d598e462c7ef50576fbc5dc490
Matthew Breeden <Matthew Breeden@MatthewBreeden>
2015-02-27 19:11:01 -0500

    added loaders

--- Erasmus/src/rad/scripts/models/ParcelUseAmount.js
+++ Erasmus/src/rad/scripts/models/ParcelUseAmount.js
@@ -5,8 +5,20 @@
 var ParcelUseAmount = function() {};
 ParcelUseAmount.prototype = {
 
-    // Any future accessors, eager loaders, etc. will go here.
+	eagerAccessors:[
+		{method: "loadCarboy", boolean:"Carboy_id"},
+		{method: "loadSolidsContainer", boolean:"SolidsContainer_id"}
+	],
 
+    // Any future accessors, eager loaders, etc. will go here.
+    loadCarboy: function(){
+    	dataLoader.loadChildObject(this, "Carboy", "Carboy", this.Carboy_id);
+    },
+
+    // Any future accessors, eager loaders, etc. will go here.
+    loadSolidsContainer: function(){
+    	dataLoader.loadChildObject(this, "SolidsContainer", "SolidsContainer", this.SolidsContainer_id);
+    }
 }
 
 // inherit from GenericModel
