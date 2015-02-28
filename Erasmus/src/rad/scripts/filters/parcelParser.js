e40bd08d4b749e8c7b004a43ecb698543124aabf
Matthew Breeden <Matthew Breeden@MatthewBreeden>
2015-02-27 19:10:48 -0500

    trying to figure out reference issue

--- Erasmus/src/rad/scripts/filters/parcelParser.js
+++ Erasmus/src/rad/scripts/filters/parcelParser.js
@@ -6,16 +6,21 @@ angular.module('00RsmsAngularOrmApp')
 	  		var filteredUses = [];
 	  		while(j--){
 	  			var use = uses[j];
-	  			use.Solids = [];use.Liquids = [];use.Vials = [];
+	  			use.Solids = [];
+	  			use.Liquids = [];
+	  			use.Vials = [];
 		  		var i = use.ParcelUseAmounts.length;
 		  		while(i--){
 		  			var amt = use.ParcelUseAmounts[i];
-		  			if(amt.Waste_type_id == 4)use.Solids.push(amt);
-	  				if(amt.Waste_type_id == 3)use.Vials.push(amt);
-	  				if(amt.Waste_type_id == 1)use.Liquids.push(amt);
+		  			setTimeout(function(){
+		  				console.log(amt);
+			  			if(amt.Waste_type_id == 4)use.Solids.push(amt);
+		  				if(amt.Waste_type_id == 3)use.Vials.push(amt);
+		  				if(amt.Waste_type_id == 1)use.Liquids.push(amt);
+		  			},100);
 		  		}
   				filteredUses.unshift(use);
 		  	}
-			return filteredUses
+			return filteredUses;
 	  };
 	})
