6734a0fe42b7e0a7ff60f27cb22c470c24a3ad95
Matthew Breeden <mbreeden@graysail.com>
2015-02-28 12:08:10 -0500

    Almost there with waste log

--- Erasmus/src/rad/scripts/controllers/pi/ParcelUseLogCtrl.js
+++ Erasmus/src/rad/scripts/controllers/pi/ParcelUseLogCtrl.js
@@ -16,32 +16,28 @@ angular.module('00RsmsAngularOrmApp')
   		var getPi = function(){
         return af.getRadPIById($stateParams.pi)
     			.then(
-    				function(pi){
-              return pi;
+    				function(){
+              $scope.pi = af.getById('PrincipalInvestigator',$stateParams.pi);
+              $scope.pi.loadWasteBags();
+              return $scope.pi;
     				},
     				function(){}
     			)
       }
 
       var getParcel = function(pi){
-        var parcel = af.getById('Parcel',$stateParams.parcel);
-        return af.getParcelUses(parcel)
-          .then(
-            function(){
-              $scope.parcel = parcel;
-              $scope.pi = pi;
-            }
-          );
+        $scope.parcel = af.getById('Parcel',$stateParams.parcel);
+        console.log($scope.parcel);
       }
 
       $scope.parcelPromise = getPi()
                               .then(getParcel);
 
       $scope.addUsage = function(parcel){
-          var use = new window.ParcelUse();
-          use.Parcel_id = $scope.parcel.Key_id;
-          use.ParcelUseAmounts = [];
-          use.edit = true;
+          $rootScope.ParcelUseCopy = new window.ParcelUse();
+          $rootScope.ParcelUseCopy.Parcel_id = $scope.parcel.Key_id;
+          $rootScope.ParcelUseCopy.ParcelUseAmounts = [];
+          $rootScope.ParcelUseCopy.edit = true;
           var solidUsageAmount = new window.ParcelUseAmount();
           var liquidUsageAmount = new window.ParcelUseAmount();
           var vialUsageAmount = new window.ParcelUseAmount();
@@ -50,33 +46,42 @@ angular.module('00RsmsAngularOrmApp')
           liquidUsageAmount.Waste_type_id = 1;
           vialUsageAmount.Waste_type_id = 3;
 
-          use.ParcelUseAmounts.push(solidUsageAmount);
-          use.ParcelUseAmounts.push(liquidUsageAmount);
-          use.ParcelUseAmounts.push(vialUsageAmount);
-          parcel.ParcelUses.unshift(use);
+          $rootScope.ParcelUseCopy.ParcelUseAmounts.push(solidUsageAmount);
+          $rootScope.ParcelUseCopy.ParcelUseAmounts.push(liquidUsageAmount);
+          $rootScope.ParcelUseCopy.ParcelUseAmounts.push(vialUsageAmount);
+          parcel.ParcelUses.unshift($rootScope.ParcelUseCopy);
       }
 
       $scope.editUse = function(use){
-          if(!use.Solids.length){
+          af.createCopy(use);
+          if(!$rootScope.ParcelUseCopy.Solids.length){
             var solidUsageAmount = new window.ParcelUseAmount();
             solidUsageAmount.Waste_type_id = 4;
-            use.ParcelUseAmounts.push(solidUsageAmount);
+            $rootScope.ParcelUseCopy.ParcelUseAmounts.push(solidUsageAmount);
+            $rootScope.ParcelUseCopy.Solids.push(solidUsageAmount);
           }
-          if(!use.Liquids.length){
+          if(!$rootScope.ParcelUseCopy.Liquids.length){
             var liquidUsageAmount = new window.ParcelUseAmount();
             liquidUsageAmount.Waste_type_id = 1;
-            use.ParcelUseAmounts.push(liquidUsageAmount);
+            $rootScope.ParcelUseCopy.ParcelUseAmounts.push(liquidUsageAmount);
+            $rootScope.ParcelUseCopy.Liquids.push(liquidUsageAmount);
           }
-          if(!use.Vials.length){
+          if(!$rootScope.ParcelUseCopy.Vials.length){
             var vialUsageAmount = new window.ParcelUseAmount();
             vialUsageAmount.Waste_type_id = 3;
-            use.ParcelUseAmounts.push(vialUsageAmount);
+            $rootScope.ParcelUseCopy.ParcelUseAmounts.push(vialUsageAmount);
+            $rootScope.ParcelUseCopy.Vials.push(vialUsageAmount);
           }
+          console.log($rootScope.ParcelUseCopy);
 
           use.edit = true;
       }
       
       $scope.selectCarboy = function(useAmount){
           useAmount.Carboy_id = useAmount.Carboy.Key_id;
+      }
+
+      $scope.selectContainer = function(useAmount){
+          useAmount.Waste_bag_id = useAmount.Waste_bag.Key_id;
       }
  });
--- Erasmus/src/rad/scripts/filters/parcelParser.js
+++ Erasmus/src/rad/scripts/filters/parcelParser.js
@@ -12,12 +12,9 @@ angular.module('00RsmsAngularOrmApp')
 		  		var i = use.ParcelUseAmounts.length;
 		  		while(i--){
 		  			var amt = use.ParcelUseAmounts[i];
-		  			setTimeout(function(){
-		  				console.log(amt);
-			  			if(amt.Waste_type_id == 4)use.Solids.push(amt);
-		  				if(amt.Waste_type_id == 3)use.Vials.push(amt);
-		  				if(amt.Waste_type_id == 1)use.Liquids.push(amt);
-		  			},100);
+		  			if(amt.Waste_type_id == 4)use.Solids.push(amt);
+	  				if(amt.Waste_type_id == 3)use.Vials.push(amt);
+	  				if(amt.Waste_type_id == 1)use.Liquids.push(amt);
 		  		}
   				filteredUses.unshift(use);
 		  	}
