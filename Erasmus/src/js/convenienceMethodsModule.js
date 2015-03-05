73f8e3e44b804c03380b78d779620e46079a67f7
Matthew Breeden <Matthew Breeden@192.168.1.109>
2015-03-05 18:02:09 -0500

    added fancy date methods

--- Erasmus/src/js/convenienceMethodsModule.js
+++ Erasmus/src/js/convenienceMethodsModule.js
@@ -356,6 +356,31 @@ angular.module('convenienceMethodModule', ['ngRoute'])
         copyObject: function(obj) {
 		    var newObject = JSON.parse(JSON.stringify(obj));
 		    return newObject;
-		}
+		},
+
+		getDate: function(dateString){
+            var seconds = Date.parse(dateString);
+            //if( !dateString || isNaN(dateString) )return;
+            var t = new Date(1970,0,1);
+            t.setTime(seconds);
+            console.log(t);
+            return t;
+        }
 	};
-});
+})
+.filter('dateToISO', function() {
+	  return function(input,object,propertyName) {
+	  		if(!input)return "N/A";
+			// Split timestamp into [ Y, M, D, h, m, s ]
+			var t = input.split(/[- :]/);
+			// Apply each element to the Date function
+			var d = new Date(t[0], t[1]-1, t[2]);
+			if(object && propertyName){
+				object["view_"+propertyName] = d;
+			}
+			//at times like these, it's important to consider the nature of addition, concatonation and the universe in general.
+			input = d.getMonth()+1 + '/' + d.getDate() + '/' + d.getFullYear();
+			if(t[0]=="0000")return "N/A";
+			return input
+	  };
+	})
