e4a1de0b24bdbe73a6ec60cb4a78e61876c0f711
Matthew Breeden <mbreeden@graysail.com>
2015-04-01 20:51:15 -0400

    added underscore to getIsotope_name

--- Erasmus/src/includes/classes/dto/IsotopeAmountDTO.php
+++ Erasmus/src/includes/classes/dto/IsotopeAmountDTO.php
@@ -4,16 +4,16 @@ class IsotopeAmountDTO {
 	private $isotope_name;
 	private $curie_level;
 	
-	public function getIsotopeName() {
+	public function getIsotope_name() {
 		return $this->isotope_name;
 	}
-	public function setIsotopeName($isotope_name) {
+	public function setIsotope_name($isotope_name) {
 		$this->isotope_name = $isotope_name;
 	}
 	public function getCurie_level() {
 		return $this->curie_level;
 	}
-	public function setCurieLevel($currie_level) {
+	public function setCurie_level($currie_level) {
 		$this->curie_level = $currie_level;
 	}
 	public function addCuries($moreCuries){
