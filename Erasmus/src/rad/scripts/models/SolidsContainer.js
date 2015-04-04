2785dc38f20ae5e4bd8780e66e10671f01e4404c
Matthew Breeden <Matthew Breeden@MatthewBreeden>
2015-04-04 11:03:23 -0400

    added loadWasteBagsForPickup and the WasteBagsForPickupRelationship

--- Erasmus/src/rad/scripts/models/SolidsContainer.js
+++ Erasmus/src/rad/scripts/models/SolidsContainer.js
@@ -4,11 +4,24 @@
 //constructor
 var SolidsContainer = function() {};
 SolidsContainer.prototype = {
+	eagerAccessors:[{method:"loadWasteBagsForPickup", boolean: 'WasteBagsForPickup'}],
+
+	WasteBagsForPickupRelationship:{
+		className:    'WasteBag',
+        keyReference:  'Container_id',
+        paramValue: 'Key_id',
+        paramName: 'id',
+        where: [{'Pickup_id':"IS NULL"},{'Date_removed':"NOT NULL"}]
+	},
 
 	loadRoom: function(){
 		if(!this.Room && this.Room_id) {
             dataLoader.loadChildObject( this, 'Room', 'Room', this.Room_id );
         }
+	},
+
+	loadWasteBagsForPickup: function(){
+        dataLoader.loadOneToManyRelationship( this, 'WasteBagsForPickup', this.WasteBagsForPickupRelationship, this.WasteBagsForPickupRelationship.where);
 	}
 
 }
