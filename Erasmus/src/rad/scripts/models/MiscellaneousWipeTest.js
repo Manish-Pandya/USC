e20194383d67e9357ba5438193e59e89c3ab5b2f
Matthew Breeden <Matthew Breeden@MatthewBreeden>
2015-05-09 15:43:25 -0400

    added loader for wipes

--- Erasmus/src/rad/scripts/models/MiscellaneousWipeTest.js
+++ Erasmus/src/rad/scripts/models/MiscellaneousWipeTest.js
@@ -5,6 +5,19 @@
 var MiscellaneousWipeTest = function() {};
 MiscellaneousWipeTest.prototype = {
 	className: "MiscellaneousWipeTest",
+
+	MiscellaneousWipeRelationship: {
+        className:    'MiscellaneousWipe',
+        keyReference:  'Miscellaneous_wipe_test_id',
+        paramValue: 'Key_id',
+        paramName: 'id'
+    },
+	loadMiscellaneous_wipes: function(){
+		alert('yo')
+		this.Miscellaneous_wipes = [];
+		console.log(dataStore);
+        dataLoader.loadManyToManyRelationship( this, 'Miscellaneous_wipes', this.MiscellaneousWipeRelationship );
+	}
 }
 
 // inherit from GenericModel
