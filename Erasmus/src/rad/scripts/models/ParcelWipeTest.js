da376923db9bc9dd30ad34ccbe9ac01d88c2450f
Matthew Breeden <Matthew Breeden@MatthewBreeden>
2015-05-09 15:44:04 -0400

    loader for wipes

--- Erasmus/src/rad/scripts/models/ParcelWipeTest.js
+++ Erasmus/src/rad/scripts/models/ParcelWipeTest.js
@@ -5,6 +5,19 @@
 var ParcelWipeTest = function() {};
 ParcelWipeTest.prototype = {
 	className: "ParcelWipeTest",
+	ParcelWipesRelationship: {
+
+        className:    'ParcelWipe',
+        keyReference:  'Parcel_wipe_test_id',
+        methodString:  'getAuthorizationsByPIId',
+        paramValue: 'Key_id',
+        paramName: 'id'
+
+    },
+	loadParcel_wipes: function(){
+		this.Parcel_wipes = [];
+        dataLoader.loadManyToManyRelationship( this, 'Parcel_wipes', this.ParcelWipesRelationship );
+	}
 }
 
 // inherit from GenericModel
