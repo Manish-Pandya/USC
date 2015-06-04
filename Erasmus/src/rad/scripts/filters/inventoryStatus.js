211f2889e49d18fd3f56fdfedd1a8fe302ba859c
Matthew Breeden <mbreeden@graysail.com>
2015-06-04 13:23:15 -0400

    admin inventories view and filter

---
+++ Erasmus/src/rad/scripts/filters/inventoryStatus.js
@@ -0,0 +1,23 @@
+angular.module('00RsmsAngularOrmApp')
+	.filter('inventoryStatus', function(convenienceMethods) {
+	  return function(piInventories, inventory) {
+	  		if(!piInventories)return;
+	  		var i = piInventories.length;
+	  		if(inventory.Due_date)var dueDate = convenienceMethods.getDate(inventory.Due_date);
+			var curDate = new Date();
+	  		while(i--){
+	  			var piInventory = piInventories[i];
+	  			if(dueDate.getTime() < curDate.getTime()){
+	  				if(!piInventory.Sign_off_date){
+	  					piInventory.Status = 'Late';
+	  				}else{
+	  					piInventory.Status = 'Complete';
+	  				}
+	  			}else{
+	  				piInventory.Status = "N/A"
+	  			}
+	  			console.log(piInventory);
+	  		}
+	  		return piInventories;
+	  };
+	})
