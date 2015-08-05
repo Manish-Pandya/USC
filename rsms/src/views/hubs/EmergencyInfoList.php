841484dd4ca8e10476358cafc8c56ec2a4ec675b
Matthew Breeden <mbreeden@graysail.com>
2015-08-05 16:05:45 -0400

    separate equipment from regular hazards

--- rsms/src/views/hubs/EmergencyInfoList.php
+++ rsms/src/views/hubs/EmergencyInfoList.php
@@ -1,7 +1,31 @@
-<li ng-repeat="hazard in SubHazards" ng-if="hazard.IsPresent">
-	<!--<pre>{{hazard | json}}</pre>-->
-	<h2 ng-if="hazard.IsPresent">{{hazard.Name}}</h2>
-	<ul ng-if="hazard.ActiveSubHazards" >
-		<div ng-init="SubHazards = hazard.ActiveSubHazards;" ng-include="'EmergencyInfoList.php'"></div>
-	</ul>
+<script type="text/ng-template" id="HazardEmergencyInfoList.html">
+    <li ng-repeat="hazard in SubHazards | filter: {Is_equipment: false}" ng-if="hazard.IsPresent">
+        <!--<pre>{{hazard | json}}</pre>-->
+        <h2 ng-if="hazard.IsPresent">{{hazard.Name}}</h2>
+        <ul ng-if="hazard.ActiveSubHazards" >
+            <div ng-init="SubHazards = hazard.ActiveSubHazards;" ng-include="'HazardEmergencyInfoList.html'"></div>
+        </ul>
+    </li>
+</script>
+<li ng-repeat="hazard in SubHazards | filter: {Is_equipment: false}" ng-if="hazard.IsPresent">
+     <h2 ng-if="hazard.IsPresent">{{hazard.Name}}</h2>
+    <ul ng-if="hazard.ActiveSubHazards" >
+        <div ng-init="SubHazards = hazard.ActiveSubHazards;" ng-include="'HazardEmergencyInfoList.html'"></div>
+    </ul>
+</li>
+<h1 ng-show="equipment.length" style="margin-top:15px;margin-left: -36px;"><span ng-if="hazard.Name == 'Biological Safety' || hazard.Name == 'Chemical/Physical Safety'">Safety Equipment</span><span ng-if="hazard.Name == 'Radiation Safety'">Equipment/Device</span></h1>
+<script type="text/ng-template" id="EquiptmentEmergencyInfoList.html">
+    <li ng-repeat="hazard in (equipment = (SubHazards | filter: {Is_equipment: true}))" ng-if="hazard.IsPresent">
+        <!--<pre>{{hazard | json}}</pre>-->
+        <h2 ng-if="hazard.IsPresent">{{hazard.Name}}</h2>
+        <ul ng-if="hazard.ActiveSubHazards" >
+            <div ng-init="SubHazards = hazard.ActiveSubHazards;" ng-include="'EquiptmentEmergencyInfoList.html'"></div>
+        </ul>
+    </li>
+</script>
+ <li ng-repeat="hazard in (equipment = (SubHazards | filter: {Is_equipment: true} | filter: {IsPresent:true}))" ng-if="hazard.IsPresent">
+    <h2>{{hazard.Name}}</h2>
+    <ul ng-if="hazard.ActiveSubHazards" >
+        <div ng-init="SubHazards = hazard.ActiveSubHazards;" ng-include="'EquiptmentEmergencyInfoList.html'"></div>
+    </ul>
 </li>
