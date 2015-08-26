f6308fe7cd769de7892452d1f7c8efe5df2fd96e
Matthew Breeden <mbreeden@graysail.com>
2015-08-26 18:49:43 -0400

    added loadPendingUserChange

--- rsms/src/verification/scripts/models/User.js
+++ rsms/src/verification/scripts/models/User.js
@@ -4,8 +4,20 @@
 //constructor
 var User = function() {};
 User.prototype = {
+    className: "User",
 
-    eagerAccessors:[]
+    eagerAccessors:[
+        {method:"loadPendingUserChange", boolean:'Name'}
+    ],
+
+    loadPendingUserChange: function()
+    {
+        // not all users have a supervisor, don't try to load something that doesn't exist.
+        if(!this.PendingUserChange) {
+            this.PendingUserChange = dataStoreManager.getChildByParentProperty("PendingUserChange", "Parent_id", this.Key_id);
+            console.log(dataStore);
+        }
+    }
 }
 
 // inherit from GenericModel
