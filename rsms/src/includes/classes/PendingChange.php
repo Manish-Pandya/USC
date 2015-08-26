d458421fbfaf75a365580b26c673b45d2befdd83
Matthew Breeden <mbreeden@graysail.com>
2015-08-26 18:47:30 -0400

    set properties to protected instead of private so PDO doesn't freak out and pretend there aren't errors when there are

--- rsms/src/includes/classes/PendingChange.php
+++ rsms/src/includes/classes/PendingChange.php
@@ -12,13 +12,12 @@ class PendingChange extends GenericCrud {
 	
 	/** Key/Value Array listing column names mapped to their types */
 	protected static $COLUMN_NAMES_AND_TYPES = array(
-			//deficiency selection is a relationship
-			"parent_id" => "integer",
-			"verification_id"	=> "verification_id",
-			"new_status"		=> "new_status",
-			"parent_class"		=> "parent_class",
+			"parent_id" 		=> "integer",
+			"verification_id"	=> "integer",
+			"new_status"		=> "text",
+			"parent_class"		=> "text",
 			"approval_date"		=> "timestamp",
-			"adding"				=> "boolean",
+			"adding"			=> "boolean",
 				
 			//GenericCrud
 			"key_id"			=> "integer",
@@ -31,18 +30,20 @@ class PendingChange extends GenericCrud {
 			"created_user_id"	=> "integer"
 	);
 	
-	private $verification_id;
-	private $parent_id;
-	private $parent_class;
-	private $new_status;
-	private $approval_date;
-	private $adding;
+	protected $verification_id;
+	protected  $parent_id;
+	protected $parent_class;
+	protected $new_status;
+	protected $approval_date;
+	protected $adding;
+	
 	
 	public function __construct(){
 	
 		// Define which subentities to load
 		$entityMaps = array();
 		//$entityMaps[] = new EntityMap("lazy","getDeficiencySelection");
+		$entityMaps[] = new EntityMap("eager","getParent_id");
 		$this->setEntityMaps($entityMaps);
 	
 	}
@@ -56,11 +57,22 @@ class PendingChange extends GenericCrud {
 		return self::$COLUMN_NAMES_AND_TYPES;
 	}
 	
-	public function getVerification_id(){return $this->verification_id;}
+	public function getVerification_id(){
+		
+		return $this->verification_id;
+	}
 	public function setVerification_id($verification_id){$this->verification_id = $verification_id;}
 	
-	public function getParent_id(){return $this->parent_id;}
-	public function setParent_id($parent_id){$this->parent_id = $parent_id ;}
+	public function getParent_id(){
+		$LOG = Logger::getLogger(_Class_);
+		$LOG->fatal('calling it');
+		$LOG->fatal($this->parent_id);
+		return $this->parent_id;
+	}
+	public function setParent_id($parent_id){
+		$LOG->fatal('calling the parent id setter');
+		$this->parent_id = $parent_id;
+	}
 	
 	public function getName(){return $this->name;}
 	public function setName($name){$this->name = $name;}
@@ -73,5 +85,6 @@ class PendingChange extends GenericCrud {
 	
 	public function getAdding(){return (boolean) $this->adding;}
 	public function setAdding($add){$this->adding = $add;}
+
 }
 ?>
