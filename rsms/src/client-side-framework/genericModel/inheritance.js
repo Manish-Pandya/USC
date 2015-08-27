93ae78a647f5c6d48135f43a7ea7d15c0f480506
Matthew Breeden <mbreeden@graysail.com>
2015-08-26 22:20:41 -0400

    fixed bug that caused the loop through object properties to stop prematurely

--- rsms/src/client-side-framework/genericModel/inheritance.js
+++ rsms/src/client-side-framework/genericModel/inheritance.js
@@ -16,18 +16,18 @@ function inherit( Child, Parent ) {
   for (var prop in Parent.prototype) {
     // Skip overwriting child props that already exist in the child,
     // as they should trump the parent's version of that prop...
-    // you know... for inheritance goodness.
-    if (Child.prototype[prop] != null) {
-        break;
-    }
-      
+    // you know... for inheritance goodness
     // Otherwise, push the Parent prop to the child
-    Child.prototype[prop] = Parent.prototype[prop];
+
+    if (Child.prototype[prop] == null) {
+        Child.prototype[prop] = Parent.prototype[prop];
+    }
+
   }
 
   //If a child has a className property, autoset its Class
   if(Child.prototype.className && !Child.prototype.Class)Child.prototype.Class = Child.prototype.className;
 
   return Child;
-  
+
 }
