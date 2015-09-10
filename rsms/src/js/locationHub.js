b889ce5c3f8d4fa939fe990ea592ba910075b376
David Hamiter <david@ionosoft.com>
2015-09-10 18:21:12 -0400

    Added logic to Edit Room modal's room use ui-select to auto-populate it. Completed logic to save custom entries.

--- rsms/src/js/locationHub.js
+++ rsms/src/js/locationHub.js
@@ -559,6 +559,8 @@ modalCtrl = function($scope, $rootScope, locationHubFactory, $modalInstance, con
     
     //make a copy without reference to the modalData so we can manipulate our object without applying changes until we save
     $scope.modalData = convenienceMethods.copyObject( locationHubFactory.getModalData() );
+    $scope.selectedUse = {Name:$scope.modalData.Purpose};
+    
     locationHubFactory.getBuildings().then(
         function(buildings){
             $scope.buildings = buildings;
@@ -593,6 +595,7 @@ modalCtrl = function($scope, $rootScope, locationHubFactory, $modalInstance, con
     $scope.cancel = function () {
       $rootScope.validationError='';
       $modalInstance.dismiss();
+        console.log($scope.use);
     };
 
 
