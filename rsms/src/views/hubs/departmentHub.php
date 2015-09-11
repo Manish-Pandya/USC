76e4445c0abc39baa15289b596588fffeaab8d2a
David Hamiter <david@ionosoft.com>
2015-09-11 13:54:27 -0400

    Style/layout corrections.

--- rsms/src/views/hubs/departmentHub.php
+++ rsms/src/views/hubs/departmentHub.php
@@ -55,8 +55,9 @@ require_once '../top_view.php';
         <THEAD>
             <tr>
                 <th class="greenBg" colspan="4">
-                    <h3 class="card-header padding greenBg">Specialty Labs</h3>
-                    <a ng-click="openModal(null, true)" class="btn btn-success btn-small left" ng-if="!creatingDepartment && departments"><i class="icon-plus-5"></i>Add New Specialty Lab</a>
+                    <h3 class="card-header padding greenBg">Specialty Labs
+                        <a ng-click="openModal(null, true)" class="btn btn-success btn-small left" style="margin-left:10px" ng-if="!creatingDepartment && departments"><i class="icon-plus-5"></i>Add New Specialty Lab</a>
+                    </h3>
                 </th>
             </tr>
             <tr>
