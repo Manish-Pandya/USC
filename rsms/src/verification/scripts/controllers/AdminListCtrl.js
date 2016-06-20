4c05b889599434f6d7d1a30ab360bf10c36bc8b2
Matt Breeden <mbreeden@graysail.com>
2016-06-20 18:14:07 -0400

    RSMS-323 added admin list

---
+++ rsms/src/verification/scripts/controllers/AdminListCtrl.js
@@ -0,0 +1,29 @@
+﻿angular
+    .module('VerificationApp')
+    .controller('AdminListCtrl', function ($scope, $rootScope, $modal, applicationControllerFactory, modelInflatorFactory, userHubFactory, locationHubFactory) {
+        var ac = $scope.ac = applicationControllerFactory;
+        $scope.dataStoreManager = dataStoreManager;
+        $scope.constants = Constants;
+
+        var currentYear = new Date().getFullYear();
+        $rootScope.loading = getVerificationsByYear(currentYear);
+
+        function getVerificationsByYear(year) {
+            console.log(year);
+            return ac.getVerificationsByYear(year)
+                    .then(
+                        function (years) {
+                            $scope.years = years;
+                            $scope.selectedYear = year.toString();
+                            console.log(dataStore);
+                            $scope.verifications = dataStoreManager.get("Verification");
+                            return years;
+                        }
+                    )
+        }
+
+        $scope.selectYear = function (year) {
+            getVerificationsByYear(year);
+        }
+
+    });
