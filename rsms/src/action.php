dda2325e309014e200afe63e6be6b205a4099cac
Matt Breeden <mbreeden@graysail.com>
2016-07-01 10:30:18 -0400

    RSMS-353

--- rsms/src/action.php
+++ rsms/src/action.php
@@ -55,10 +55,15 @@ else{
 		$_SESSION['DESTINATION'] = str_replace("rsms/", "", $_SESSION['DESTINATION']);
 		$_SESSION['DESTINATION'] = str_replace("//", "", $_SESSION['DESTINATION']);
         $LOG->fatal(WEB_ROOT . $_SESSION['DESTINATION'] );
+        $host  = $_SERVER['HTTP_HOST'];
+        $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
         if(isset($_SESSION['REDIRECT'])){
+            $LOG->fatal(WEB_ROOT . $_SESSION['DESTINATION'] );
             header("location:" . $_SESSION['REDIRECT']);
         }else{
-            header("location:" . WEB_ROOT);
+            $LOG->fatal( $uri . $_SESSION['DESTINATION'] );
+
+            header("location:"  . $uri  . '/' . $_SESSION['DESTINATION']);
         }
     }
     
