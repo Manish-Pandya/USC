9f9f4b334d504f52c3409c9723dfc0bef6b58980
Matthew Breeden <mbreeden@graysail.com>
2016-08-18 16:19:37 -0400

    RSMS-370 Removed unused folder to fix filepath issues

--- ignorasmus/client-side-framework/User.ts
+++
@@ -1,9 +0,0 @@
-﻿class User extends BaseModel {
-
-    private Name: string;
-
-    public setUrlMappings() {
-        var mappings = new UrlMapping("getAllUsers", "getUserById&id=", "saveUser");
-        super.setUrlMappings(mappings);
-    }
-}
