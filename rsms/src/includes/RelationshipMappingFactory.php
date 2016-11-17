2a6ce93bcd507feb9a0f0934ccc2b48b705f9c0a
Matthew Breeden <mbreeden@graysail.com>
2016-11-17 14:43:16 -0500

    RSMS-432 fixed typo in previous commit.

--- rsms/src/includes/RelationshipMappingFactory.php
+++ rsms/src/includes/RelationshipMappingFactory.php
@@ -9,19 +9,19 @@ class RelationshipMappingFactory {
 
 	public function getMap() {
 		return array(
-			new RelationMapping("DeficiencySelection"  , "CorrectiveAction"   , "deficiency_selection_corrective_action", "deficiency_selection_id"   , "corrective_action_id"),
-			new RelationMapping("DeficiencySelection"  , "Room"               , "deficiency_selection_room"             , "deficiency_selection_id"   , "room_id"             ),
-			new RelationMapping("Checklist"			   , "Inspection"	      , "inspection_checklist"				    , "inspection_id"             , "checklist_id"        ),
-			new RelationMapping("Inspector"			   , "Inspection"		  , "inspection_inspector"				    , "inspection_id"             , "inspector_id"        ),
-			new RelationMapping("Room"				   , "Inspection"		  , "inspection_room"						, "inspection_id"             , "room_id"             ),
-			new RelationMapping("PrincipalInvestigator", "Department"		  , "principal_investigator_department"  	, "principal_investigator_id" , "department_id"       ),
-			new RelationMapping("PrincipalInvestigator", "Room"				  , "principal_investigator_room"			, "principal_investigator_id" , "room_id"             ),
-			new RelationMapping("Response"			   , "Observation"		  , "response_observation"					, "response_id"               , "observation_id"      ),
-			new RelationMapping("Response"			   , "Recommendation"	  , "response_recommendation"				, "response_id"               , "recommendation_id"   ),
-			new RelationMapping("User"				   , "Role"				  , "user_role"								, "user_id"                   , "role_id"             ),
-			new RelationMapping("PIAuthorization"	   , "Room"				  , "pi_authorization_room"		            , "pi_authorization_id"       , "room_id"             ),
-			new RelationMapping("PIAuthorization"	   , "Department"		  , "pi_authorization_department"  	  		, "pi_authorization_id"       , "department_id"       ),
-		    new RelationMapping("IBCProtocol"	       , "IBCProtocol"		  , "protocol_pi"  	  		                , "protocol_id"               , "principal_investigator_id"  )
+			new RelationMapping("DeficiencySelection"  , "CorrectiveAction"         , "deficiency_selection_corrective_action", "deficiency_selection_id"   , "corrective_action_id"),
+			new RelationMapping("DeficiencySelection"  , "Room"                     , "deficiency_selection_room"             , "deficiency_selection_id"   , "room_id"             ),
+			new RelationMapping("Checklist"			   , "Inspection"	            , "inspection_checklist"				    , "inspection_id"             , "checklist_id"        ),
+			new RelationMapping("Inspector"			   , "Inspection"		        , "inspection_inspector"				    , "inspection_id"             , "inspector_id"        ),
+			new RelationMapping("Room"				   , "Inspection"		        , "inspection_room"						, "inspection_id"             , "room_id"             ),
+			new RelationMapping("PrincipalInvestigator", "Department"		        , "principal_investigator_department"  	, "principal_investigator_id" , "department_id"       ),
+			new RelationMapping("PrincipalInvestigator", "Room"				        , "principal_investigator_room"			, "principal_investigator_id" , "room_id"             ),
+			new RelationMapping("Response"			   , "Observation"		        , "response_observation"					, "response_id"               , "observation_id"      ),
+			new RelationMapping("Response"			   , "Recommendation"	        , "response_recommendation"				, "response_id"               , "recommendation_id"   ),
+			new RelationMapping("User"				   , "Role"				        , "user_role"								, "user_id"                   , "role_id"             ),
+			new RelationMapping("PIAuthorization"	   , "Room"				        , "pi_authorization_room"		            , "pi_authorization_id"       , "room_id"             ),
+			new RelationMapping("PIAuthorization"	   , "Department"		        , "pi_authorization_department"  	  		, "pi_authorization_id"       , "department_id"       ),
+		    new RelationMapping("IBCProtocol"	       , "PrincipalInvestigator"	, "protocol_pi"  	  		                , "protocol_id"               , "principal_investigator_id"  )
 
        );
 	}
