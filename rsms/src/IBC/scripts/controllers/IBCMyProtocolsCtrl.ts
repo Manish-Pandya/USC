887b6f3a43173d8ef6f6a128530da412461d3464
David Hamiter <david@ionosoft.com>
2017-07-12 16:18:23 -0400

    RSMS-461: Additional my-protocols view work. Added activate/inactivate toggle buttons and functionality. Added show active/inactive filter button to header. Added "Prepare a New Biosafety Protocol Application" button to header. Work on my-protocols controller, adding active/inactive toggle functionality and some modal control stub code.

--- rsms/src/IBC/scripts/controllers/IBCMyProtocolsCtrl.ts
+++ rsms/src/IBC/scripts/controllers/IBCMyProtocolsCtrl.ts
@@ -23,12 +23,37 @@ angular.module('ng-IBC')
         }
 
         $scope.loading = $rootScope.getCurrentRoles().then(getPI);
+
+        $scope.toggleActive = function (protocol) {
+            protocol.Is_active = !protocol.Is_active;
+            $scope.saving = $q.all([DataStoreManager.save(protocol)]);
+        }
+
+        $scope.openModal = function (object: FluxCompositerBase) {
+            var modalData = {};
+            if (!object) {
+                object = new ibc.IBCProtocol;
+            }
+            modalData[object.thisClass['name']] = object;
+            DataStoreManager.ModalData = modalData;
+            var modalInstance = $modal.open({
+                templateUrl: 'views/modals/assign-for-review-modal.html',
+                controller: 'IBCModalCtrl'
+            });
+        }
+
     })
-    .controller('IBCMyProtocolsModalCtrl', function ($scope, $rootScope, $modalInstance, convenienceMethods, roleBasedFactory) {
+    .controller('IBCMyProtocolsModalCtrl', function ($scope, $rootScope, $modalInstance, convenienceMethods, roleBasedFactory, $q) {
         $scope.constants = Constants;
+        $scope.modalData = DataStoreManager.ModalData;
         var rbf = roleBasedFactory;
+
+        $scope.save = function (copy) {
+            $scope.saving = $q.all([DataStoreManager.save(copy)]).then($scope.close);
+        }
 
         $scope.close = function () {
             $modalInstance.dismiss();
+            DataStoreManager.ModalData = null;
         }
     })
