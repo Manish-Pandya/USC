2b62298a8f5e6c12c4bbd65389334d0ac2e26c6f
Mitch Martin <mmartin@graysail.com>
2018-08-06 10:27:41 -0400

    Remove reference of developer local path for bio uploads

--- rsms/src/Application.php
+++ rsms/src/Application.php
@@ -19,7 +19,7 @@ if(isProduction()){
 }else{
 	define('WEB_ROOT', '/rsms/src/');
 	define('LOGIN_PAGE', 'http://erasmus.graysail.com:9080/rsms/');
-	define('BISOFATEY_PROTOCOLS_UPLOAD_DATA_DIR', 'D:/git/00_rsms/rsms/src/biosafety-committees/protocol-documents/');
+	define('BISOFATEY_PROTOCOLS_UPLOAD_DATA_DIR', getcwd().'/biosafety-committees/protocol-documents/');
 
 }
 define('UPLOAD_DIR_PATH', getcwd());
