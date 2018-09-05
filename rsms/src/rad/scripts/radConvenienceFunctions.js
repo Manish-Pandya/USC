0b73194c6d564b873c3db7d1b5486cde8996562d
Mitch Martin <mmartin@graysail.com>
2018-09-05 09:06:09 -0400

    Refactor RAD convenience functions into smaller reusable parts

--- rsms/src/rad/scripts/radConvenienceFunctions.js
+++ rsms/src/rad/scripts/radConvenienceFunctions.js
@@ -62,59 +62,87 @@ angular.module('radUtilitiesModule', [
             return containers;
         };
 
+        /**
+         * Retrieves all waste containers in the given Pickup
+         * @param {Pickup} pickup
+         */
         radUtilitiesFactory.getAllWasteContainersFromPickup = function(pickup){
             console.debug("Collect all containers from pickup ", pickup);
-            var allContainers =
-                (pickup.Carboy_use_cycles || [])
-                .concat(pickup.Waste_bags || [])
-                .concat(pickup.Scint_vial_collections || [])
-                .concat(pickup.Other_waste_containers || [])
-                .map(function (c, idx) {
-                    var container = angular.extend({}, c);
+            var allContainers = radUtilitiesFactory._mergeContainerArrays([
+                pickup.Carboy_use_cycles,
+                pickup.Waste_bags,
+                pickup.Scint_vial_collections,
+                pickup.Other_waste_containers
+            ]);
 
-                    // Consolidate label-name mismatches
-                    container.ViewLabel = c.Label || c.CarboyNumber || c.Name;
-
-                    //we index at 1 because JS can't tell the difference between false and the number 0 (see return of $scope.getContainer method below)
-                    container.idx = idx + 1;
-
-                    // Apply 'friendly' labels by-type
-                    container.ClassLabel = radUtilitiesFactory.getFriendlyWasteLabel(c.Class);
-
-                    return container;
-                }
-            );
+            allContainers = radUtilitiesFactory._extendWasteContainersInfo(allContainers);
 
             console.debug(allContainers);
             return allContainers;
         }; // end getAllWasteContainersFromPickup
 
+        /**
+         * Retrieves all waste containers which are mapped to the given Drum
+         * @param {Drum} drum
+         */
         radUtilitiesFactory.getAllWasteContainersFromDrum = function(drum){
             console.debug("Collect all containers from drum ", drum);
-            var allContainers = (dataStore.WasteBag || [])
-                                .concat(dataStore.ScintVialCollection || [])
-                                .concat(dataStore.CarboyUseCycle || [])
-                                .concat(dataStore.OtherWasteContainer || [])
-                .filter(c => c.Drum_id == drum.Key_id)
-                .map(function (c, idx) {
-                    var container = angular.extend({}, c);
+            // filter only containers in this drum
+            var allContainers = radUtilitiesFactory.getAllWasteContainers()
+                .filter(c => c.Drum_id == drum.Key_id);
 
-                    // Consolidate label-name mismatches
-                    container.ViewLabel = c.Label || c.CarboyNumber || c.Name;
-
-                    //we index at 1 because JS can't tell the difference between false and the number 0 (see return of $scope.getContainer method below)
-                    container.idx = idx + 1;
-
-                    // Apply 'friendly' labels by-type
-                    container.ClassLabel = radUtilitiesFactory.getFriendlyWasteLabel(c.Class);
-
-                    return container;
-                }
-            );
+            allContainers = radUtilitiesFactory._extendWasteContainersInfo(allContainers);
 
             console.debug(allContainers);
             return allContainers;
         };
+
+        /**
+         * Retrieves all waste containers from the dataStore
+         */
+        radUtilitiesFactory.getAllWasteContainers = function(){
+            // Read all containers from datastore and merge them into one array
+            var containers = radUtilitiesFactory._mergeContainerArrays([
+                dataStore.WasteBag,
+                dataStore.ScintVialCollection,
+                dataStore.CarboyUseCycle,
+                dataStore.OtherWasteContainer
+            ]);
+
+            containers = radUtilitiesFactory._extendWasteContainersInfo(allContainers);
+
+            return containers;
+        };
+
+        /**
+         * Merges the given array of arrays into a single array
+         * @param {Array[Array]} arrays
+         */
+        radUtilitiesFactory._mergeContainerArrays = function(arrays){
+            // concat all elements of 2D array
+            return arrays.reduce( (_all, _arr) => _all.concat(_arr || []), []);
+        };
+
+        /**
+         * Extends each container to include 'friendly' labels and a 1-based index
+         * @param {Array} containers
+         */
+        radUtilitiesFactory._extendWasteContainersInfo = function(containers){
+            return containers.map( (c, idx) => {
+                var container = angular.extend({}, c);
+
+                // Consolidate label-name mismatches
+                container.ViewLabel = c.Label || c.CarboyNumber || c.Name;
+
+                //we index at 1 because JS can't tell the difference between false and the number 0 (see return of $scope.getContainer method below)
+                container.idx = idx + 1;
+
+                // Apply 'friendly' labels by-type
+                container.ClassLabel = radUtilitiesFactory.getFriendlyWasteLabel(c.Class);
+
+                return container;
+            });
+        }
 
         return radUtilitiesFactory;
     });
