576f038336ec6e437ccc780fc8258168719100bc
Mitch Martin <mmartin@graysail.com>
2018-09-19 13:18:12 -0400

    RSMS-644: account for transfers in total inventory report

--- rsms/src/includes/dao/GenericDAO.php
+++ rsms/src/includes/dao/GenericDAO.php
@@ -1468,7 +1468,7 @@ class GenericDAO {
 				isotope_name,
 				auth_limit,
 				parcel_id,
-				parcel_quantity,
+				parcel_quantity - COALESCE(SUM( transfer_amount ), 0) AS parcel_quantity,
 				COALESCE(SUM( use_quantity ), 0) AS amount,
 				COALESCE(SUM( disposed_amount ), 0) AS total_disposed,
 				COALESCE(SUM( waste_amount ), 0) AS total_waste
@@ -1479,9 +1479,14 @@ class GenericDAO {
 					iso.auth_limit AS auth_limit,
 					parcel.key_id AS parcel_id,
 					parcel.quantity AS parcel_quantity,
+                    parcel.comments AS parcel_comments,
+                    use_log.key_id AS use_log_id,
 					amt.curie_level AS use_quantity,
+                    amt.key_id AS use_amt_id,
+                    amt.comments AS transfer_comments,
 					IF(container.is_disposed = 1, amt.curie_level, 0) AS disposed_amount,
-					IF(IFNULL(container.is_disposed, 0) = 0, amt.curie_level, 0) AS waste_amount
+                    IF(amt.waste_type_id = 6, amt.curie_level, 0) AS transfer_amount,
+					IF(amt.waste_type_id != 6 && IFNULL(container.is_disposed, 0) = 0, amt.curie_level, 0) AS waste_amount
 				FROM isotope iso
 		
 				LEFT OUTER JOIN parcel parcel ON
@@ -1505,7 +1510,8 @@ class GenericDAO {
 						drum.pickup_date AS drum_ship_date,
 						IF(	c.carboy_pour_date IS NOT NULL
 							OR (c.drum_id IS NOT NULL AND drum.pickup_date IS NOT NULL)
-							OR (c.type = 'other_waste_container' AND c.close_date IS NOT NULL), 1, 0) AS is_disposed
+							OR (c.type = 'other_waste_container' AND c.close_date IS NOT NULL), 1, 0
+                        ) AS is_disposed
 					FROM (
 						SELECT 'scint_vial_collection' AS type, key_id, pickup_id, close_date, drum_id, NULL AS carboy_status, NULL AS carboy_pour_date FROM scint_vial_collection   UNION ALL
 						SELECT 'waste_bag'             AS type, key_id, pickup_id, close_date, drum_id, NULL AS carboy_status, NULL AS carboy_pour_date FROM waste_bag               UNION ALL
@@ -1520,7 +1526,7 @@ class GenericDAO {
 					OR (container.type = 'other_waste_container' AND container.key_id = amt.other_waste_container_id)
 					OR (container.type = 'carboy_use_cycle' AND container.key_id = amt.carboy_id)
 				)
-		
+
 			) package
 		
 			GROUP BY package.isotope_id, package.parcel_id
