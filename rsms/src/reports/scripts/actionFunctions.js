28ea1ef212febf8742c941f79f53d76f840ff6c2
Mitch Martin <mmartin@graysail.com>
2018-10-16 13:49:20 -0400

    RSMS-697: Add action functions module for Reports

---
+++ rsms/src/reports/scripts/actionFunctions.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular
+    .module('actionFunctionsModule', [])
+    .factory('reportsActionFunctionsFactory', function reportsActionFunctionsFactory(){
+        var af = {};
+
+        af.getDepartmentInfo = function getDepartmentInfo(deptId){
+            var action = "getDepartmentInfo";
+            if( deptId ){
+                action += "&department_id=" + deptId;
+            }
+
+            return XHR.GET(action)
+                .then( department => {
+                    console.debug("Retrieved department", deptId, department);
+                    return department;
+                });
+        };
+
+        af.getInspectionSummaryReport = function getInspectionSummaryReport(year, dept){
+            var action = "getInspectionsSummaryReport&year=" + year + "&department_id=" + dept;
+            return XHR.GET(action)
+                .then( report => {
+                    console.debug("Retrieved Inspections Summary Report", year, dept, report);
+                    return report;
+                });
+        };
+
+        return af;
+    });
