b630d2f9d24758b615cabc60a4fbd367719417b2
Mitch Martin <mmartin@graysail.com>
2018-10-16 15:37:30 -0400

    RSMS-697: Add placeholder 'root' page for Reports module

---
+++ rsms/src/reports/scripts/controllers/ReportTypesCtrl.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('ng-Reports')
+    .controller('ReportTypesCtrl', function($scope, $stateParams){
+        console.debug("ReportTypesCtrl running");
+
+        // TODO: Get/define types of reports available to the current user
+    });
