fce1c9b9bdc43659b3c12df4dffcb2831a37ec14
Mitch Martin <mmartin@graysail.com>
2018-10-16 15:39:39 -0400

    RSMS-697: Add config/sort functions for report table

--- rsms/src/reports/scripts/controllers/InspectionsSummaryReportCtrl.js
+++ rsms/src/reports/scripts/controllers/InspectionsSummaryReportCtrl.js
@@ -4,6 +4,33 @@ angular.module('ng-Reports')
     .controller('InspectionsSummaryReportCtrl', function($scope, $stateParams, reportsActionFunctionsFactory){
         console.debug("InspectionsSummaryReport running");
 
+        // Set up configuration settings
+        $scope.config = {
+            // Default table sort details
+            orderByField: 'Principal_investigator_name',
+            reverseSort: false,
+
+            // Display the Department name (useful for multi-dept reports)
+            list_department: false
+        };
+
+        /**
+         * Reorder the report results by the given field.
+         * Repeat calls to the same field will reverse sort order
+         */
+        $scope.reorder = function reorder(field){
+            if( $scope.config.orderByField == field ){
+                $scope.config.reverseSort = !$scope.config.reverseSort;
+            }
+            else{
+                $scope.config.orderByField = field;
+                $scope.config.reverseSort = false;
+            }
+        }
+
+        /**
+         * Reads parameters for the requested report
+         */
         $scope.getReportParams = function(){
             var selection = {};
 
