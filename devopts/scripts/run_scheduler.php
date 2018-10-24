bcdf5b2ef7a41d3a4e3f159dcdc497c9e87c446c
Mitch Martin <mmartin@graysail.com>
2018-10-24 10:28:46 -0400

    RSMS-673: Add support for run_scheduler.php params to pass module-name filters

--- devopts/scripts/run_scheduler.php
+++ devopts/scripts/run_scheduler.php
@@ -3,7 +3,12 @@
 // Set up RSMS application
 require_once '/var/www/html/rsms/Application.php';
 
+$moduleFilters = null;
+if( $argc > 1 ){
+    $moduleFilters = array_slice($argv, 1);
+}
+
 // Execute scheduler
-Scheduler::run();
+Scheduler::run($moduleFilters);
 
 ?>
