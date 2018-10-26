598fe0c12d84b1b7f33c2a91f7cbf18c410e9c80
Mitch Martin <mmartin@graysail.com>
2018-10-26 10:59:35 -0400

    RSMS-743: Update summary report query to join to inspection_status and exclude any 'NOT SCHEDULED' inspections

--- rsms/src/includes/modules/reports/dao/LabInspectionSummaryReportDAO.php
+++ rsms/src/includes/modules/reports/dao/LabInspectionSummaryReportDAO.php
@@ -10,7 +10,10 @@ class LabInspectionSummaryReportDAO extends GenericDAO {
         $LOG = Logger::getLogger(__CLASS__ . '.' . __FUNCTION__);
 
         // Prepare predicates to constraint (optionally) on Year and/or Department
-        $predicates = array();
+        $predicates = array(
+            // RSMS-743: Always exclude 'NOT SCHEDULED' inspections
+            "insp_status.inspection_status != 'NOT SCHEDULED'"
+        );
 
         if( $year != NULL ){
             $predicates[] = "insp.schedule_year = :year";
@@ -36,33 +39,7 @@ class LabInspectionSummaryReportDAO extends GenericDAO {
             insp.notification_date AS notification_date,
             insp.cap_submitted_date AS cap_submitted_date,
             insp.cap_submitter_id AS cap_submitter_id,
-
-            -- Calculate inspection status (Logically copied from Inspection#getStatus())
-            (CASE
-                WHEN insp.date_closed IS NOT NULL THEN 'CLOSED OUT'
-                WHEN insp.cap_submitted_date IS NOT NULL THEN 'SUBMITTED CAP'
-                WHEN insp.notification_date IS NOT NULL THEN
-                    CASE
-                        -- 'when there are no deficiencies, 'CLOSED OUT'
-                        WHEN (SELECT count(*) = 0 FROM deficiency_selection WHERE response_id in (SELECT key_id FROM `response` WHERE inspection_id = insp.key_id)) THEN 'CLOSED OUT'
-                        -- 'when we are past 14 days after the notification_date, 'OVERDUE CAP'
-                        WHEN DATE_ADD(insp.notification_date, INTERVAL 14 DAY) < CURDATE() THEN 'OVERDUE CAP'
-                        ELSE 'INCOMPLETE CAP'
-                    END
-                WHEN insp.date_started IS NOT NULL THEN 'INCOMPLETE INSPECTION'
-                WHEN insp.schedule_month IS NOT NULL THEN
-                    CASE
-                        -- 'when we are within 30 days of the scheduled date, inspection is pending'
-                        WHEN DATE_ADD(STR_TO_DATE(CONCAT_WS('/', insp.schedule_year, insp.schedule_month, '01'), '%Y/%m/%d'), INTERVAL 30 DAY) > CURDATE() THEN
-                            CASE
-                                -- when inspectors are assigned, 'SCHEDULED'
-                                WHEN (SELECT count(*) > 0 FROM inspection_inspector WHERE inspection_id = insp.key_id) THEN 'SCHEDULED'
-                                ELSE 'NOT ASSIGNED'
-                            END
-                        ELSE 'OVERDUE INSPECTION'
-                    END
-                ELSE 'NOT SCHEDULED'
-            END) AS inspection_status,
+            insp_status.inspection_status AS inspection_status,
 
             -- Inspection details
             (SELECT count(*) FROM response resp WHERE resp.inspection_id = insp.key_id) as items_inspected,
@@ -73,6 +50,9 @@ class LabInspectionSummaryReportDAO extends GenericDAO {
             dept.name AS department_name
 
         FROM inspection insp
+
+        JOIN inspection_status insp_status
+            ON insp_status.inspection_id = insp.key_id
 
         JOIN principal_investigator_department pi_d
             ON pi_d.principal_investigator_id = insp.principal_investigator_id
