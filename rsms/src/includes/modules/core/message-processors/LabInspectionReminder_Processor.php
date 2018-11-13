19b68846b0f17ef209a4c5aa7ce04b6f8483cfdb
Mitch Martin <mmartin@graysail.com>
2018-11-13 10:58:38 -0500

    RSMS-752: Look up previous email recipients for Inspection Reminder emails

--- rsms/src/includes/modules/core/message-processors/LabInspectionReminder_Processor.php
+++ rsms/src/includes/modules/core/message-processors/LabInspectionReminder_Processor.php
@@ -31,13 +31,57 @@ class LabInspectionReminder_Processor implements MessageTypeProcessor {
         // TODO: Who to send to?
         //   "the PI and any other individuals sent the original inspection report email"
         // Problem: RSMS postInspection allows the inspector to selectively send email to PI, Contacts, and arbitrary addresses
-        $recipients = array(
-            $inspection->getPrincipalInvestigator()->getUser()->getEmail()
-        );
+
+        /* Find the latest inspection report email sent for this inspection, and use the same To/Cc settings
+            PostInspection* message which references this inspection
+        */
+        $matches = $this->findLatestInspectionReportEmail($context);
+
+        if( count($matches) > 0 ){
+            $LOG->info("Found " . count($matches) . " previous email(s) for $context. Copying recipients from latest email");
+
+            // Email(s) have been sent for this inspection before. Get the SAME recipients from the latest
+            $match = $matches[0];
+            $recipients = $match['recipients'];
+            $cc = $match['cc_recipients'];
+        }
+        else{
+            $LOG->info("No previous emails have been sent for $context. Inferring recipients from Inspection");
+            // No emails have been sent for this context. While this is an unusual use-case, infer the recipients from the inspection
+            // Send to:
+            //   PI
+            //   Lab Contacts
+            //   Inspectors
+
+            $pi = $inspection->getPrincipalInvestigator();
+            $inspectors = $inspection->getInspectors();
+            $lab_contacts = $pi->getLabPersonnel();
+
+            // Merge to a single array of users
+            $users = array();
+            $users[] = $pi->getUser();
+
+            foreach( $inspectors as $inspector ){
+                $users[] = $inspector->getUser();
+            }
+
+            $users = array_merge( $users, $lab_contacts );
+
+            // Build array of user emails
+            $recipients = array();
+            foreach($users as $user){
+                $recipients[] = $user->getEmail();
+            }
+
+            // Implode array to csv
+            $recipients = implode(',', $recipients);
+            $cc = null;
+        }
 
         // prepare email details
         $details = array(
             'recipients' => $recipients,
+            'cc' => $cc,
             'macromap' => $macromap
         );
 
@@ -51,6 +95,56 @@ class LabInspectionReminder_Processor implements MessageTypeProcessor {
         return array(
             $details
         );
+    }
+
+    private function findLatestInspectionReportEmail( $context ){
+        $LOG = Logger::getLogger(__CLASS__ . '.' . __FUNCTION__);
+        $LOG->debug("Find latest inspection email for $context");
+
+        $wc = '%"inspection_id":"' . $context->inspection_id . '"%';
+        $mtypes = array(
+            CoreModule::$MTYPE_NO_DEFICIENCIES,
+            CoreModule::$MTYPE_DEFICIENCIES_FOUND,
+            CoreModule::$MTYPE_DEFICIENCIES_CORRECTED
+        );
+
+        $sql = "SELECT
+                mq.key_id as message_id,
+                mq.context_descriptor as context_descriptor,
+                mq.sent_date as sent_date,
+                eq.key_id as email_id,
+                eq.recipients as recipients,
+                eq.cc_recipients as cc_recipients
+            FROM message_queue mq
+            JOIN email_queue eq ON eq.message_id = mq.key_id
+            WHERE mq.message_type IN (:m1, :m2, :m3)
+                AND mq.context_descriptor LIKE :wildcard
+            ORDER BY mq.sent_date DESC";
+
+        $stmt = DBConnection::prepareStatement($sql);
+
+        $stmt->bindValue(':m1', CoreModule::$MTYPE_NO_DEFICIENCIES, PDO::PARAM_STR);
+        $stmt->bindValue(':m2', CoreModule::$MTYPE_DEFICIENCIES_FOUND, PDO::PARAM_STR);
+        $stmt->bindValue(':m3', CoreModule::$MTYPE_DEFICIENCIES_CORRECTED, PDO::PARAM_STR);
+
+        $stmt->bindValue(':wildcard', $wc, PDO::PARAM_STR);
+
+        if( $LOG->isTraceEnabled() ){
+            $LOG->trace($sql);
+        }
+
+        if( $stmt->execute() ){
+            $result = $stmt->fetchAll();
+        }
+        else{
+            $LOG->error("Error matching previous email for $context");
+            $LOG->error($stmt->errorInfo());
+            $result = null;
+        }
+
+        $stmt = null;
+
+        return $result;
     }
 
     public static function getInspectionReportLink($inspection_id){
