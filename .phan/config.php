e6eca43fd3316b122d35c1861e9f5a02a1a40718
Mitch Martin <mmartin@graysail.com>
2018-12-03 10:59:24 -0500

    RSMS-775: Add Phan configuration file
    Configure static-analysis tool Phan

---
+++ .phan/config.php
@@ -0,0 +1,25 @@
+<?php
+
+use Phan\Config;
+
+return [
+    "allow_missing_properties" => true,
+    "null_casts_as_any_type" => true,
+    'backward_compatibility_checks' => false,
+    "quick_mode" => true,
+    "minimum_severity" => 5,
+    'directory_list' => [
+         'rsms/src',
+         'rsms/test'
+    ],
+
+    "exclude_analysis_directory_list" => [
+         'rsms/src/logs',
+         'rsms/src/logging',
+         'rsms/src/font',
+         'rsms/src/css',
+         'rsms/src/sass',
+         'rsms/src/sql',
+         'rsms/test/simpletest'
+    ],
+];
