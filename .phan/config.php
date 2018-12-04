8ae4bda039535b3822c7d0d6cab0c0b291e0c541
Mitch Martin <mmartin@graysail.com>
2018-12-04 09:11:56 -0500

    RSMS-775: Expand phan config

--- .phan/config.php
+++ .phan/config.php
@@ -3,17 +3,26 @@
 use Phan\Config;
 
 return [
-    "allow_missing_properties" => true,
-    "null_casts_as_any_type" => true,
+    'allow_missing_properties' => true,
+    'null_casts_as_any_type' => true,
     'backward_compatibility_checks' => true,
-    "quick_mode" => true,
-    "minimum_severity" => 5,
+    'quick_mode' => false,
+    'generic_types_enabled' => true,
+    'minimum_severity' => 5,
+
+    'enable_include_path_checks' => true,
+
+    'suppress_issue_types' => [
+          'PhanUndeclaredClassMethod',
+          'PhanUndeclaredMethod'
+    ],
+
     'directory_list' => [
          'rsms/src',
          'rsms/test'
     ],
 
-    "exclude_analysis_directory_list" => [
+    'exclude_analysis_directory_list' => [
          'rsms/src/logs',
          'rsms/src/logging',
          'rsms/src/font',
