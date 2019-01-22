f5a831922bfad6f203c0a3961958a15c1975777e
Mitch Martin <mmartin@graysail.com>
2019-01-22 15:22:33 -0500

    RSMS-847: Update description for LabInspectionReminderPendingCAPs

--- rsms/src/includes/modules/core/CoreModule.php
+++ rsms/src/includes/modules/core/CoreModule.php
@@ -50,7 +50,7 @@ class CoreModule implements RSMS_Module, MessageTypeProvider {
 
             // RSMS-826: Pending CAP Reminder
             new MessageTypeDto(self::$NAME, self::$MTYPE_CAP_REMINDER_PENDING,
-                'Automatic email is sent 14 days after CAP submitted, ONLY when there are pending CAPs. Recurring every 14 days until pending CAPs are changed to complete.',
+                'Automatic email sent two weeks after the CAP is submitted, ONLY when there are pending CAPs. Recurring email sent every two weeks until all pending CAPs are updated to complete or until December 31st of the inspection year.',
                 'LabInspectionReminder_Processor',
                 array('Inspection', 'LabInspectionReminderContext')),
 
