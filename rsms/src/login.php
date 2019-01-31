d7c296fe2c84716b2c98c69befa43730712a2d80
Mitch Martin <mmartin@graysail.com>
2019-01-31 16:07:37 -0500

    Remove session redirect value after redirecting so the user doesn't always go there

--- rsms/src/login.php
+++ rsms/src/login.php
@@ -7,6 +7,9 @@ if( isset($_SESSION) && isset($_SESSION['USER']) && !isset($_SESSION['error']) )
     // There is an active session; redirect user to appropriate place
     if(isset($_SESSION["REDIRECT"])){
         $redirect = $_SESSION["REDIRECT"];
+
+        // Once user has been redirected, clear their session var
+        unset($_SESSION['REDIRECT']);
     }
     else {
         // No requested target; default to home page
