c61f2b9155e2a65309ab67ff6c2a365c1521feea
Mitch Martin <mmartin@graysail.com>
2019-02-28 15:54:05 -0500

    RSMS-891: Filter inactive PIs from Room lists in getAllRooms

--- rsms/src/includes/ActionManager.php
+++ rsms/src/includes/ActionManager.php
@@ -2366,13 +2366,24 @@ class ActionManager {
         }
     }
 
-    public function getAllRooms($allLazy = NULL){
+    public function getAllRooms($allLazy = NULL, $filterInactivePis = FALSE){
         $LOG = Logger::getLogger( __CLASS__ . '.' . __FUNCTION__ );
 
         $dao = new RoomDAO();
 
         $rooms = $dao->getAll("name");
         $allLazy = $this->getValueFromRequest('allLazy', $allLazy);
+
+        if( $filterInactivePis ){
+            foreach($rooms as $room){
+                // Filter inactive PIs
+                $pis = array_filter($room->getPrincipalInvestigators(), function($pi){
+                    return $pi->getIs_active();
+                });
+
+                $room->setPrincipalInvestigators($pis);
+            }
+        }
 
         // initialize an array of entityMap settings to assign to rooms, instructing them to lazy-load children
         // necessary because rooms by default eager-load buildings, and this would set up an infinite load loop between building->room->building->room...
