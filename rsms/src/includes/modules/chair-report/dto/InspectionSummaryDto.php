de53d52b57caa540419d66532bcc0ea13ffa7eab
Mitch Martin <mmartin@graysail.com>
2019-04-04 10:48:28 -0400

    RSMS-917: Refactor InspectionSummaryDto to use new Trait for inspection/hazard types to avoid duplicate code

--- rsms/src/includes/modules/chair-report/dto/InspectionSummaryDto.php
+++ rsms/src/includes/modules/chair-report/dto/InspectionSummaryDto.php
@@ -4,6 +4,8 @@
  * @author Mitch Martin, GraySail LLC
  */
 class InspectionSummaryDto {
+    use T_InspectionPresentHazards;
+
     private $inspection_id;
     private $principal_investigator_name;
     private $principal_investigator_id;
@@ -19,18 +21,6 @@ class InspectionSummaryDto {
     private $pending_caps;
     private $department_id;
     private $department_name;
-
-    private $is_rad;
-    private $bio_hazards_present;
-	private $chem_hazards_present;
-	private $rad_hazards_present;
-    private $corrosive_gas_present;
-    private $flammable_gas_present;
-    private $toxic_gas_present;
-    private $hf_present;
-    private $lasers_present;
-    private $animal_facility;
-    private $xrays_present;
 
     public function getInspection_id(){
         return $this->inspection_id;
@@ -151,38 +141,6 @@ class InspectionSummaryDto {
     public function setDepartment_name($val){
         $this->department_name = $val;
     }
-
-    public function getIs_rad(){ return $this->is_rad; }
-    public function setIs_rad($val){ $this->is_rad = $val; }
-    public function getBio_hazards_present() { return $this->bio_hazards_present; }
-    public function setBio_hazards_present( $val ) { $this->bio_hazards_present = $val; }
-
-    public function getChem_hazards_present() { return $this->chem_hazards_present; }
-    public function setChem_hazards_present( $val ) { $this->chem_hazards_present = $val; }
-
-    public function getRad_hazards_present() { return $this->rad_hazards_present; }
-    public function setRad_hazards_present( $val ) { $this->rad_hazards_present = $val; }
-
-    public function getCorrosive_gas_present() { return $this->corrosive_gas_present; }
-    public function setCorrosive_gas_present( $val ) { $this->corrosive_gas_present = $val; }
-
-    public function getFlammable_gas_present() { return $this->flammable_gas_present; }
-    public function setFlammable_gas_present( $val ) { $this->flammable_gas_present = $val; }
-
-    public function getToxic_gas_present() { return $this->toxic_gas_present; }
-    public function setToxic_gas_present( $val ) { $this->toxic_gas_present = $val; }
-
-    public function getHf_present() { return $this->hf_present; }
-    public function setHf_present( $val ) { $this->hf_present = $val; }
-
-    public function getLasers_present() { return $this->lasers_present; }
-    public function setLasers_present( $val ) { $this->lasers_present = $val; }
-
-    public function getAnimal_facility() { return $this->animal_facility; }
-    public function setAnimal_facility( $val ) { $this->animal_facility = $val; }
-
-    public function getXrays_present() { return $this->xrays_present; }
-    public function setXrays_present( $val ) { $this->xrays_present = $val; }
 
     public function getScore(){
         $val = 0;
