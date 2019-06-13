c79efb1af0cae277ae64a01b50f4596089856b7f
Mitch Martin <martina3@mailbox.sc.edu>
2019-06-13 15:16:44 -0400

    RSMS-955: Fix Room to compute if it has multiple PIs by checking against only Active PIs
    This functionality existed previously, but was broken by a change to
    GenericDAO's getRelatedItemsById function

--- rsms/src/includes/modules/lab-inspection/classes/Room.php
+++ rsms/src/includes/modules/lab-inspection/classes/Room.php
@@ -303,7 +303,7 @@ class Room extends GenericCrud {
 	public function getPrincipalInvestigators(){
 		if($this->principalInvestigators == null && !is_array($this->principalInvestigators)) {
 			$thisDAO = new GenericDAO($this);
-			$this->principalInvestigators = $thisDAO->getRelatedItemsById($this->getKey_Id(), DataRelationship::fromArray(self::$PIS_RELATIONSHIP), NULL, NULL, TRUE);
+			$this->principalInvestigators = $thisDAO->getRelatedItemsById($this->getKey_Id(), DataRelationship::fromArray(self::$PIS_RELATIONSHIP), NULL, TRUE, TRUE);
 		}
 		return $this->principalInvestigators;
 	}
