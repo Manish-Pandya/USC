0311b327d5352fd246d0bf96ed9e700c58541cb1
Mitch Martin <martina3@mailbox.sc.edu>
2019-07-12 14:49:48 -0400

    RSMS-999: Additional formatting for run_tests output

--- rsms/test/run_tests.php
+++ rsms/test/run_tests.php
@@ -47,27 +47,50 @@ $results = $runner->runTests();
 
 // Echo results
 $LOG->debug("Print test results");
-echo "+---------------+\n";
-echo "| Test Results\n";
-echo "| Tests run: " . count($results) . "\n";
-echo "+---------------+\n\n";
 
-function pass( $str = NULL ){ return "\e[0;32m[PASS]" . (isset($str) ? ": $str" : '') . "\e[0m"; }
-function fail( $str = NULL ){ return "\e[0;31m[FAIL]" . (isset($str) ? ": $str" : '') . "\e[0m"; }
+function green($str){ return "\e[0;32m$str\e[0m"; }
+function red($str){ return "\e[0;31m$str\e[0m"; }
+function pass( $str = NULL ){ return green("[PASS]" . (isset($str) ? ": $str" : '')); }
+function fail( $str = NULL ){ return red("[FAIL]" . (isset($str) ? ": $str" : '')); }
 
+$passed = 0;
 foreach( $results as $testname => $testresults ){
-    echo "$testname:\n";
+    echo str_pad('+', strlen($testname) + 2, '-') . "\n";
+    echo "|$testname:\n";
+    $pass_count = 0;
     foreach( $testresults as $test => $res ){
         $passOrError = $res['pass'];
         $assertions = $res['assertions'];
 
-        echo "  $test: " . ($passOrError === true ? pass() : fail($passOrError)) . "\n";
+        if( $passOrError === true ){
+            $pass_count++;
+        }
+
+        echo "+  $test: " . ($passOrError === true ? pass() : fail($passOrError)) . "\n";
         foreach( $assertions as $a ){
 
-            echo "    " . ($a[1] ? pass($a[0]) : fail($a[0])) . "\n";
+            echo "|    " . ($a[1] ? pass($a[0]) : fail($a[0])) . "\n";
         }
     }
+
+    if( $pass_count == count($testresults) ){
+        $passed++;
+    }
+
+    echo "\n";
 }
+
+$percent = $passed / count($results) * 100;
+$summary = "$passed / " . count($results) . " Passed ($percent%)";
+$div = str_pad('', strlen($summary) + 2, '-');
+
+// Decorate summary string
+$summary = ($percent == 100 ? green($summary) : red($summary));
+
+echo "+$div+\n";
+echo "|" . str_pad('Test Results', strlen($div), ' ', STR_PAD_BOTH) . "\n";
+echo "| $summary\n";
+echo "+$div+\n\n";
 
 $LOG->debug("Completed test running");
 ?>
