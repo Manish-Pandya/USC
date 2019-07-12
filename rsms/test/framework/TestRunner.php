01700c6adb3fd919440745f651aa624d94c30975
Mitch Martin <martina3@mailbox.sc.edu>
2019-07-12 12:06:50 -0400

    RSMS-999: Add support for before/after test functions

--- rsms/test/framework/TestRunner.php
+++ rsms/test/framework/TestRunner.php
@@ -1,6 +1,9 @@
 <?php
 class TestRunner {
     public const TEST_PREFIX = 'test__';
+    public const BEFORE_TEST_PREFIX = 'before__';
+    public const AFTER_TEST_PREFIX = 'after__';
+
     private $results = array();
     private $collector;
 
@@ -31,6 +34,10 @@ class TestRunner {
         return $this->getResults();
     }
 
+    static function str_starts_with(string $needle, string $haystack){
+        return substr($haystack, 0, strlen($needle)) == $needle;
+    }
+
     /**
      * Runs all test functions in an instance of I_Test.
      *
@@ -44,21 +51,37 @@ class TestRunner {
 
         $tests->setup();
         $methods = get_class_methods( $tests_name );
+
+        // Collect method names for test execution
+        $before_methods = array();
+        $test_methods = array();
+        $after_methods = array();
+
         foreach($methods as $method){
-            if( substr($method, 0, strlen(TestRunner::TEST_PREFIX)) == TestRunner::TEST_PREFIX ){
-                Assert::logAssertions();
-
-                // Run test(s)
-                $overall_result = $this->run_test_method( array($tests, $method) );
-
-                // Get logged assertions
-                $assertions = Assert::getAssertions();
-
-                $results[$method] = array(
-                    'pass' => $overall_result,
-                    'assertions' => $assertions
-                );
+            if( self::str_starts_with(TestRunner::TEST_PREFIX, $method) ){
+                $test_methods[] = $method;
             }
+            else if( self::str_starts_with(TestRunner::BEFORE_TEST_PREFIX, $method) ){
+                $before_methods[] = $method;
+            }
+            else if( self::str_starts_with(TestRunner::AFTER_TEST_PREFIX, $method) ){
+                $after_methods[] = $method;
+            }
+        }
+
+        foreach($test_methods as $method){
+            Assert::logAssertions();
+
+            // Run test(s)
+            $overall_result = $this->run_test_method( $tests, $method, $before_methods, $after_methods );
+
+            // Get logged assertions
+            $assertions = Assert::getAssertions();
+
+            $results[$method] = array(
+                'pass' => $overall_result,
+                'assertions' => $assertions
+            );
         }
 
         $this->results[$tests_name] = $results;
@@ -69,24 +92,44 @@ class TestRunner {
     /**
      * Runs a single test function
      *
-     * @param Callable $callable Callable representing the test to run
+     * @param I_Test $instance Test instance on which to run the method
+     * @param string $method Name of the test method to run
      *
      * @return True if the test executed without error; string describing the error otherwise
      */
-    function run_test_method( $callable ){
+    function run_test_method( $instance, $method, $before_methods, $after_methods ){
         $LOG = LogUtil::get_logger(__CLASS__, __FUNCTION__);
 
         try{
-            $test_name = is_array($callable) ? $callable[1] : '';
-            $LOG->info("*** BEGIN TEST $test_name ***");
+            $LOG->info("*** BEGIN TEST $method ***");
             DBConnection::get()->beginTransaction();
 
-            call_user_func($callable);
+            // before-test
+            $LOG->info("Test-method setup");
+            foreach( $before_methods as $before ){
+                $LOG->debug("$before");
+                call_user_func( array($instance, $before) );
+            }
 
-            $LOG->info("Test $test_name passed");
+            if( method_exists($instance, 'before_test') ){
+                $LOG->info("Before-test");
+                $instance->before_test();
+            }
+
+            $LOG->info("Execute test method");
+            call_user_func( array($instance, $method) );
+
+            // after-test
+            $LOG->info("Test-method tear-down");
+            foreach( $after_methods as $after ){
+                $LOG->debug("$after");
+                call_user_func( array($instance, $after) );
+            }
+
+            $LOG->info("Test $method passed");
         }
         catch(Throwable $t){
-            $LOG->error("Test $test_name failed due to error: " . $t->getMessage());
+            $LOG->error("Test $method failed due to error: " . $t->getMessage());
             $LOG->error($t->getMessage() . ":\n    " . str_replace("\n", "\n    ", $t->getTraceAsString()));
 
             // Log additional assertion failure for non-assertion exception
@@ -97,7 +140,7 @@ class TestRunner {
             return get_class($t) . ': ' . $t->getMessage();
         }
         finally {
-            $LOG->info("*** TEAR DOWN $test_name ***");
+            $LOG->info("*** TEAR DOWN $method ***");
             DBConnection::get()->rollback();
         }
 
