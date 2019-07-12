beba2e812c9b8d3f3e880f863175b0cdf8d2cc82
Mitch Martin <martina3@mailbox.sc.edu>
2019-07-12 09:46:57 -0400

    RSMS-999: Update run_tests script to support new TestRunner setup

--- rsms/test/tickets/run_tests.php
+++ rsms/test/tickets/run_tests.php
@@ -5,6 +5,8 @@
 require_once dirname(__FILE__) . '/../framework/I_Test.php';
 require_once dirname(__FILE__) . '/../framework/Assert.php';
 require_once dirname(__FILE__) . '/../framework/TestRunner.php';
+require_once dirname(__FILE__) . '/../framework/I_TestCollector.php';
+require_once dirname(__FILE__) . '/../framework/DeclaredClassTestCollector.php';
 
 // Set up RSMS application
 require_once '/var/www/html/rsms/ApplicationBootstrapper.php';
@@ -13,50 +15,47 @@ ApplicationBootstrapper::bootstrap(null, array(
     ApplicationBootstrapper::CONFIG_LOGGING_CONFIGFILE => dirname(__FILE__) . '/test-log4php.php',
 ));
 
+$LOG = Logger::getLogger('run_tests');
+
 // Make sure we throw exceptions on assertion failures
+$LOG->debug("Enable assert exceptions");
 ini_set('assert.exception', 1);
 
-// Set up test runner
-$runner = new TestRunner();
-
 // Identify tests to run
+$LOG->debug("Read script arguments");
 $ticket_dirs = null;
 if( $argc > 1 ){
     $ticket_dirs = array_slice($argv, 1);
+    $LOG->trace( implode(',', $ticket_dirs));
 }
 
-// Find test classes in dirs
+// Load all files in specified dirs
+$LOG->debug("Load scripts in specified directories");
 foreach( $ticket_dirs as $dir ){
     $results = scandir($dir);
     foreach ($results as $result){
         //ignore these
         if ($result === '.' or $result === '..') continue;
-        require_once $dir . '/' . $result;
+        $path = "$dir/$result";
+        $LOG->debug("Loading $path");
+        require_once $path;
     }
 }
 
-// Collect all loaded I_Test implementations
-$classes = get_declared_classes();
-$test_classes = array();
-foreach($classes as $klass) {
-    $reflect = new ReflectionClass($klass);
-    if($reflect->implementsInterface( I_Test::class )) {
-        $test_classes[] = $klass;
-    }
-}
+// Set up test runner
+$LOG->debug("Set up test runner");
+$runner = new TestRunner(new DeclaredClassTestCollector());
 
-if( empty($test_classes) ){
-    exit("No tests to run\n");
-}
-
-// Run tests
-$runner->runAll( $test_classes );
+// Collect and Run tests
+$LOG->debug("Running tests");
+$results = $runner->runTests();
 
 // Echo results
+$LOG->debug("Print test results");
 function pass( $str = NULL ){ return "\e[0;32mPASS" . (isset($str) ? ": $str" : '') . "\e[0m"; }
 function fail( $str = NULL ){ return "\e[0;31mFAIL" . (isset($str) ? ": $str" : '') . "\e[0m"; }
 
-foreach( $runner->getResults() as $testname => $testresults ){
+foreach( $results as $testname => $testresults ){
     echo "$testname:\n";
     foreach( $testresults as $test => $res ){
         $passOrError = $res['pass'];
@@ -69,4 +68,6 @@ foreach( $runner->getResults() as $testname => $testresults ){
         }
     }
 }
+
+$LOG->debug("Completed test running");
 ?>
