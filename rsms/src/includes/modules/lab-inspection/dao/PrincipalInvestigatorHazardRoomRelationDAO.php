b0478cd24a78cb174a17141aef3da770e380ab4f
Mitch Martin <martina3@mailbox.sc.edu>
2019-07-18 16:12:44 -0400

    RSMS-1010: Refactor HazardDto::filterRooms, moving data functionality to PIHR DAO

--- rsms/src/includes/modules/lab-inspection/dao/PrincipalInvestigatorHazardRoomRelationDAO.php
+++ rsms/src/includes/modules/lab-inspection/dao/PrincipalInvestigatorHazardRoomRelationDAO.php
@@ -122,10 +122,195 @@ class PrincipalInvestigatorHazardRoomRelationDAO extends GenericDAO {
             }
 
             // Instruct the Hazard DTO to construct a collection of InspectionRoom DTOs
-			$dto->setAndFilterInspectionRooms($roomDtos);
+            $this->determineHazardStatus( $dto, $roomDtos );
 		}
 		return $hazardDtos;
-	}
+    }
+
+    public function getPIsAssignedInRooms( Array $roomIds ){
+        ////////////////////////////////////////
+        // Get PI assignments to the hazadDto's set of Room IDs
+        $roomIds = implode (',', $roomIds);
+
+        $queryString = "SELECT
+                pi_room.principal_investigator_id,
+                pi_room.room_id
+            FROM principal_investigator_room pi_room
+            LEFT JOIN principal_investigator pi
+                ON pi_room.principal_investigator_id = pi.key_id
+            WHERE pi.is_active = 1
+                AND pi_room.room_id IN ( $roomIds )
+            group by pi_room.principal_investigator_id";
+
+		$stmt = DBConnection::prepareStatement($queryString);
+        $stmt->execute();
+
+        // Collect PI Ids per-room
+        $pi_room_assignments = $stmt->fetchAll();
+
+        /**
+         * Array keyed by Room ID mapping to arrays of PI IDs
+         *
+         * [
+         *   123  => [             // ROOM
+         *     1, 2, 3, ...N       // PIs
+         *   ],
+         *   ...
+         * ]
+         **/
+        $piIds = array_reduce(
+            $pi_room_assignments,
+            function( $pis_per_room, $assignment ){
+                $_room_id = $assignment['room_id'];
+                $_pi_id = $assignment['principal_investigator_id'];
+
+                if( !array_key_exists($_room_id, $pis_per_room) ){
+                    // Init list for this room
+                    $pis_per_room[$_room_id] = array();
+                }
+
+                if( !in_array($_pi_id, $pis_per_room[$_room_id]) ){
+                    // Push PI into room
+                    $pis_per_room[$_room_id][] = $_pi_id;
+                }
+
+                return $pis_per_room;
+            },
+            array()
+        );
+
+        return $piIds;
+    }
+
+    public function getPIHazardRoomRelations( &$piId, &$hazard_id, Array &$roomIds){
+        // FIXME: Bind these instead of concat
+        $_ids = implode(', ', $roomIds);
+
+        $queryString = "SELECT
+            a.key_id, a.hazard_id, a.room_id, a.principal_investigator_id, a.status
+            FROM principal_investigator_hazard_room a
+            LEFT JOIN principal_investigator_room b
+            ON a.principal_investigator_id = b.principal_investigator_id
+            LEFT JOIN principal_investigator c
+            ON c.key_id = a.principal_investigator_id
+            WHERE (c.is_active = 1 OR c.key_id = $piId)
+                AND a.hazard_id = $hazard_id
+                AND a.room_id IN ($_ids)
+                AND b.room_id IN($_ids)";
+
+        $stmt = DBConnection::prepareStatement($queryString);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_CLASS, "PrincipalInvestigatorHazardRoomRelation");
+    }
+
+    public function pihrReferencesOtherPI( PrincipalInvestigatorHazardRoomRelation &$relation, Array &$piIdsPerRoom, $subjectPI ){
+        /** Relation references PI which is not the subject */
+        $_rel_is_other_pi = $relation->getPrincipal_investigator_id() != $subjectPI;
+
+        /** List of PI IDs assigned to the relation's Room (IFF the room was included in our earlier query) */
+        $_rel_room_pi_list = $piIdsPerRoom[$relation->getRoom_id()] ?? [];
+
+        /** Relation's PI is listed in its room's assignments */
+        $_rel_pi_is_listed = in_array($relation->getPrincipal_investigator_id(), $_rel_room_pi_list);
+
+        return $_rel_is_other_pi && $_rel_pi_is_listed;
+    }
+
+    public function determineHazardStatus( HazardDto &$hazardDto, Array &$pihrDtos ){
+        $LOG = Logger::getLogger(__CLASS__ );
+
+        ////////////////////////////////////////
+        $piId = $hazardDto->getPrincipal_investigator_id();
+        $hazard_id = $hazardDto->getHazard_id();
+        $roomIds = $hazardDto->getRoomIds();
+        ////////////////////////////////////////
+
+        ////////////////////////////////////////
+        $piIds = $this->getPIsAssignedInRooms( $hazardDto->getRoomIds() );
+        ////////////////////////////////////////
+
+        ////////////////////////////////////////
+        //get all the Relationships between this hazard and rooms that this PI has,
+        //  so we can determine if this PI or ANY PI has the hazard in any of these rooms
+        $piHazardRooms = $this->getPIHazardRoomRelations($piId, $hazard_id, $roomIds);
+        ////////////////////////////////////////
+
+        ////////////////////////////////////////
+        $relationHashMap = array();
+        //build key_id arrays for fast comparison in the next step
+        foreach($piHazardRooms as $relation){
+            //while we're at it, determine if any of these relations belong to other PIs
+            $referencesOtherPI = $this->pihrReferencesOtherPI($relation, $piIds, $piId);
+
+            if( $referencesOtherPI === TRUE ){
+                // Relation (and therefore this hazard DTO) is assigned to a PI other than the HazardDto's subject
+                $hazardDto->setHasMultiplePis( true );
+                $relation->setHasMultiplePis( true );
+            }else{
+                // Relation is assigned to at most One PI
+                $relation->setHasMultiplePis(false);
+            }
+
+            if(!isset($relationHashMap[$relation->getRoom_id()])){
+                $relationHashMap[$relation->getRoom_id()] = array();
+            }
+
+            array_push($relationHashMap[$relation->getRoom_id()], $relation);
+        }
+        ////////////////////////////////////////
+
+        // Assume the hazard is not present or stored-only
+        $hazardDto->setIsPresent( false );
+        $hazardDto->setStored_only( false );
+
+        ////////////////////////////////////////
+        $storedOnly = true;
+        foreach ($pihrDtos as &$room){
+            $room->setContainsHazard(false);
+            $room->setHasMultiplePis(false);
+
+            if( isset($relationHashMap[$room->getRoom_id()])) {
+                //see if there's a relation for this room and hazard with this pi id
+                foreach ($relationHashMap[$room->getRoom_id()] as $relation){
+                    if($relation->getPrincipal_investigator_id() == $hazardDto->getPrincipal_investigator_id() ){
+                        $room->setContainsHazard(true);
+                        $hazardDto->setIsPresent( true );
+                        $room->setStatus($relation->getStatus());
+                        if($relation->getStatus() != "STORED_ONLY"){
+                            $storedOnly = false;
+                        }else{
+                            $room->setStored(true);
+                        }
+
+                    }
+                    if($relation->getHasMultiplePis() == true){
+                        $room->setHasMultiplePis(true);
+                        if($relation->getPrincipal_investigator_id() != $hazardDto->getPrincipal_investigator_id() && in_array($relation->getPrincipal_investigator_id(), $piIds)){
+                            $room->setOtherLab(true);
+                        }
+                        if($relation->getStatus() != "STORED_ONLY"){
+                            $storedOnly = false;
+                        }
+                    }
+                }
+            }
+        }
+        ////////////////////////////////////////
+
+        ////////////////////////////////////////
+        //if Another PI has this hazard in one of these rooms, but the relevant PI does not
+        if($hazardDto->getHasMultiplePis() == true && $hazardDto->getIsPresent() == false){
+            $hazardDto->setBelongsToOtherPI( true );
+        }
+
+        //if the hazard is stored only in every room, and is present, set its stored_only property to true.
+        $hazardDto->setStored_only( (bool) ($storedOnly && ($hazardDto->getIsPresent() || $hazardDto->getBelongsToOtherPI())) );
+        ////////////////////////////////////////
+
+        // Set our inspection-rooms
+        $hazardDto->setInspectionRooms($pihrDtos);
+        return $pihrDtos;
+    }
 }
 
 ?>
--- rsms/src/includes/modules/lab-inspection/dtos/HazardDto.php
+++ rsms/src/includes/modules/lab-inspection/dtos/HazardDto.php
@@ -50,127 +50,28 @@ class HazardDto {
 	public function setOrder_index($idx){ $this->order_index = $idx;}
 	public function setBelongsToOtherPI($belongs){$this->belongsToOtherPI = $belongs;}
 
-
+    /**
+     * @deprecated Refactored into PrincipalInvestigatorHazardRoomRelationDAO.
+     */
     public function setAndFilterInspectionRooms($rooms) {
     	$LOG = Logger::getLogger( __CLASS__ );
+        $LOG->warn("Call to deprecated function " . __FUNCTION__);
 
         //$LOG->fatal($this->getHazard_name());
         $rooms = $this->filterRooms($rooms);
         $this->inspectionRooms = $rooms;
-
     }
 
     /**
-     *
      * @param Array $rooms Array of PIHazardRoomDto
+     * @deprecated Refactored into PrincipalInvestigatorHazardRoomRelationDAO.
      */
     public function filterRooms($rooms){
         $LOG = Logger::getLogger(__CLASS__ );
-        $this->isPresent = false;
-        // Get the db connection
-        $db = DBConnection::get();
-        $roomIds = implode (',',$this->roomIds);
+        $LOG->warn("Call to deprecated function " . __FUNCTION__);
 
-		$queryString = "SELECT principal_investigator_id, room_id
-                        FROM principal_investigator_room a
-                        LEFT JOIN principal_investigator b
-                        ON a.principal_investigator_id = b.key_id
-                        WHERE b.is_active = 1 AND a.room_id IN ( $roomIds ) group by a.principal_investigator_id";
-
-		$stmt = DBConnection::prepareStatement($queryString);
-
-		$stmt->execute();
-		$piIds = array();
-
-
-        /*principal_investigator_id] => 344
-            [0] => 344
-            [room_id] => 718
-            [1] => 718
-            */
-        foreach($stmt->fetchAll() as $row){
-            if(!array_key_exists($row["room_id"], $piIds)){
-                $piIds[$row["room_id"]] = array();
-            }
-            $piIds[$row["room_id"]][] = $row["principal_investigator_id"];
-        }
-
-        //get all the Relationships between this hazard and rooms that this PI has, so we can determine if this PI or ANY PI has the hazard in any of these rooms
-        $queryString = "SELECT
-						a.key_id, a.hazard_id, a.room_id, a.principal_investigator_id, a.status
-						FROM principal_investigator_hazard_room a
-						LEFT JOIN principal_investigator_room b
-						ON a.principal_investigator_id = b.principal_investigator_id
-                        LEFT JOIN principal_investigator c
-                        ON c.key_id = a.principal_investigator_id
-						WHERE (c.is_active = 1 OR c.key_id = $this->principal_investigator_id) AND a.hazard_id = $this->hazard_id AND a.room_id IN ($roomIds) AND b.room_id IN($roomIds)";
-
-        $stmt = DBConnection::prepareStatement($queryString);
-        $stmt->execute();
-        $piHazardRooms = $stmt->fetchAll(PDO::FETCH_CLASS, "PrincipalInvestigatorHazardRoomRelation");
-
-        $this->stored_only = false;
-
-        $relationHashMap = array();
-        //build key_id arrays for fast comparison in the next step
-        foreach($piHazardRooms as $relation){
-        	//while we're at it, determine if any of these relations belong to other PIs
-        	if($relation->getPrincipal_investigator_id() != $this->getPrincipal_investigator_id()
-                    && array_key_exists($relation->getRoom_id(), $piIds) && in_array($relation->getPrincipal_investigator_id(), $piIds[$relation->getRoom_id()])){
-
-        		$this->hasMultiplePis = true;
-        		$relation->setHasMultiplePis(true);
-        	}else{
-        		$relation->setHasMultiplePis(false);
-        	}
-
-        	if(!isset($relationHashMap[$relation->getRoom_id()])){
-        		$relationHashMap[$relation->getRoom_id()] = array();
-        	}
-        	array_push($relationHashMap[$relation->getRoom_id()], $relation);
-        }
-        $this->isPresent = false;
-        $storedOnly = true;
-        foreach ($rooms as &$room){
-        	$room->setContainsHazard(false);
-        	$room->setHasMultiplePis(false);
-
-            if( isset($relationHashMap[$room->getRoom_id()])) {
-            	//see if there's a relation for this room and hazard with this pi id
-            	foreach ($relationHashMap[$room->getRoom_id()] as $relation){
-            		if($relation->getPrincipal_investigator_id() == $this->getPrincipal_investigator_id() ){
-            			$room->setContainsHazard(true);
-            			$this->isPresent = true;
-            			$room->setStatus($relation->getStatus());
-            			if($relation->getStatus() != "STORED_ONLY"){
-            				$storedOnly = false;
-            			}else{
-                            $room->setStored(true);
-                        }
-
-            		}
-            		if($relation->getHasMultiplePis() == true){
-            			$room->setHasMultiplePis(true);
-                        if($relation->getPrincipal_investigator_id() != $this->principal_investigator_id && in_array($relation->getPrincipal_investigator_id(), $piIds)){
-                            //array_push($this->inspectionRooms, $room);
-                            $room->setOtherLab(true);
-                        }
-                        if($relation->getStatus() != "STORED_ONLY"){
-            				$storedOnly = false;
-            			}
-            		}
-            	}
-            }
-        }
-
-        //if Another PI has this hazard in one of these rooms, but the relevant PI does not
-        if($this->getHasMultiplePis() == true && $this->getIsPresent() == false){
-        	$this->belongsToOtherPI = true;
-        }
-
-        //if the hazard is stored only in every room, and is present, set its stored_only property to true.
-        $this->stored_only = (bool) ($storedOnly && ($this->isPresent || $this->belongsToOtherPI));
-        return $rooms;
+        $pihrDao = new PrincipalInvestigatorHazardRoomRelationDAO();
+        return $pihrDao->determineHazardStatus( $this, $rooms );
     }
 
 }
