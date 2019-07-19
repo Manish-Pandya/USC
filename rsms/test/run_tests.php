44673b90890b06309b9dd96581309fd59b6950b2
Mitch Martin <martina3@mailbox.sc.edu>
2019-07-19 15:36:28 -0400

    RSMS-999: Refactor run_tests script to parse CLI options using TestSetup

--- rsms/test/run_tests.php
+++ rsms/test/run_tests.php
@@ -12,121 +12,21 @@ Autoloader::register_class_dir( dirname(__FILE__) . '/framework');
 
 $LOG = Logger::getLogger('run_tests');
 
-// Make sure we throw exceptions on assertion failures
-$LOG->debug("Enable assert exceptions");
-ini_set('assert.exception', 1);
+require_once dirname(__FILE__) . '/framework/TestSetup.php';
 
-// Identify tests to run
-$LOG->debug("Read script arguments");
-$include_dirs = null;
-if( $argc > 1 ){
-    $include_dirs = array_slice($argv, 1);
-    $LOG->trace( implode(',', $include_dirs));
-}
+// Get CLI options
+TestSetup::init();
 
-// Load all files in specified dirs
-function load_dir( $dir, $recurse = false ){
-    $results = scandir($dir);
-    foreach ($results as $result){
-        //ignore these
-        if ($result === '.' or $result === '..') continue;
-
-        $path = "$dir/$result";
-
-        if( is_file($path) && file_exists($path) ){
-            load_path($path);
-        }
-        else if( $recurse && is_dir($path) && file_exists($path) ){
-            load_dir($path, $recurse);
-        }
-    }
-}
-
-function load_path( $path ){
-    global $LOG;
-    $LOG->debug("Loading $path");
-    require_once $path;
-}
-
-$LOG->debug("Load scripts in specified directories");
-foreach( $include_dirs as $param ){
-    $LOG->info("Check include param '$param'");
-
-    // Include a file
-    if( is_file($param) && file_exists($param) ){
-        load_path($param);
-    }
-
-    // Include contents of Directory
-    else if( is_dir($param) && file_exists($param) ){
-        load_dir($param, true);
-    }
-
-    // Invalid parameter
-    else {
-        $LOG->warn("Invalid parameter '$param'");
-    }
-}
-
-// Set up test runner
-$LOG->debug("Set up test runner");
-$runner = new TestRunner(new DeclaredClassTestCollector());
+// Configure test runner
+$runner = TestSetup::getTestRunner();
+$reportWriter = TestSetup::getReportWriter();
 
 // Collect and Run tests
 $LOG->debug("Running tests");
 $results = $runner->runTests();
 
-// Echo results
+// Analyze results and write Report(s)
 $LOG->debug("Print test results");
-
-function green($str){ return "\e[0;32m$str\e[0m"; }
-function red($str){ return "\e[0;31m$str\e[0m"; }
-function pass( $str = NULL ){ return green("[PASS]" . (isset($str) ? ": $str" : '')); }
-function fail( $str = NULL ){ return red("[FAIL]" . (isset($str) ? ": $str" : '')); }
-
-$passed = 0;
-foreach( $results as $testname => $testresults ){
-    echo str_pad('+', strlen($testname) + 2, '-') . "\n";
-    echo "|$testname:\n";
-    $pass_count = 0;
-    foreach( $testresults as $test => $res ){
-        $passOrError = $res['pass'];
-        $assertions = $res['assertions'];
-
-        if( $passOrError === true ){
-            $pass_count++;
-        }
-
-        echo "+  $test: " . ($passOrError === true ? pass() : fail($passOrError)) . "\n";
-        foreach( $assertions as $a ){
-
-            echo "|    " . ($a[1] ? pass($a[0]) : fail($a[0])) . "\n";
-        }
-    }
-
-    if( $pass_count == count($testresults) ){
-        $passed++;
-    }
-
-    echo "\n";
-}
-
-$total_tests = count($results);
-$percent = 0;
-if( $total_tests > 0 ){
-    $percent = $passed / $total_tests * 100;
-}
-
-$summary = "$passed / " . count($results) . " Passed ($percent%)";
-$div = str_pad('', strlen($summary) + 2, '-');
-
-// Decorate summary string
-$summary = ($percent == 100 ? green($summary) : red($summary));
-
-echo "+$div+\n";
-echo "|" . str_pad('Test Results', strlen($div), ' ', STR_PAD_BOTH) . "\n";
-echo "| $summary\n";
-echo "+$div+\n\n";
-
+$reportWriter->write($results);
 $LOG->debug("Completed test running");
 ?>
