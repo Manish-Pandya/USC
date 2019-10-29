22243b8821e60cec2d7ed27fb615eceb01a493e0
Mitch Martin <martina3@mailbox.sc.edu>
2019-10-29 12:28:01 -0400

    RSMS-1060: Remove redundant tests
    Since LabInspectionReminder_Processor extends LabInspectionUpdatedMessage_Processor, there is no need to test unchanged functionality

--- rsms/test/includes/modules/lab-inspection/message-processors/Test_LabInspectionReminder_Processor.php
+++ rsms/test/includes/modules/lab-inspection/message-processors/Test_LabInspectionReminder_Processor.php
@@ -17,26 +17,6 @@ class Test_LabInspectionReminder_Processor implements I_Test {
         Assert::eq( $emails['to'], $to, "To is set");
         Assert::false( isset($emails['cc']), "CC is not set");
     }
-
-    public function test__getRecipientEmailAddressesFromInspection(){
-        // Test that correct emails are pulled from Inspections
-        $emails = $this->processor->getRecipientEmailAddressesFromInspection( $this->inspection );
-
-        Assert::eq( $emails['lab_emails'], ['pi@email.com','contact2@email.com'], 'Send to PI and inspection\'s contact' );
-        Assert::eq( $emails['inspector_emails'], ['inspector@email.com'], 'Send to Inspector' );
-    }
-
-    public function test__computeEmailRecipients_nocontext(){
-        // Test that correct emails are computed
-        $emails = $this->processor->computeEmailRecipients( $this->inspection, null );
-
-        Assert::eq( $emails['to'], 'pi@email.com,contact2@email.com', "To is set to PI and Inspection contact");
-        Assert::eq( $emails['cc'], '', "CC is not set");
-    }
-
-    public function test__computeEmailRecipients_contextAddedRecipient(){
-        // TODO
-    }
 }
 
 ?>
