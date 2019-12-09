8ef2e611bec3244879159460548663615197187e
Mitch Martin <martina3@mailbox.sc.edu>
2019-12-09 12:05:37 -0500

    RSMS-1013, RSMS-1088: Add RoomType definition and Tests

---
+++ rsms/src/includes/modules/lab-inspection/classes/RoomType.php
@@ -0,0 +1,60 @@
+<?php
+class RoomType {
+    public const DEPT_MAPPING_TABLE_NAME = 'department_inspect_room_type';
+
+	public const RESEARCH_LAB = 'RESEARCH_LAB';
+	public const ANIMAL_FACILITY = 'ANIMAL_FACILITY';
+	public const TEACHING_LAB = 'TEACHING_LAB';
+
+	public const DEPT_ROOM_TYPE_RELATIONSHIP = array(
+        "className"	=>	Department::class,
+        "tableName"	=>	RoomType::DEPT_MAPPING_TABLE_NAME,
+        "keyName"	=>	"key_id",
+        "foreignKeyName"	=>	"department_id"
+    );
+
+	private static $TYPES;
+
+    private static function init_types(){
+        if( !isset(RoomType::$TYPES) ){
+            RoomType::$TYPES = [
+                RoomType::RESEARCH_LAB => new RoomType( RoomType::RESEARCH_LAB, 'Research Lab', true),
+                RoomType::ANIMAL_FACILITY => new RoomType( RoomType::ANIMAL_FACILITY, 'Animal Facility', true),
+                RoomType::TEACHING_LAB => new RoomType( RoomType::TEACHING_LAB, 'Teaching Lab', false)
+            ];
+        }
+    }
+
+	public static function of( string $name ){
+        RoomType::init_types();
+		if( isset(RoomType::$TYPES[$name]) ){
+			return RoomType::$TYPES[$name];
+		}
+
+		throw new Exception("Invalid Room Type '$name'");
+	}
+
+	private $name;
+	private $label;
+	private $is_inspectable;
+
+	private function __construct( $name, $label, $is_inspectable ){
+		$this->name = $name;
+		$this->label = $label;
+		$this->is_inspectable = $is_inspectable;
+	}
+
+	public function getName(){ return $this->name; }
+	public function getLabel(){ return $this->label; }
+    public function isInspectable(){ return $this->is_inspectable; }
+
+    public function getRestrictedToDepartments(){
+        $inspectionTypeDeptsRel = DataRelationship::fromArray(RoomType::DEPT_ROOM_TYPE_RELATIONSHIP);
+
+        return QueryUtil::selectFrom( new Department() )
+            ->joinTo($inspectionTypeDeptsRel)
+            ->where(Field::create('inspect_room_type', $inspectionTypeDeptsRel->getTableName()), '=', $this->name)
+            ->getAll();
+    }
+}
+?>
