6577ad1b3aff169201b81467a07901f22944f874
Mitch Martin <martina3@mailbox.sc.edu>
2019-12-27 16:03:03 -0500

    RSMS-1013, RSMS-1089: Add DAO function to check if a user is assigned a role

--- rsms/src/includes/modules/core/dao/UserDAO.php
+++ rsms/src/includes/modules/core/dao/UserDAO.php
@@ -4,7 +4,21 @@ class UserDAO extends GenericDAO {
 
     public function __construct(){
         parent::__construct(new User());
-    }
+	}
+
+	function userHasRole($user_id, $role_id){
+		$sql = "SELECT COUNT(*) > 0 AS has_role FROM user_role WHERE user_id = ? AND role_id = ? ";
+		$stmt = DBConnection::prepareStatement($sql);
+		$stmt->bindValue(1, $user_id);
+		$stmt->bindValue(2, $role_id);
+
+		if($stmt->execute()){
+			return (bool) $stmt->fetch()[0];
+		}
+
+		$error = $stmt->errorInfo();
+		return new QueryError($error[2]);
+	}
 
 	function getUserByUsername($username){
 		$this->LOG->debug("Looking up user with username $username");
