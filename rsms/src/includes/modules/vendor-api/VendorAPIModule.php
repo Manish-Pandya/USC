08a6fd33bb36805ca744aa8f627b53b382fea578
Mitch Martin <martina3@mailbox.sc.edu>
2020-03-17 12:13:37 -0400

    RSMS-1043: Implement Location API service as part of VendorAPI module
    Requests will need to be routed to the module's api_request_handler.php file by the webserver, providing the following additional request parameters:
    
    'api' : the name of the API service (in our only use case, this is 'location')
    'version' : version number of the API. Currently unused at time of commit
    'endpoint' : REST-style path describing the requested endpoint

--- rsms/src/includes/modules/vendor-api/VendorAPIModule.php
+++ rsms/src/includes/modules/vendor-api/VendorAPIModule.php
@@ -1,6 +1,8 @@
 <?php
 
 class VendorAPIModule implements RSMS_Module {
+    public const API_NAME_LOCATION = 'location';
+
     public function getModuleName(){
         return 'VendorApi';
     }
@@ -10,17 +12,38 @@ class VendorAPIModule implements RSMS_Module {
     }
 
     public function isEnabled() {
-        return basename($_SERVER['SCRIPT_FILENAME']) === 'api.php';
+        return basename($_SERVER['SCRIPT_FILENAME']) === 'api_request_handler.php';
     }
 
     public function getActionManager(){
-        return new VendorApi_ActionManager();
+        // multiple services
+        //  Based on API path parameter
+        $api = $this->getCurrentAPI();
+        switch( $api ){
+            case VendorAPIModule::API_NAME_LOCATION: return new LocationApiService();
+            default:
+                throw new Exception("Invalid API '$api'");
+        }
     }
 
     public function getActionConfig(){
-        return array_merge(
-            VendorApi_ActionMappingFactory::readActionConfig()
-        );
+        $api_restricted_roles = [];
+
+        $mappings = [];
+
+        // FIXME: This only supports one API; consider namespacing or splitting into submodules for additional
+        if( VendorAPIModule::API_NAME_LOCATION === $this->getCurrentAPI() ){
+            $mappings['getAll'] = new SecuredActionMapping('getAll', $api_restricted_roles);
+            $mappings['search'] = new SecuredActionMapping('search', $api_restricted_roles);
+            $mappings['getInfo'] = new SecuredActionMapping('getInfo', $api_restricted_roles);
+            $mappings['getDetail'] = new SecuredActionMapping('getDetail', $api_restricted_roles);
+        }
+
+        return $mappings;
+    }
+
+    private function getCurrentAPI(){
+        return $_REQUEST['api'] ?? self::API_NAME_LOCATION;
     }
 }
 ?>
--- rsms/src/includes/modules/vendor-api/api_request_handler.php
+++ rsms/src/includes/modules/vendor-api/api_request_handler.php
@@ -1,48 +1,81 @@
 <?php
 
-require_once( dirname(__FILE__) . '/Application.php');
-require_once( dirname(__FILE__) . '/includes/http_status.php' );
+require_once( dirname(__FILE__) . '/../../..' . '/Application.php');
+require_once( dirname(__FILE__) . '/../../..' . '/includes/http_status.php' );
 
 $apiName = $_REQUEST["api"];
 $apiVersion = $_REQUEST["version"];
-$actionName = $_REQUEST["endpoint"];
+$endpoint = $_REQUEST["endpoint"];
 
-// TODO: Split up action name to extract path parameters
+session_start();
 
-$dataSource = $_REQUEST;
+$user_ip = $_SERVER['REMOTE_ADDR'];
+if( !isset($_SESSION['ip']) ){
+    $_SESSION['ip'] = $user_ip;
+}
 
-RequestLog::init($actionName, $dataSource);
-$LOG = Logger::getLogger('api.' . $actionName);
+if( !isset($_SESSION['USER']) ){
+    $apiuser = new User();
+    $apiuser->setUsername($user_ip);
+    $_SESSION['USER'] = $apiuser;
+}
 
-RequestLog::log_start();
+$LOG = Logger::getLogger('api_handler');
+$response_code = 200;
+$json = '';
 
 // Write standard headers before calling action.
 //   Some actions (such as attachment download) may trigger sending
 //     of headers.
 header('content-type: application/javascript');
 
-$actionDispatcher = new ActionDispatcher($_REQUEST, []);
+$actionDispatcher = new ApiRequestDispatcher($_REQUEST, $_SESSION);
 
 // Attempt to dispatch to the requested action
 $dispatchId = Metrics::start("Dispatch action " . RequestLog::describe());
-$actionResult = $actionDispatcher->dispatch($actionName);
-Metrics::stop($dispatchId);
+try{
+    ////////
+    // Map Resource to action
+    $actionName = $actionDispatcher->parse_api_request($endpoint, $_REQUEST);
+    $LOG->debug( $_REQUEST );
+    ////////
 
-// JSON-Encode result
-// Time how long it takes to encode this
-$jsonifyId = Metrics::start("Encode response " . RequestLog::describe());
-$json = JsonManager::encode($actionResult->actionFunctionResult);
-Metrics::stop($jsonifyId);
+    RequestLog::init($actionName, $_REQUEST, 'request.api');
+    RequestLog::log_start();
+
+    $actionResult = $actionDispatcher->dispatch($actionName);
+    $response_code = $actionResult->statusCode;
+
+    // JSON-Encode result
+    // Time how long it takes to encode this
+    $jsonifyId = Metrics::start("Encode response " . RequestLog::describe());
+    $json = JsonManager::encode($actionResult->actionFunctionResult);
+    Metrics::stop($jsonifyId);
+}
+catch(InvalidApiPathException $e){
+    $json = $e->getMessage();
+    $response_code = 400;
+}
+catch(ResourceNotFoundException $e){
+    $json = $e->getMessage();
+    $response_code = 404;
+}
+catch(Exception $e){
+    $LOG->error("Error occurred during API request handling");
+    $LOG->error($e);
+    $json = 'An error occurred';
+    $response_code = 500;
+}
 
 // begin output
-
 // Set the HTTP status code. ActionResult defaults this to 200
-http_response_code( $actionResult->statusCode );
+http_response_code( $response_code );
 
 // Output JSON (with possible callback)
-RequestLog::log_stop( $actionResult, strlen($json));
+RequestLog::log_stop( $response_code, strlen($json));
 
 // Simply echo the json
 echo $json;
 
+Metrics::stop($dispatchId);
 ?>
