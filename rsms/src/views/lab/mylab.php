3018e3b4f791dcfdb0e39a04c7f75c5578701cc2
Mitch Martin <martina3@mailbox.sc.edu>
2020-04-21 10:32:40 -0400

    RSMS-1186: Ensure that application is loaded in mylab prior to checking auth'd user

--- rsms/src/views/lab/mylab.php
+++ rsms/src/views/lab/mylab.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../../RequireUserLoggedIn.php';
 require_once '../top_view.php';
+require_once '../../RequireUserLoggedIn.php';
 ?>
 <script src="../../js/lab/myLab.js"></script>
 <script src="widgets/my-lab-widget.js"></script>
