cbe11522cef02f9b538e9ffda561d2573a957d33
Mitch Martin <martina3@mailbox.sc.edu>
2020-07-16 13:06:03 -0400

    RSMS-1229: Update interface definition for NotificationStatsProvider to declare return type

--- rsms/src/includes/modules/core/NotificationStatsProvider.php
+++ rsms/src/includes/modules/core/NotificationStatsProvider.php
@@ -1,5 +1,9 @@
 <?php
+/**
+ * A NotificationStatsProvider module provides a NotificationStatsDto
+ * which contains items which warrant notifying to the user
+ */
 interface NotificationStatsProvider {
-    public function getNotificationStats();
+    public function getNotificationStats() : NotificationStatsDto;
 }
 ?>
