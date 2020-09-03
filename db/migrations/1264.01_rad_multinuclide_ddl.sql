d36b16552190b3ca68f03d42c6da987f840c88fc
Mitch Martin <martina3@mailbox.sc.edu>
2020-09-03 11:53:48 -0400

    RSMS-1264: Update multinuclide migration script to default creation date to zero...
    mysql does not allow CURRENT_TIMESTAMP to be the default value for more than one column

--- db/migrations/1264.01_rad_multinuclide_ddl.sql
+++ db/migrations/1264.01_rad_multinuclide_ddl.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `parcel_authorization` (
     `key_id` int(11) NOT NULL AUTO_INCREMENT,
     `is_active` tinyint(1) DEFAULT '1',
-    `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    `date_created` timestamp NULL DEFAULT '0000-00-00 00:00:00',
     `created_user_id` int(11) DEFAULT NULL,
     `date_last_modified` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `last_modified_user_id` int(11) DEFAULT NULL,
