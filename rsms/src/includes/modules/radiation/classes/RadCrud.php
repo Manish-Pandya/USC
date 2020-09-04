b69b6243edb7479db7be33ad835772fc5e0edb2a
Mitch Martin <martina3@mailbox.sc.edu>
2020-09-04 08:19:22 -0400

    RSMS-1264: Fix Container collection of total contents to reflect multi-nuclide amounts

--- rsms/src/includes/modules/radiation/classes/RadCrud.php
+++ rsms/src/includes/modules/radiation/classes/RadCrud.php
@@ -15,44 +15,77 @@ abstract class RadCrud extends GenericCrud {
 	 *
 	 */
 	public function sumUsages($useAmounts){
-		$LOG = Logger::getLogger(__CLASS__);
-        //$LOG->fatal($useAmounts);
+		$LOG = Logger::getLogger(__CLASS__ . '.' . __FUNCTION__);
+
 		$isotopeAmounts = array();
 
+		// FIXME: collect use amounts PER ISOTOPE
+		$LOG->debug("Collect isotope contents from use amounts");
 		foreach($useAmounts as $amount){
-            if($amount->getIs_active() != true)continue;
-            $isotopeDao = new GenericDAO(new Isotope());
+			$LOG->debug("  $amount");
 
-            if($amount->getIsotope_id() == null){
-                $useDao = new GenericDAO(new ParcelUse());
+			// A use amount may reference one or more isotopes
+			// We need to collect all isotopes and their relative percentage of the amount
+			// Once we have all the parts, we can calculate the per-isotope values for this Amount
+			$isotopes_percents = [];
+
+			// Amount is related to a miscellaneous waste,
+			//  or a single isotope
+			// This amount is comprised 100% of a single Isotope
+            if($amount->getIsotope_id() != null){
+				$isotope = $isotopeDao->getById($amount->getIsotope_id());
+				$isotopes_percents[] = [$isotope, 100];
+
+				$LOG->debug("  Amount references a single isotope");
+			}
+
+			// else amount is related to multiple isotopes
+			else {
+				$LOG->debug("  Amount references multiple isotopes");
+
+				// use -> parcel -> parcelauth => auths -> isotopes
+				$useDao = new GenericDAO(new ParcelUse());
                 $use = $useDao->getById($amount->getParcel_use_id());
 
                 $parcelDao = new GenericDAO(new Parcel());
-                $parcel = $parcelDao->getById($use->getParcel_id());
+				$parcel = $parcelDao->getById($use->getParcel_id());
 
-                $authDao = new GenericDAO(new Authorization());
-                $auth = $authDao->getById(18);
+				$parcelauths = $parcel->getParcelAuthorizations();
+				foreach($parcelauths as $parcelauth){
+					$isotopes_percents[] = [$parcelauth->getIsotope(), $parcelauth->getPercentage()];
+				}
+			}
 
-                $isotope = $isotopeDao->getById($auth->getIsotope_id());
-            }else{
-                $isotope = $isotopeDao->getById($amount->getIsotope_id());
-            }
+			$LOG->debug("  Amount includes " . count($isotopes_percents) . " isotopes:");
+			foreach($isotopes_percents as $ip){
+				$isotope = $ip[0];
+				$percentage = $ip[1];
 
-			$isotopeName = $isotope->getName();
-			$isotopeId   = $isotope->getKey_id();
-			$isMass   = $isotope->getIs_mass();
-			if(!array_key_exists($isotopeName, $isotopeAmounts)){
-				$isotopeAmount = new IsotopeAmountDTO();
-				$isotopeAmount->setIsotope_name($isotopeName);
-				$isotopeAmount->setIsotope_name($isotopeName);
-				$isotopeAmount->setIs_mass($isMass);
-				$isotopeAmount->setCurie_level($amount->getCurie_level());
-				$isotopeAmounts[$isotopeName] = $isotopeAmount;
-			}else{
-				$isotopeAmounts[$isotopeName]->addCuries($amount->getCurie_level());
+				$LOG->debug("   : $percentage% $isotope");
+
+				$isotopeValue = $amount->getCurie_level() * ($percentage / 100);
+				$isotopeName = $isotope->getName();
+				$isotopeId   = $isotope->getKey_id();
+				$isMass   = $isotope->getIs_mass();
+
+				// Apply isotope values to our collection
+				if(!array_key_exists($isotopeName, $isotopeAmounts)){
+					// Add empty entry for our collection
+					$isotopeAmount = new IsotopeAmountDTO();
+					$isotopeAmount->setIsotope_name($isotopeName);
+					$isotopeAmount->setIs_mass($isMass);
+					$isotopeAmount->setCurie_level(0);
+
+					$isotopeAmounts[$isotopeName] = $isotopeAmount;
+				}
+
+				// Add value to the existing entry
+				$isotopeAmounts[$isotopeName]->addCuries($isotopeValue);
 			}
 		}
+
 		return array_values($isotopeAmounts);
 	}
 
 }
+?>
